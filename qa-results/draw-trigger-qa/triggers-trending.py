38dc8e40732d6123c090ffeb5dcabcab1d8da5d8
Iohan Petrov <petrov.knu@gmail.com>
2016-09-07 17:34:28 +0200

    Fixed style for trigger QA plots

--- qa-results/draw-trigger-qa/triggers-trending.py
+++ qa-results/draw-trigger-qa/triggers-trending.py
@@ -13,8 +13,16 @@ def explore(data):
 			print
 
 def draw_multiple(hists, colors):
+	styles  = [2, 3, 1, 1] * 2
+	widths  = [2, 2, 1, 1] * 2
+	markers = [34, 20, 21, 22] * 2
+	map(lambda h, c: h.SetMarkerColor(c), hists, colors)
 	map(lambda h, c: h.SetLineColor(c), hists, colors)
-	map(lambda h: h.Draw("hist same"), hists)
+	map(lambda h: h.Draw("ep same"), hists)
+	# for i, h in enumerate(hists): h.SetLineStyle(styles[i])
+	# for i, h in enumerate(hists): h.SetLineWidth(widths[i])
+	for i, h in enumerate(hists): h.SetMarkerStyle(markers[i])
+	for i, h in enumerate(hists): h.SetMarkerSize(1.2)
 
 	legend = ROOT.TLegend(0.88, 0.6, 0.93, 0.93)
 	map(lambda h: legend.AddEntry(h, h.label), hists)
@@ -36,7 +44,7 @@ class TrendDrawer(object):
 		self.path = path
 		self.ntrus = 8
 		self.quantity = quantity
-		self.colors = [8, 9, 38, 48, 41, 5, 12, 1]
+		self.colors = [8, 28, 38, 46, 41, 5, 12, 1]
 
 		data = self.quantity.extract_modules(path + fname)
 		self.draw_trending_plots(data)
@@ -71,6 +79,8 @@ class TrendDrawer(object):
 			canvas = ROOT.TCanvas('c' + str(sm), 'canvas', 1200, 403)
 			legend = draw_multiple(hists, colors)
 			legend.Draw('same')
+			canvas.SetGridx()
+			canvas.SetGridy()
 			canvas.Update()
 			canvas.SaveAs(self.path + '/images/' + self.quantity.oname + '_' + sm + '.pdf')
 			# raw_input('Press <ENTER> ...')
@@ -87,14 +97,14 @@ class Quantity(object):
 	def extract_tru(self, tru):
 		fitf = ROOT.TF1('fitf', "[0]", self.threshod, self.maxenergy)
 		fitf.SetParameter(0, 0)
-		tru.Fit(fitf, 'wrq')
+		tru.Fit(fitf, 'wwrq')
 		# tru.SetAxisRange(-0.01, 2.,"Y");
 		# nicelydraw(tru)
 
 		# i = tru.GetName()[-1]
 		# return float(i), 0.
 
-		return fitf.GetParameter(0), 0 #fitf.GetParError(0)
+		return fitf.GetParameter(0), fitf.GetParError(0)
 
 	def extract_run(self, run):
 		return {tru.GetName(): self.extract_tru(tru) for tru in run }
@@ -102,6 +112,8 @@ class Quantity(object):
 	def extract_modules(self, fname):
 		ifile = ROOT.TFile(fname)
 		run_numbers = sorted(set([r.GetName().split('M')[0] for r in ifile.GetListOfKeys()]))
+		run_numbers = [r for r in run_numbers if r.isdigit()]
+		run_numbers = [r for r in run_numbers if int(r) > 257606]
 		supermods = [ 'M%d'%i for i in range(1, 5)]
 
 		get_ratio = lambda x: [h for h in x if 'Ratio' in h.GetName()]
@@ -125,14 +137,14 @@ class QuantityChannels(Quantity):
 		self.histname = 'h4x4' + kwargs['cluster']
 		self.nevents  = 'hNev'
 		self.oname = title.split()[0] + kwargs['cluster']
+		self.normalization = 1.
 
 	def extract_tru(self, itru, hist): 
 		x, y = tru_coordinates(itru)
-		# [hist.SetBinContent(i, j, 1)  for j in y for i in x]
-		# value = hist.Integral(x[0], x[-1], y[0], y[-1])
 		if not hist: return 137, 137
-		value = self.cell_function(hist, x, y)
-		return value, 0
+		# [hist.SetBinContent(i, j, 1)  for j in y for i in x]
+		value, error = self.cell_function(hist, x, y, self.normalization)
+		return value, error
 
 	def extract_run(self, perrun):
 		res = {str(itru): self.extract_tru(itru, perrun) for itru in range(1, 9) }
@@ -143,6 +155,9 @@ class QuantityChannels(Quantity):
 	def extract_modules(self, fname):
 		ilist = ROOT.TFile(fname).L0
 		run_numbers = sorted(r.GetName() for r in ilist)
+		run_numbers = [r for r in run_numbers if r.isdigit()]
+		run_numbers = [r for r in run_numbers if int(r) > 257606]
+
 		supermods = list('SM' + str(i) for i in range(1, 5))
 		get_hist = lambda run, sm: ilist.FindObject(run).FindObject(self.histname + sm)
 		raw  = {sm: {run: get_hist(run, sm) for run in run_numbers} for sm in supermods}
@@ -150,12 +165,27 @@ class QuantityChannels(Quantity):
 		get_nevents = lambda run : ilist.FindObject(run).FindObject(self.nevents).GetBinContent(1)
 		for sm in raw: 
 			for run in raw[sm]:
-				if not raw[sm][run]: continue
+				if not raw[sm][run]:
+					print 'ERROR: no data for ', run, sm
+					continue
 				raw[sm][run].Rebin2D(2, 2)
-				raw[sm][run].Scale(1./ get_nevents(run))
+				# raw[sm][run].Scale(1./ get_nevents(run))
+				self.normalization = get_nevents(run)
 
 		data = {sm: {run: self.extract_run(raw[sm][run]) for run in sorted(raw[sm])} for sm in raw}
 		return data
+
+from math import sqrt
+def integral(h, x, y, norm):
+	val_err = ROOT.Double(0)
+	val, norm_err = h.IntegralAndError(x[0], x[-1], y[0], y[-1], val_err), sqrt(norm)
+	res, res_err = val / norm, sqrt( (val_err / norm) ** 2 + (norm_err * val / norm / norm) ** 2)
+	print res, res_err
+	return res, res_err
+
+def nchannels(h, x, y, norm):
+	val = sum(1  for j in y for i in x if h.GetBinContent(i, j) > 0)
+	return val, sqrt(val)
 
 		
 def main():
@@ -164,19 +194,20 @@ def main():
 	ratio = Quantity("Triggered clusters / all clusters in %s in E > 4.5 GeV region;;Triggered clusters / all clusters")
 	TrendDrawer(path, 'ratio.root', ratio)
 
+	trigfile = 'TriggerQASingle.root'
+	draw_quantity = lambda q: TrendDrawer(path, trigfile, q)
+
 	cluster_types = {'': '', 'matching cluters with E > 2 GeV': 'Clu'}
 	for tp in cluster_types:
 		cltype = cluster_types[tp]
 
-		integral = lambda h, x, y: h.Integral(x[0], x[-1], y[0], y[-1])
 		ctitle   = "Probability for TRUs in %s to be fired, " + tp + " ;; N_{Fired channels} / N_{kINT7}" 
 		probability = QuantityChannels(ctitle, cluster=cltype, cellf=integral)
-		TrendDrawer(path, 'TriggerQASingle.root', probability)
+		draw_quantity(probability)
 
-		sumfunc  = lambda h, x, y: sum(1  for j in y for i in x if h.GetBinContent(i, j) > 0)
 		ptitle   = "Number of TRU channels fired in %s " + tp + ";; # of TRU channels"
-		channels = QuantityChannels(ptitle, cluster=cltype, cellf=sumfunc)
-		TrendDrawer(path, 'TriggerQASingle.root', channels)
+		channels = QuantityChannels(ptitle, cluster=cltype, cellf=nchannels)
+		draw_quantity(channels)
 
 
 if __name__ == '__main__':
