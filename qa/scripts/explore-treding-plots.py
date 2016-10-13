037f09912ee58e171506af537006997a9230c8df
Iohan Petrov <petrov.knu@gmail.com>
2016-10-13 16:58:17 +0200

    Improved explore-trending-plots.py

--- qa/scripts/explore-treding-plots.py
+++ qa/scripts/explore-treding-plots.py
@@ -11,7 +11,7 @@ import sys
 PERIOD = sys.argv[1]
 DIRECTORY = sys.argv[2]
 
-def convert_rdata(dser, dlist):
+def scale_averages(dser, dlist):
     if len(dser) < 1 : return []
     if dlist and len(dser) != len(dlist): return []
     coef = max(dser) * 1. / max(dlist)
@@ -19,31 +19,52 @@ def convert_rdata(dser, dlist):
     return  res
 
 
+def draw_averages(indeces, averages, avtitle):
+    fig = plt.figure(figsize=(2500, 5));
+    plt.subplots_adjust(left=0.02, right=0.98, top=0.9, bottom=0.15)
+    axbar = plt.bar(indeces, averages, color='g', alpha=0.5, align='center', label = avtitle)
+    plt.bar(indeces, averages, color='g', alpha=0.5, align='center', label = avtitle)
+    plt.bar(indeces, np.zeros_like(averages) + averages * (averages > averages.mean()), color='r', alpha=0.5, align='center', label = avtitle)
 
-def draw_data(i, frame, rdata, runs, rtitle):
+    # fig.tight_layout()
+    return fig, axbar
+
+
+
+
+def draw_data(i, frame, averages, runs, avtitle):
     if 'time' in i.lower() : return
     data = frame[i].dropna()
-    rdata = convert_rdata(data, rdata)
+    averages = scale_averages(data, averages)
 
-    if not len(rdata) > 1: return
+    if not len(averages) > 1: return
 
-    fig = plt.figure(figsize=(2500, 5));
-    alpha = 0 if frame[i].dtype == np.int else 1
-    ax = frame[i].plot(style='o', alpha=alpha, label = i)
-    plt.subplots_adjust(left=0.02, right=0.98, top=0.9, bottom=0.15)
-    axbar = plt.bar(data.index, rdata, color='g', alpha=0.5, align='center', label = rtitle)
-    plt.legend((axbar, ), (rtitle, ))
+    # Draw Scaled averages
+    fig, axbar = draw_averages(data.index, averages, avtitle)
 
-    if frame[i].dtype == np.int:
-        for x, y in frame[i].iteritems(): ax.annotate(str(y),xy=(x, y), rotation=90, horizontalalignment='center', verticalalignment='center')
+    # Draw data
+    ax = data.plot(style='o', alpha= int(data.dtype != np.int) , label = i)
+
+    # Legend, and labels
+    plt.legend((axbar, ), (avtitle, ))
+    if data.dtype == np.int:
+        for x, y in data.iteritems(): 
+            ax.annotate(str(y),xy=(x, y), rotation=90, horizontalalignment='center', verticalalignment='center')
+
 
     if 'event' in i.lower(): ax.set_yscale('log')
+    if 'duration' in i.lower(): ax.set_yscale('log')
+
+    # Run numbers/ titles
     plt.xticks(frame.index, frame['run'], rotation='vertical')
     plt.title(i.replace('_', ' '))
+
+    # Image sizes
     x1, x2, y1, y2 = plt.axis()
     plt.axis((x1, x2* 1.0001, y1, y2 * 1.0001))
+
     plt.show()
-    # fig.tight_layout()
+
     fig.savefig(DIRECTORY + '/from-logbook-' + i + '.pdf')
 
 def read_averages(mdir):
@@ -54,15 +75,14 @@ def read_averages(mdir):
     return values, map(int, runs), mhsit.GetTitle()
 
 
-
 def main():
     frame = pd.read_csv(PERIOD + '/goodruns.csv', delimiter=';').sort_values(by='run')
     frame = frame[frame.applymap(np.isreal)]
-    data, runs, t = read_averages(DIRECTORY)
+    averages, runs, t = read_averages(DIRECTORY)
     frame = frame[frame['run'].isin(runs)]
     frame.sort_values('run')
     frame = frame.reset_index(drop=True) # No gaps
-    for i in frame: draw_data(i, frame, data, runs, t)
+    for i in frame: draw_data(i, frame, averages, runs, t)
 
 
 if __name__ == '__main__':
