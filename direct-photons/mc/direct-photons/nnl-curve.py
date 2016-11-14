c8f4e775a339a374ef604869cf89ada8491dcb81
Oleksandr Kovalenko <oleksandr.kovalenko@cern.ch>
2016-11-14 11:11:51 +0100

    Moved config to 7TeV state; All scale factors for nnl are set to 1

--- direct-photons/mc/direct-photons/nnl-curve.py
+++ direct-photons/mc/direct-photons/nnl-curve.py
@@ -6,9 +6,9 @@ import sys
 print sys.argv[1]
 
 class NNL2ROOT(object):
-    def __init__(self, fname, sigma_mb):
+    def __init__(self, fname, scale = 1):
         super(NNL2ROOT, self).__init__()
-        self.scale = sigma_mb #* 1e12
+        self.scale = scale = 1 
         self.data, self.sqrts = self.read(fname)
         self.graph()
 
@@ -27,8 +27,8 @@ class NNL2ROOT(object):
     def graph(self):
         graph = ROOT.TGraph()
         graph.SetName('nll')
-        for i, (pt, sig) in enumerate(self.data): graph.SetPoint(i, pt, sig / self.scale)
-        graph.SetTitle('NNL theoretical curve #sqrt{s} = %0.2g TeV' %  (self.sqrts / 1e3) + '; P_{t}, GeV/c')
+        for i, (pt, sig) in enumerate(self.data): graph.SetPoint(i, pt, sig * self.scale)
+        graph.SetTitle('NNL theoretical curve #sqrt{s} = %0.2g TeV' %  (self.sqrts ) + '; P_{t}, GeV/c')
         return graph
 
 
@@ -49,7 +49,10 @@ class NNL2ROOT(object):
 
 def main():
     if len(sys.argv) < 2: print 'Please, specify the input file.'
-    reader = NNL2ROOT(sys.argv[1], 74. * 1e6)
+    reader = NNL2ROOT(sys.argv[1])
+    # When it will be clear what is the scale then
+    # All the parameters can be hardcoded.
+    # reader = NNL2ROOT(sys.argv[1],  1. / 74. / 1e6 * 1e3)
     reader.save('nnl.root')
 
 if __name__ == '__main__':
