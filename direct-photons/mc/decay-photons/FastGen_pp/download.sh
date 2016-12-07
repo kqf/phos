f01ec0014568a12befa0e54aa6b4743e752871f4
Oleksandr Kovalenko <oleksandr.kovalenko@cern.ch>
2016-12-07 14:16:12 +0100

    Fixed download script in fast-gen-pp

--- direct-photons/mc/decay-photons/FastGen_pp/download.sh
+++
@@ -1,9 +0,0 @@
-function download_from_grid()
-{
-	counter=1
-    MHOME=/alice/cern.ch/user/o/okovalen
-	for run in $(alien_find $MHOME/$1/output/$2/ $3 | grep .root); do echo $run; alien_cp alien:$run tmp_results/$((counter++)).root; done
-}
-
-# download_from_grid Task SEED filename
-download_from_grid $1 $2 $3
