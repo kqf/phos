c48298d48556c2a426a71a0d1670aa45bab7e210
Oleksandr Kovalenko <oleksandr.kovalenko@cern.ch>
2017-01-09 21:30:21 +0100

    Disabled timing cut in PhysPhotonSelection

--- protons/analysis-task-pp/PhysPhotonSelection.cxx
+++ protons/analysis-task-pp/PhysPhotonSelection.cxx
@@ -128,7 +128,6 @@ void PhysPhotonSelection::InitSummaryHistograms()
 	{
 		TH1 * hist = dynamic_cast<TH1 *>(fListOfHistos->At(i));
 		if(!hist) continue;
-		cout << "REACHED HERE " << endl;
 		hist->Sumw2();
 	}
 }
@@ -198,9 +197,9 @@ void PhysPhotonSelection::SelectPhotonCandidates(const TObjArray * clusArray, TO
 		Float_t energy = clus->E();
 		Int_t isHighECluster = Int_t(energy > 1.);
 
-		Float_t timesigma = 12.5e-9; // 12.5 ns
-		if (TMath::Abs(clus->GetTOF()) > timesigma)
-			continue;
+		// Float_t timesigma = 12.5e-9; // 12.5 ns
+		// if (TMath::Abs(clus->GetTOF()) > timesigma)
+		// 	continue;
 
 		FillHistogram(Form("hMainClusterEnergy_SM%d", 0), p.E());
 		FillHistogram(Form("hMainClusterEnergy_SM%d", sm), p.E());
