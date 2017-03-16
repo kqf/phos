7094f371ec54d97d1f5e9b55463e1527175189fd
Iohan Petrov <petrov.knu@gmail.com>
2017-03-16 19:19:16 +0100

    Updated phspace test for algorithm cross-check.

--- neutral-meson-spectra/test/playground/phspace.py
+++ neutral-meson-spectra/test/playground/phspace.py
@@ -2,13 +2,46 @@
 
 
 import ROOT
+from itertools import combinations, product
+
+
+class BackgroundGenerator(object):
+    def __init__(self, raw_gamma_pectrum):
+        super(BackgroundGenerator, self).__init__()
+        self.spectrum = raw_gamma_pectrum
+
+    def generate(self):
+        # TODO: add multiple pi0s
+        # TODO: add spherical distribution
+        p = self.spectrum.GetRandom()
+        return [ROOT.TLorentzVector(0, 0, p, p)]
+
+    def generated_signal(self):
+        # TODO: return here something meaningfull
+        pass
+
+
+class SignalGenerator(object):
+    def __init__(self, config):
+        super(SignalGenerator, self).__init__()
+        self.config = config
+
+    def generate(self):
+        # TODO: add phase space dependence
+        # TODO: add mass/sigma pt dependence
+        return [ROOT.TLorentzVector(0, 0, 0, 0), ROOT.TLorentzVector(0, 0, 0, 0.135)]
 
 
 class InclusiveGenerator(object):
-    def __init__(self, fname, selname = 'PhysTender', hnames = ['hMassPtN3', 'hMixMassPtN3', 'EventCounter']):
+    def __init__(self, fname, selname = 'PhysTender', 
+                 hnames = ['hMassPtN3', 'hMixMassPtN3', 'EventCounter'], hpdistr='hClusterPt_SM0'):
         super(InclusiveGenerator, self).__init__()
         self.selname = selname
-        self.data = self.read(fname, hnames)
+        # TODO: we have to have Nevents here 
+        # as we wil luse it later for reading and retreiving the data
+        self.signal = SignalGenerator(None)
+        self.backgrnd = BackgroundGenerator(self.read(fname, [hpdistr])[0])
+        self.data, self.mixed, self.nevents = self.read(fname, hnames)
 
 
     def read(self, fname, hnames):
@@ -20,12 +53,39 @@ class InclusiveGenerator(object):
     def save_fake(self, fname):
         ofile = ROOT.TFile(fname, 'recreate')
         olist = ROOT.TList()
-        for i in self.data: olist.Add(i)
+        olist.Add(self.data)
+        olist.Add(self.mixed)
+        olist.Add(self.nevents)
         olist.Write(self.selname, 1)
         ofile.Close()
 
 
-    def real_distributions(self):
-        pass
+    def real_distributions(self, nevents):
+        mixed = []
+        for i in range(nevents):
+            photons = self.fill(self.signal.generate() + self.backgrnd.generate(), mixed)
+            mixed.append(photons)
+
+            if len(mixed) > 100:
+                mixed.pop()
+
+        # Just fill event counter
+        self.nevents.Fill(1, nevents)
+        return self.signal.generated
+
+
+    def fill(self, photons, mixed):
+        masspt = lambda x: x.M(), x.Pt()
+
+        for combination in combinations(photons):
+            self.data.Fill(*masspt(sum(combination)))
+
+        for previous in mixed:
+            for combination in product(photons, previous):
+                self.mixed.Fill(*masspt(sum(combination)))
+
+        # just return untouched to update mixed events
+        return photons  
+
 
 
--- neutral-meson-spectra/test/playground/test_check_algorithm.py
+++ neutral-meson-spectra/test/playground/test_check_algorithm.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python
 
-from spectrum.spectrum import PtAnalyzer, Spectrum
-from spectrum.input import Input, TimecutInput
+from spectrum.spectrum import Spectrum
+from spectrum.input import Input
 
 from phspace import InclusiveGenerator
 
 import test.check_default
 import os
 
-class CheckPileup(test.check_default.CheckDefault):
+class CheckAlgorithm(test.check_default.CheckDefault):
     def setUp(self):
-        super(CheckPileup, self).setUp()
+        super(CheckAlgorithm, self).setUp()
 
         self.genfilename = 'LHC16-fake.root'
-        generator = InclusiveGenerator('input-data/LHC16.root')
-        generator.save_fake(self.genfilename)
+        self.generator = InclusiveGenerator('input-data/LHC16.root')
+        self.original_distributions = self.generator.save_fake(self.genfilename)
 
+    
+    def test(self):
         f = lambda x, y, z: Spectrum(x, label=y, mode=z, relaxedcb=True).evaluate()
         self.results = [
-                        f(Input(self.genfilename, generator.selname).read(), '', self.mode)
-                        # generator.real_distributions()
+                        f(Input(self.genfilename, self.generator.selname).read(), '', self.mode),
+                        self.original_distributions
                        ]
-
+  
     def tearDown(self):
         os.remove(self.genfilename)
 
