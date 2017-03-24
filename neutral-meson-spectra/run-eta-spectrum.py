0f12412d8d77c7b68072e27403c0dac439a4aa85
Sasha <petrov.knu@gmail.com>
2017-03-24 17:57:33 +0100

    Important: fixed a bug with eta parameters. Added some hists for asymmetry check.

--- neutral-meson-spectra/run-eta-spectrum.py
+++ neutral-meson-spectra/run-eta-spectrum.py
@@ -5,16 +5,17 @@ import ROOT
 from spectrum.spectrum import PtAnalyzer, Spectrum
 from spectrum.input import Input, ExampleInput
 from spectrum.sutils import get_canvas
+from spectrum.options import Options
 
 def main():
     c1 = get_canvas()
     # f = lambda x, y, z: Spectrum(x, label=y, mode=z).evaluate()
-    f = lambda x, y, z: PtAnalyzer(x, label=y, mode=z, particle='eta').quantities()
+    f = lambda x, y, z: PtAnalyzer(x, label=y, mode=z, options=Options(particle='eta')).quantities()
 
-    infile = 'input-data/LHC16.root'
+    infile = 'input-data/LHC16-new.root'
     results = [
-               f(Input(infile, 'EtaTender').read(), 'strict', 'q'), 
-               f(Input(infile, 'EtaOnlyTender').read(), 'standard', 'q')
+               f(Input(infile, 'EtaTender').read(), 'strict', 'v'), 
+               f(Input(infile, 'EtaOnlyTender').read(), 'standard', 'v')
               ]
 
     import spectrum.comparator as cmpr
