8697a413cfbba25a72275de231cd42ee47f29c6b
Iohan Petrov <petrov.knu@gmail.com>
2017-04-12 18:25:26 +0200

    Created a snippet of a repository to run analysis on old data.

---
+++ protons/run-analysis-old/CreatePlugin.C
@@ -0,0 +1,94 @@
+AliAnalysisGrid * CreatePlugin(const char * pluginmode = "test", Int_t * runs, Int_t nruns, TString period, TString comment, Bool_t useJDL, Bool_t isMC)
+{
+	if (period.Length() < 6)
+		cerr << "Error: Wrong run period (too short)" << period << endl;
+
+	AliAnalysisAlien * plugin = new AliAnalysisAlien();
+	plugin->SetOverwriteMode(kTRUE);
+
+	plugin->SetMergeViaJDL(useJDL);
+	plugin->SetOutputToRunNo(kTRUE);
+
+
+	plugin->SetRunMode(pluginmode);
+
+	plugin->SetAPIVersion("V1.1x");
+	plugin->SetAliPhysicsVersion("vAN-20170222-1");
+
+
+	plugin->SetExecutableCommand("aliroot");
+	plugin->SetExecutableArgs("-b -q -x");
+
+	plugin->SetCheckCopy(kFALSE);
+
+	// Extract period and reconstruction pass
+	TString dir(period, isMC ? 9 : 6); // fancy slicing
+	TString reconstruction(period);
+	reconstruction.ReplaceAll(dir + (reconstruction.Contains(dir + "-") ? "-" : "") , "");
+	reconstruction.ReplaceAll("-", "_");
+
+	TString globaldir = isMC ? "/alice/sim/2010/" : "/alice/data/2010/";
+	plugin->SetGridDataDir(globaldir + dir);
+	cout << "/alice/data/2010/" + dir << endl;
+
+	TString datasuffix = isMC ? "AOD/" : "/*.";
+	plugin->SetDataPattern("/" + reconstruction + datasuffix + "*/AliAOD.root");
+	cout << "Data pattern " << "/" + reconstruction + "/*.*/AliAOD.root" << endl;
+
+	// plugin->SetDataPattern("/" + reconstruction + "/AOD/*/AliAOD.root");
+	// plugin->SetDataPattern("/muon_calo_pass1/*.*/AliESDs.root");
+	if(!isMC)
+		plugin->SetRunPrefix("000");
+
+	// TODO: Read runs here
+	cout << "We are trying to analyse " << nruns << " runs" << endl;
+
+	for (Int_t i = 0; i < nruns; ++i)
+		plugin->AddRunNumber(runs[i]);
+
+	plugin->SetDefaultOutputs(kFALSE);
+	// plugin->SetOutputFiles("CaloCellsQA2.root TriggerQA.root");
+	// plugin->SetOutputFiles("TriggerQA.root");
+
+	period.ToLower();
+	plugin->SetGridWorkingDir("pp-phos-" + period + comment);
+	// plugin->SetGridWorkingDir("phos-16h-muon-calo-pass1-good-tender");
+	plugin->SetGridOutputDir("output");
+	// plugin->SetDefaultOutputs();
+	// Now this should be added in your AddTaskMacro.C
+
+
+	plugin->AddIncludePath("-I$ALICE_PHYSICS/include");
+	// plugin->SetAnalysisSource("AliAnalysisTaskPi0QA.cxx");
+	// plugin->SetAdditionalLibs("libPWGGAPHOSTasks.so");// AliAnalysisTaskPi0QA.cxx AliAnalysisTaskPi0QA.h");
+
+	period.ReplaceAll('-', '_');
+
+	// All files are set in the Add*Task.C macros
+	// plugin->SetAnalysisSource();
+	// plugin->SetAdditionalLibs("libPWGGAPHOSTasks.so ");
+
+	plugin->SetAnalysisMacro(TString("TaskQA") + period + ".C");
+	plugin->SetSplitMaxInputFileNumber(100);
+	plugin->SetExecutable(TString("TaskQA") + period + ".sh");
+
+	plugin->SetTTL(30000);
+	plugin->SetInputFormat("xml-single");
+	plugin->SetJDLName(TString("TaskQA") + period + ".jdl");
+	plugin->SetPrice(1);
+	plugin->SetSplitMode("se");
+	plugin->SetProofCluster ( "alice-caf.cern.ch" );
+	plugin->SetProofDataSet ( "/alice/data/LHC10h_000138150_p2" );
+	plugin->SetProofReset ( 0 );
+	plugin->SetNproofWorkers ( 0 );
+
+
+	plugin->SetAliRootMode ( "default" );
+	plugin->SetClearPackages ( kFALSE );
+	plugin->SetFileForTestMode ( "filesmc.txt" );
+	plugin->SetProofConnectGrid ( kFALSE );
+	plugin->SetDropToShell(kFALSE);
+	if (!plugin)
+		cout << "Warning \n";
+	return plugin;
+}
---
+++ protons/run-analysis-old/run.C
@@ -0,0 +1,114 @@
+#include "algorithm"
+
+void run(TString period, const char * runmode = "local", const char * pluginmode = "test", Bool_t isMC = kFALSE, Bool_t useJDL = kTRUE)
+{
+    SetupEnvironment();
+
+    // TODO: Don't use getRunsBadCells function anymore
+    // getRunsBadCells(period, good_runs, nruns, excells, nexc);
+
+
+    gROOT->LoadMacro("CreatePlugin.C");
+
+    // TODO: Don't don't keep runnumbers inside the runmacro
+    AliAnalysisGrid * alienHandler = CreatePlugin(pluginmode, period, "", useJDL, isMC);
+
+    if (!alienHandler) return;
+
+    AliAnalysisManager * mgr  = new AliAnalysisManager("PHOS_QA");
+    AliESDInputHandler * esdH = new AliESDInputHandler();
+    AliAODInputHandler * aodH = new AliAODInputHandler();
+
+    if (isMC)
+    {
+        esdH->SetReadFriends( isMC );
+        esdH->SetNeedField();
+        // mgr->SetInputEventHandler( esdH );
+
+    }
+    // mgr->SetInputEventHandler( esdH );
+    mgr->SetInputEventHandler(aodH);
+
+    if ( isMC )
+    {
+        AliMCEventHandler * mchandler = new AliMCEventHandler();
+        mchandler->SetReadTR ( kFALSE ); // Don't read track references
+        mgr->SetMCtruthEventHandler ( mchandler );
+    }
+
+    // Connect plug-in to the analysis manager
+    mgr->SetGridHandler(alienHandler);
+
+    gROOT->LoadMacro ("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C");
+
+    Bool_t enablePileupCuts = kTRUE;
+    AddTaskPhysicsSelection (isMC, enablePileupCuts);  //false for data, true for MC
+
+    gROOT->LoadMacro("AliAnalysisTaskCaloCellsQAPt.h+g");
+    gROOT->LoadMacro("AddTaskCaloCellsQAPt.C");
+    gROOT->LoadMacro("AddAnalysisTaskPP.C");
+    gROOT->LoadMacro("../../qa/qa-track-averages/AddAnalysisTaskTrackAverages.C");
+
+    TString files = "";
+    TString pref =  isMC ? "MC": "";
+
+    gROOT->LoadMacro("$ALICE_PHYSICS/PWGGA/PHOSTasks/PHOS_PbPb/AddAODPHOSTender.C");
+
+    // TODO: Use old tender parameters
+    AliPHOSTenderTask * tenderPHOS = AddAODPHOSTender("PHOSTenderTask", "PHOStender") ;
+    AliPHOSTenderSupply * PHOSSupply = tenderPHOS->GetPHOSTenderSupply();
+
+    // TODO: Check old map of bad channels
+    PHOSSupply->ForceUsingBadMap("BadMap_LHC10.root");
+
+    // There is no need to download QA when we use don't use JDL
+    if (useJDL)
+        files += AddTaskCaloCellsQAPt(excells, nexc);
+
+    AddAnalysisTaskPP(AliVEvent::kINT7, period + pref + " ##Updated event counters, ncontributors cut## only tender", "OnlyTender", "", 0, 0, isMC);
+
+
+    if ( !mgr->InitAnalysis( ) ) return;
+    mgr->PrintStatus();
+
+
+    alienHandler->SetOutputFiles(files);
+    mgr->StartAnalysis (runmode);
+    gObjectTable->Print( );
+}
+
+void SetupEnvironment()
+{
+    // ROOT
+    gSystem->Load ( "libCore.so" );
+    gSystem->Load ( "libGeom.so" );
+    gSystem->Load ( "libVMC.so" );
+    gSystem->Load ( "libPhysics.so" );
+    gSystem->Load ( "libTree.so" );
+    gSystem->Load ( "libMinuit.so" );
+
+    // AliROOT
+    gSystem->Load ( "libSTEERBase.so" );
+    gSystem->Load ( "libESD.so" );
+    gSystem->Load ( "libAOD.so" );
+    gSystem->Load ( "libANALYSIS.so" );
+    gSystem->Load ( "libANALYSISalice.so" );
+    gSystem->Load ( "libPWGGAPHOSTasks.so" );
+
+    // Tender
+    gSystem->Load("libTender.so");
+    gSystem->Load("libTenderSupplies.so");
+    gSystem->Load("libPWGGAPHOSTasks.so");
+
+    // for running with root only
+    gSystem->Load( "libTree.so" );
+    gSystem->Load( "libGeom.so" );
+    gSystem->Load( "libVMC.so" );
+    gSystem->Load( "libPhysics.so" );
+
+    //add include path
+    gSystem->AddIncludePath( "-I$ALICE_ROOT/include" );
+    gSystem->AddIncludePath( "-I$ALICE_PHYSICS/include" );
+    gSystem->SetMakeSharedLib(TString(gSystem->GetMakeSharedLib()).Insert(19, " -Wall ") );
+}
+
