18ffbd6a4e5fca62f4dce5818acc6f4aa2891c2f
Iohan Petrov <petrov.knu@gmail.com>
2017-05-09 23:43:49 +0200

    Added test to compare results from different MC flags (neutral mesons).

--- neutral-meson-spectra/test/mc/test_mc.py
+++ neutral-meson-spectra/test/mc/test_mc.py
@@ -2,7 +2,7 @@
 
 from spectrum.spectrum import Spectrum
 from spectrum.input import Input, TimecutInput
-from spectrum.sutils import get_canvas
+from spectrum.sutils import get_canvas, adjust_canvas
 from spectrum.options import Options
 
 import unittest
@@ -31,6 +31,29 @@ class CheckMC(unittest.TestCase):
         for r in self.results:
             diff = cmpr.Comparator()
             diff.compare_set_of_histograms(r)
+
+
+class CheckMCDifferentVersions(unittest.TestCase):
+
+    def setUp(self):
+        super(CheckMCDifferentVersions, self).setUp()
+        f = lambda x, y, z: Spectrum(x, label=y, mode = 'q', options = z).evaluate()
+
+        # The problem with the time distribution in MC:
+        #  The right flag in AddTender should be used for MC case.
+
+        data = f(Input('input-data/LHC16.root', 'PhysTender').read(), 'Data', Options())
+        new = f(TimecutInput('input-data/Pythia-LHC16.root', 'TimeTender', 'MassPtN3').read(), 'Run2Default', Options(priority = 1))
+        old = f(TimecutInput('input-data/Pythia-LHC16-iteration2.root', 'TimeTender', 'MassPtN3').read(), 'LHC16all', Options(priority = 0)) 
+
+        self.results = [new, data, old]
+  
+    def testResultMC(self):
+        c1 = adjust_canvas(get_canvas(1./2, resize = True))
+
+        import spectrum.comparator as cmpr
+        diff = cmpr.Comparator()
+        diff.compare_set_of_histograms(self.results)
 
 
 
