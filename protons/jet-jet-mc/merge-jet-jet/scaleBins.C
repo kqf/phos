bc92ff135bd1149dee8ce348c734f19f158101cb
Sasha <petrov.knu@gmail.com>
2017-06-23 19:19:06 +0200

    Fixed bin scaling for jet-jet-mc

--- protons/jet-jet-mc/merge-jet-jet/scaleBins.C
+++ protons/jet-jet-mc/merge-jet-jet/scaleBins.C
@@ -1,91 +1,80 @@
-void ScaleMergedExample(Int_t bin = -1, TString filename = "Merged", TString folder = "HistogramFolder" )
+void scaleBins(TString fileToScale = "LHC17f8a_10.root", TString paths = "LHC17f8a_10.root/MCStudyOnlyTender/", TString prefix = "scaled-")
 {
-  if (bin <= 0)  for (Int_t i = 1; i < 11; i++) ForBin(i);
-  else ForBin(bin, opt, tm);
+	Float_t scaleFactor = ScaleFactor(paths);
+
+	TFile * file = new TFile(fileToScale, "open");
+	TList * keys = file->GetListOfKeys();
+
+	for (int i = 0; i < keys->GetEntries(); ++i)
+	{
+		TKey * key = dynamic_cast<TKey * >(keys->At(i));
+		ScaleSelection(prefix + fileToScale, key->ReadObj(), scaleFactor);
+	}
+
 }
 
-void ForBin(Int_t bin = 1, TString filename = "Merged", TString folder = "HistogramFolder")
+Float_t ScaleFactor(TString paths)
 {
-  //printf("Scale bin %d\n",bin);
 
-  // TODO: Tune it for my input
+	TString name;
+	Int_t step = 0;
 
-  TFile * f = TFile::Open(Form("%d/%s.root", bin, filename.Data()), "read");
-  TList* list = (TList*) f->Get(folder);
+	vector<TString> path;
 
-  if (!list)
-  {
-    printf("No list available \n");
-    return;
-  }
+	while (paths.Tokenize(name, step, "/"))
+		path.push_back(name);
 
-  Int_t nEvents = ((TH1F*)list->FindObject("hNEvents"))->GetEntries();
-  //printf("N events %e %s\n",nEvents, opt.Data());
-  TObject * h ;
+	if (path.size() != 2)
+		return -1;
 
-// Set directly the cross section for production LHC12a2f
-//  Double_t scaleBin[] = {
-//    4.37e+00 , // Bin 0 : 0-5 GeV
-//    4.33e+00 , // Bin 1 : 5-11 GeV
-//    5.21e-01 , // Bin 2 : 11-21 GeV
-//    4.15e-02 , // Bin 3 : 21-36 GeV
-//    4.01e-03 , // Bin 4 : 36-57 GeV
-//    4.59e-04 , // Bin 5 : 57-84 GeV
-//    6.55e-05 , // Bin 6 : 84-117 GeV
-//    1.05e-05 , // Bin 7 : 117-152 GeV
-//    2.31e-06 , // Bin 8 : 152-191 GeV
-//    5.80e-07 , // Bin 9 : 191-234 GeV
-//    2.08e-07 , // Bin 10: > 234 GeV
-//  };
+	TFile * file = new TFile(path[0], "read");
+	TList * list = file->Get(path[1]);
+	list->ls();
 
-//  Double_t scale = scaleBin[bin]/nEvents ;
-//  printf("xs per event %e, xe nevents %e \n",scaleBin[bin],scale);
-
-  // Extract the cross section from the corresponding histograms in the file
-  TH1F* hScale = (TH1F *) list->FindObject("hScaleFactor");
-  Int_t nmerged  = hScale->GetEntries();
-  Double_t scaleF = hScale->GetBinContent(1);
-
-  printf("Bin %d, scale %e \n", bin, scaleF / nmerged);
-
-  Double_t scale = scaleF / nmerged / nEvents ;
-
-  printf("\t scaling ...\n ");
-
-  for (Int_t i = 0; i < list->GetEntries(); i++)
-  {
-    h = list->At(i);
-    if (h)
-    {
-      if ( !strncmp(h->ClassName(), "TH", 2) )
-      {
-        //snprintf(name, buffersize, "%sScaled", h->GetName()) ;
-
-        TH1 * hout = dynamic_cast<TH1*> (h);//(h->Clone(name)) ;
-
-        if (hout)
-        {
-          hout->Sumw2();
-          hout->Scale(scale) ;
-        }
-      }
-    }
-  }
-
-  printf("\t ... end scaling, write ...\n");
-  TFile * fout = new TFile(Form("%d/Scaled.root", bin), "recreate");
-
-  fout->Print("");
-
-  list->Write();
-
-  fout->Close();
-  delete fout;
-  f->Close();
-  delete f;
-  delete h;
-  delete hScale;
-  printf("\t ... done.\n");
+	TH1F * hXsec = dynamic_cast<TH1F *>(list->FindObject("hXsec"));
+	TH1F * hTrials = dynamic_cast<TH1F *>(list->FindObject("hTrials"));
 
 
+	if (!hXsec || !hTrials)
+		return -1;
+
+	if (!Int_t(hTrials->GetBinContent(1)))
+		return -1;
+
+	Float_t res =  hXsec->GetBinContent(1) / hTrials->GetBinContent(1);
+	file->Close();
+	delete file;
+	return res;
+
+}
+
+Bool_t ScaleSelection(TString out, TObject * objlist, Float_t scaleFactor)
+{
+	TList * list = dynamic_cast<TList *>(objlist);
+
+	TH1F * nevents = list->FindObject("EventCounter");
+	Int_t nev = nevents->GetBinContent(1);
+
+	for (int i = 0; i < list->GetEntries(); ++i)
+	{
+
+		TH1 * hist = dynamic_cast<TH1 * >(list->At(i));
+		TString name = hist->GetName();
+
+
+		if (name.Contains("hXsec") || name.Contains("hTrials"))
+			continue;
+
+		Float_t scale = (nev > 0) ? 1. / nev : 1.;
+		hist->Scale(scale);
+
+		if (!name.Contains("EventCounter"))
+			hist->Scale(scaleFactor);
+	}
+
+	TFile file(out, "update");
+	list->Write(list->GetName(), TObject::kSingleKey);
+	file.Close();
+
+	return kTRUE;
 }
