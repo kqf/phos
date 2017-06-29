bd63a4bf3098f024b5a2ab5a77e9bbb7d6b13a64
Oleksandr Kovalenko <oleksandr.kovalenko@cern.ch>
2017-06-29 21:23:27 +0200

    Imported an example of single pi0 written by Daiki for LHC16 data.

---
+++ protons/single-pi0-production/simulation-dpg/ReconstructionConfig.C
@@ -0,0 +1,172 @@
+/*
+ * AliDPG - ALICE Experiment Data Preparation Group
+ * Reconstruction steering script
+ *
+ */
+
+/*****************************************************************/
+/*****************************************************************/
+/*****************************************************************/
+
+enum EReconstruction_t {
+  kReconstructionDefault,
+  kReconstructionMuon,
+  kReconstructionITSpureSA,  
+  kReconstructionCustom,
+  kReconstructionPHOS,
+  kNReconstructions
+};
+
+const Char_t *ReconstructionName[kNReconstructions] = {
+  "Default",
+  "Muon",
+  "ITSpureSA",
+  "Custom"
+  ,"PHOS" //Daiki added
+};
+
+/*****************************************************************/
+
+ReconstructionConfig(AliReconstruction &rec, EReconstruction_t tag)
+{
+
+  TString system = gSystem->Getenv("CONFIG_SYSTEM");
+  
+  switch(tag) {
+    
+    // Default
+  case kReconstructionDefault:
+    ReconstructionDefault(rec);
+    return;
+    
+    // Muon
+  case kReconstructionMuon:
+    ReconstructionDefault(rec);
+    rec.SetRunReconstruction("MUON ITS VZERO");
+    return;
+
+    // ITSpureSA
+  case kReconstructionITSpureSA:
+    ReconstructionDefault(rec);
+    if (system.EqualTo("Pb-Pb"))
+      rec.SetRecoParam("ITS", OverrideITSRecoParam());
+    return;
+    
+    // Custom
+  case kReconstructionCustom:
+    if ((gROOT->LoadMacro("ReconstructionCustom.C")) != 0) {
+      printf("ERROR: cannot find ReconstructionCustom.C\n");
+      abort();
+      return;
+    }
+    ReconstructionCustom(rec);
+    return;
+
+    // PHOS Daiki added
+  case kReconstructionPHOS:
+    ReconstructionDefault(rec);
+    rec.SetRunReconstruction("PHOS");
+    return;
+
+  }
+
+}
+
+/*****************************************************************/
+
+ReconstructionDefault(AliReconstruction &rec)
+{
+
+  Int_t year = atoi(gSystem->Getenv("CONFIG_YEAR"));
+
+  //
+  // set OCDB source
+  TString ocdbConfig = "default,snapshot";
+  if (gSystem->Getenv("CONFIG_OCDB"))
+    ocdbConfig = gSystem->Getenv("CONFIG_OCDB");
+  if (ocdbConfig.Contains("alien")) {
+    // set OCDB 
+    //gROOT->LoadMacro("$ALIDPG_ROOT/MC/OCDBConfig.C");
+    gROOT->LoadMacro("OCDBConfig.C");
+    OCDBDefault(1);
+  }
+  else {
+    // set OCDB snapshot mode
+    rec.SetCDBSnapshotMode("OCDBrec.root");
+    //    AliCDBManager *cdbm = AliCDBManager::Instance();
+    //    cdbm->SetSnapshotMode("OCDBrec.root");
+  }
+
+    //
+    rec.SetCleanESD(kFALSE);
+    rec.SetStopOnError(kFALSE);
+    rec.SetWriteESDfriend();
+    rec.SetWriteAlignmentData();
+    rec.SetFractionFriends(.1);
+    rec.SetRunPlaneEff(kTRUE);
+    rec.SetUseTrackingErrorsForAlignment("ITS");
+    //
+    // IMPORTANT: For pPb productions anchored to 2013 runs, this setting has to be also added in the rec.C using AliRoot from v5-03-Rev-28 (announced in the Physics Board the 6th of June)
+    if (year == 2013)
+      rec.SetOption("TPC","PID.OADB=TSPLINE3_MC_%s_LHC13B2_FIXn1_PASS1_PPB_MEAN");
+    //
+    rec.SetRunQA(":");
+    //
+}
+
+/*****************************************************************/
+
+AliITSRecoParam *
+OverrideITSRecoParam()
+{
+
+  AliITSRecoParam * itsRecoParam = AliITSRecoParam::GetHighFluxParam();
+  itsRecoParam->SetClusterErrorsParam(2);
+
+  // special settings for ITS-SA-PURE only at all centralities
+  itsRecoParam->SetSAUseAllClusters();
+  itsRecoParam->SetTrackerSAOnly(kFALSE);
+  //  itsRecoParam->SetMaxSPDcontrForSAToUseAllClusters(10000000);
+  itsRecoParam->SetNLoopsSA(20);
+  itsRecoParam->SetPhiLimitsSA(0.002,0.07);
+  itsRecoParam->SetLambdaLimitsSA(0.003,0.04);
+
+  itsRecoParam->SetImproveWithVertex(kTRUE);
+  // Misalignment syst errors decided at ITS meeting 25.03.2010
+  // additional error due to misal (B off)
+  itsRecoParam->SetClusterMisalErrorY(0.0010,0.0010,0.0100,0.0100,0.0020,0.0020); // [cm]
+  itsRecoParam->SetClusterMisalErrorZ(0.0100,0.0100,0.0100,0.0100,0.0500,0.0500); // [cm]
+  // additional error due to misal (B on)
+  itsRecoParam->SetClusterMisalErrorYBOn(0.0010,0.0030,0.0250,0.0250,0.0020,0.0020); // [cm]
+  itsRecoParam->SetClusterMisalErrorZBOn(0.0050,0.0050,0.0050,0.0050,0.1000,0.1000); // [cm]
+  //----
+  
+  //Vertexer Z
+  itsRecoParam->SetVertexerZ();
+  
+  
+  // tracklets
+  itsRecoParam->SetTrackleterPhiWindowL2(0.07);
+  itsRecoParam->SetTrackleterZetaWindowL2(0.4);
+  itsRecoParam->SetTrackleterPhiWindowL1(0.10);
+  itsRecoParam->SetTrackleterZetaWindowL1(0.6);
+  //
+  itsRecoParam->SetTrackleterPhiWindow(0.06);
+  itsRecoParam->SetTrackleterThetaWindow(0.025);
+  itsRecoParam->SetTrackleterScaleDThetaBySin2T(kTRUE);
+  //
+  // Removal of tracklets reconstructed in the SPD overlaps 
+  itsRecoParam->SetTrackleterRemoveClustersFromOverlaps(kTRUE);
+  
+  // SDD configuration 
+  itsRecoParam->SetUseSDDCorrectionMaps(kTRUE); 
+  itsRecoParam->SetUseSDDClusterSizeSelection(kTRUE);
+  itsRecoParam->SetMinClusterChargeSDD(30.);
+  itsRecoParam->SetUseUnfoldingInClusterFinderSDD(kFALSE);
+  
+  itsRecoParam->SetEventSpecie(AliRecoParam::kHighMult);
+  itsRecoParam->SetTitle("HighMult");
+  
+  //
+  return itsRecoParam;
+}
---
+++ protons/single-pi0-production/simulation-dpg/SimulationConfig.C
@@ -0,0 +1,132 @@
+/*
+ * AliDPG - ALICE Experiment Data Preparation Group
+ * Simulation configuration script
+ *
+ */
+
+/*****************************************************************/
+/*****************************************************************/
+/*****************************************************************/
+
+enum ESimulation_t {
+  kSimulationDefault,
+  kSimulationMuon,
+  kSimulationCustom,
+  kSimulationPHOS,
+  kNSimulations
+};
+
+const Char_t *SimulationName[kNSimulations] = {
+  "Default",
+  "Muon",
+  "Custom"
+  ,"PHOS" //Daiki added
+};
+
+
+/*****************************************************************/
+
+SimulationConfig(AliSimulation &sim, ESimulation_t tag)
+{
+  
+  
+  switch(tag) {
+    
+    // Default
+  case kSimulationDefault:
+    SimulationDefault(sim);
+    return;
+    
+    // Muon
+  case kSimulationMuon:
+    SimulationDefault(sim);
+    sim.SetMakeSDigits("MUON VZERO");
+    sim.SetMakeDigitsFromHits("ITS");
+    return;
+    
+    // Custom
+  case kSimulationCustom:
+    if ((gROOT->LoadMacro("SimulationCustom.C")) != 0) {
+      printf("ERROR: cannot find SimulationCustom.C\n");
+      abort();
+      return;
+    }
+    SimulationCustom(sim);
+    return;
+ 
+  case kSimulationPHOS:
+    SimulationDefault(sim);
+    sim.SetMakeSDigits("PHOS");
+    sim.SetMakeDigits("PHOS");
+    return;
+ 
+  }
+  
+}
+
+SimulationDefault(AliSimulation &sim)
+{
+
+  Int_t year = atoi(gSystem->Getenv("CONFIG_YEAR"));
+  
+  //
+  // set OCDB source
+  TString ocdbConfig = "default,snapshot";
+  if (gSystem->Getenv("CONFIG_OCDB"))
+    ocdbConfig = gSystem->Getenv("CONFIG_OCDB");
+  if (ocdbConfig.Contains("alien")) {
+    // set OCDB 
+    //gROOT->LoadMacro("$ALIDPG_ROOT/MC/OCDBConfig.C");
+    gROOT->LoadMacro("OCDBConfig.C");
+    OCDBDefault(0);
+  }
+  else {
+    // set OCDB snapshot mode
+    sim.SetCDBSnapshotMode("OCDBsim.root");
+    //    AliCDBManager *cdbm = AliCDBManager::Instance();
+    //    cdbm->SetSnapshotMode("OCDBsim.root");
+  }
+
+  //
+  //
+  if (year < 2015) sim.SetMakeSDigits("TRD TOF PHOS HMPID EMCAL MUON ZDC PMD T0 VZERO FMD");
+  else             sim.SetMakeSDigits("TRD TOF PHOS HMPID EMCAL MUON ZDC PMD T0 VZERO FMD AD");
+  sim.SetMakeDigitsFromHits("ITS TPC");
+
+  //
+  // HLT settings
+  TString hltConfig = "auto";
+  if (gSystem->Getenv("CONFIG_HLT"))
+    hltConfig = gSystem->Getenv("CONFIG_HLT");
+  sim.SetRunHLT(hltConfig.Data());
+
+  //
+  //
+  SimulationConfigPHOS(sim);
+
+  //
+  //
+  sim.UseVertexFromCDB();
+  sim.UseMagFieldFromGRP();
+
+  //
+  //
+  sim.SetRunQA(":");
+  //
+}
+
+/*** PHOS ****************************************************/
+
+SimulationConfigPHOS(AliSimulation &sim)
+{
+  Int_t year = atoi(gSystem->Getenv("CONFIG_YEAR"));
+  AliPHOSSimParam *simParam = AliPHOSSimParam::GetInstance();
+  if (year < 2015) {
+    simParam->SetCellNonLineairyA(0.001);
+    simParam->SetCellNonLineairyB(0.2);
+    simParam->SetCellNonLineairyC(1.02);
+  }
+  else {
+    simParam->SetCellNonLinearity(kFALSE);
+  }
+}
