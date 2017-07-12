dbe521ac3a11f49897fa73a0e36f7b446ee830e5
Oleksandr Kovalenko <oleksandr.kovalenko@cern.ch>
2017-07-12 18:25:37 +0200

    Picked the corrected version of aliroot for single pi0 production

--- protons/single-pi0-production/simulation-dpg/run-sim.sh
+++ protons/single-pi0-production/simulation-dpg/run-sim.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cp ../*.root .
+# alien_cp alien:///alice/sim/2016/LHC16j2a1/OCDB/257893/OCDBsim.root . 
+# alien_cp alien:///alice/sim/2016/LHC16j2a1/OCDB/257893/OCDBrec.root . 
+chmod +x dpgsim.sh
 ./dpgsim.sh --run 257893 --mode full --uid 1 --nevents 1 --generator PHOSPi0 --detector PHOS --simulation PHOS --reconstruction PHOS
