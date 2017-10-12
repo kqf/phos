27714d44c187958537e03245221ae189ca7e463a
Oleksandr Kovalenko <oleksandr.kovalenko@cern.ch>
2017-10-12 17:26:46 +0200

    Add e/p ratio steering macros.

---
+++ protons/e2p-ratio/README.md
@@ -0,0 +1,41 @@
+E/p ratio analysis
+=============================
+
+
+## Run
+
+```bash
+
+# On lxplus machine
+
+# Setup the environment
+alienv enter VO_ALICE@AliPhysics::<RECENT VERSION OF ALIROOT>
+
+
+# Obrain token and update the environment
+alien-token-init
+
+source /tmp/gclient_env_`id -u $USER`
+
+# Runs full analysis
+# IMPORTANT: Due to grid limitations all datasets are split on two parts
+#            Remember that you need to modify DPART variable : put first or second.
+make grid
+```
+
+## Results
+Use the same scripts and code as in the case of new dataset.
+
+
+```bash
+# Ensure that you have proper environment and a valid token
+# merge the results 
+
+make terminate 
+
+
+# Download, merge and upload back the merged result
+make download
+```
+
+
---
+++ protons/e2p-ratio/run.C
@@ -0,0 +1,114 @@
+void run(TString period, const char * runmode = "local", const char * pluginmode = "test", TString dpart = "first", Bool_t isMC = kFALSE, Bool_t useJDL = kTRUE)
+{
+    SetupEnvironment();
+
+    gROOT->LoadMacro("CreatePlugin.cc+");
+    AliAnalysisGrid * alienHandler = CreatePlugin(pluginmode, period, dpart, useJDL, isMC);
+
+    if (!alienHandler) return;
+
+    AliAnalysisManager * mgr  = new AliAnalysisManager("PHOS_PP");
+    AliESDInputHandler * esdH = new AliESDInputHandler();
+    AliAODInputHandler * aodH = new AliAODInputHandler();
+
+    if (isMC)
+    {
+        esdH->SetReadFriends( isMC );
+        esdH->SetNeedField();
+        // mgr->SetInputEventHandler( esdH );
+
+    }
+    // mgr->SetInputEventHandler( esdH );
+    mgr->SetInputEventHandler(aodH);
+
+    if ( isMC )
+    {
+        AliMCEventHandler * mchandler = new AliMCEventHandler();
+        mchandler->SetReadTR ( kFALSE ); // Don't read track references
+        mgr->SetMCtruthEventHandler ( mchandler );
+    }
+
+    // Connect plug-in to the analysis manager
+    mgr->SetGridHandler(alienHandler);
+
+    gROOT->LoadMacro ("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C");
+
+    Bool_t enablePileupCuts = kTRUE;
+    AddTaskPhysicsSelection (isMC, enablePileupCuts);  //false for data, true for MC
+
+    TString files = "";
+    TString pref =  isMC ? "MC": "";
+
+    gROOT->LoadMacro("$ALICE_PHYSICS/PWGGA/PHOSTasks/PHOS_PbPb/AddAODPHOSTender.C");
+
+    TString tenderOption = isMC ? "Run2Default" : "";
+    AliPHOSTenderTask * tenderPHOS = AddAODPHOSTender("PHOSTenderTask", "PHOStender", tenderOption, 1, isMC);
+
+    AliPHOSTenderSupply * PHOSSupply = tenderPHOS->GetPHOSTenderSupply();
+    PHOSSupply->ForceUsingBadMap("../datasets/BadMap_LHC16-updated.root");
+
+    gROOT->LoadMacro("$ALICE_PHYSICS/PWGGA/PHOSTasks/PHOS_EpRatio/AddTaskPHOSEpRatio.C");
+    AddTaskPHOSEpRatio(isMC);
+
+    if(isMC)
+    {
+        // Important: Keep track of this variable
+        // ZS threshold in unit of GeV  
+        Double_t zs_threshold = 0.020;
+        PHOSSupply->ApplyZeroSuppression(zs_threshold); 
+    }
+
+
+    TString msg = "## Updated parameters for nonlinearity, 20 MeV Zero Supression ";
+
+    if (tenderOption)
+    {
+        msg += " with tender option ";
+        msg += tenderOption;
+    }
+
+    if ( !mgr->InitAnalysis( ) ) return;
+    mgr->PrintStatus();
+
+
+    cout << "Downloading files " << files << endl;
+    alienHandler->SetOutputFiles(AliAnalysisManager::GetCommonFileName());
+    mgr->StartAnalysis (runmode);
+    gObjectTable->Print( );
+}
+
+void SetupEnvironment()
+{
+    // ROOT
+    gSystem->Load ( "libCore.so" );
+    gSystem->Load ( "libGeom.so" );
+    gSystem->Load ( "libVMC.so" );
+    gSystem->Load ( "libPhysics.so" );
+    gSystem->Load ( "libTree.so" );
+    gSystem->Load ( "libMinuit.so" );
+
+    // AliROOT
+    gSystem->Load ( "libSTEERBase.so" );
+    gSystem->Load ( "libESD.so" );
+    gSystem->Load ( "libAOD.so" );
+    gSystem->Load ( "libANALYSIS.so" );
+    gSystem->Load ( "libANALYSISalice.so" );
+    gSystem->Load ( "libPWGGAPHOSTasks.so" );
+
+    // Tender
+    gSystem->Load("libTender.so");
+    gSystem->Load("libTenderSupplies.so");
+    gSystem->Load("libPWGGAPHOSTasks.so");
+
+    // for running with root only
+    gSystem->Load( "libTree.so" );
+    gSystem->Load( "libGeom.so" );
+    gSystem->Load( "libVMC.so" );
+    gSystem->Load( "libPhysics.so" );
+
+    //add include path
+    gSystem->AddIncludePath( "-I$ALICE_ROOT/include" );
+    gSystem->AddIncludePath( "-I$ALICE_PHYSICS/include" );
+    gSystem->SetMakeSharedLib(TString(gSystem->GetMakeSharedLib()).Insert(19, " -Wall ") );
+}
+
