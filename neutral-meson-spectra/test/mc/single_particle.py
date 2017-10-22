e3379df08f3bca912b6e3b97a8a1645c32615b37
Iohan Petrov <petrov.knu@gmail.com>
2017-10-22 19:46:20 +0200

    Add reconstructed/inclusive spectra to the single particle QA output.

--- neutral-meson-spectra/test/mc/single_particle.py
+++ neutral-meson-spectra/test/mc/single_particle.py
@@ -27,6 +27,7 @@ class SingleParticleQA(unittest.TestCase):
     def setUp(self):
         # Dataset description
         #
+        # NB: Always use unscaled version for QA
         self.simulation = {'LHC17j3a': '#gamma', 'LHC17j3b': '#pi^{0}', 'LHC17j3c': '#eta'}
         self.ptbin  = {'1': 'low p_{T}', '2': 'high p_{T}'}
 
@@ -35,26 +36,62 @@ class SingleParticleQA(unittest.TestCase):
         self.productions = 'LHC17j3b2', 'LHC17j3a2'
 
 
+    def test_pt_reconstructed(self):
+        for prod in self.productions:
+            reco = self.reconstructed(prod)
+            diff = Comparator(oname = prod + '_reco')
+            diff.compare(reco)
+
+
+    # @unittest.skip('')
     def test_pt_generated(self):
         for prod in self.productions:
-            etaphi = self.distribution(prod, 'hEtaPhi_{0}')
-            diff = Comparator()
-            diff.compare(etaphi)
+            pt = self.distribution(prod, 'hEtaPhi_{0}', 0)
+            diff = Comparator(oname = prod + '_yphi')
+            diff.compare(pt)
 
 
+    # @unittest.skip('')
     def test_eta_phi_distribution(self):
         for prod in self.productions:
             etaphi = self.distribution(prod, 'hPt_{0}_primary_')
-            diff = Comparator()
+            diff = Comparator(oname = prod + '_pt')
             diff.compare(etaphi)
 
 
-    def distribution(self, filename, histname):
+    def distribution(self, filename, histname, logy = 1):
         part, ptbin = self.particle_ptbin(filename)
         label = '{0} {1}'.format(part, ptbin)
         hist = read_histogram(filename, self.selection,\
             histname.format(part), label = label)
+        hist.SetTitle(hist.GetTitle() + ', ' + ptbin)
 
+        hist.logy = logy
         br.scalew(hist)
         return hist 
+
+
+    def reconstructed(self, filename):
+        part, ptbin = self.particle_ptbin(filename)
+
+        # NB: Gamma Follows different procedure
+        if 'gamma' in part:
+            hist = self.distribution(filename, 'hPt_{0}')
+            hist.SetTitle('Inclusive ' + hist.GetTitle().lower())
+            return hist
+
+        return self._reconstructed(filename)
+
+
+    def _reconstructed(self, filename):
+        part, ptbin = self.particle_ptbin(filename)
+        label = '{0} {1}'.format(part, ptbin)
+
+        inp = Input(filename, self.selection)
+        opt = Options(label= label, mode = 'd', particle = 'pi0' if 'pi' in part else 'eta')
+        reco = Spectrum(inp, opt).evaluate().spectrum
+        reco.SetTitle(reco.GetTitle() + ', ' + ptbin)
+        br.scalew(reco)
+        return reco
+
 
