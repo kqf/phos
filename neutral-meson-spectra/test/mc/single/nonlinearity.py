91f5776be9ec93cb87753d4c1d0337f7fbb72815
Iohan Petrov <petrov.knu@gmail.com>
2017-12-13 18:58:59 +0100

    Change the parametrization for nonlinearity

--- neutral-meson-spectra/test/mc/single/nonlinearity.py
+++ neutral-meson-spectra/test/mc/single/nonlinearity.py
@@ -13,12 +13,15 @@ import ROOT
 class TestNonlinearitySPMC(unittest.TestCase):
 
     def _nonlinearity_function(self):
-        func_nonlin = ROOT.TF1("func_nonlin", "[2] * (1.+[0]*TMath::Exp(-x/2*x/2/2./[1]/[1]))", 0, 20)
+        func_nonlin = ROOT.TF1("func_nonlin", "[2] * (1.+[0]*TMath::Exp(-x*x/2./[1]/[1]))", 0, 20)
         func_nonlin.SetParNames('A', '#sigma', 'E_{scale}')
-        # func_nonlin.SetParameters(-0.020071867975567686, 1.1184319273953713, 1.049371977284965)
-        func_nonlin.SetParameter(0, -0.01)
-        func_nonlin.SetParameter(1, 1.12)
-        func_nonlin.FixParameter(2, 1.06)
+        # func_nonlin.SetParameters(-0.014719244288611932, 0.8017501954719543, 1.050000000000015)
+        func_nonlin.FixParameter(0, -0.014719244288611932)
+        func_nonlin.FixParameter(1, 0.8017501954719543 * 2)
+        func_nonlin.FixParameter(2, 1.050000000000015)
+        # func_nonlin.SetParameter(0, -0.01)
+        # func_nonlin.SetParameter(1, 1.12)
+        # func_nonlin.FixParameter(2, 1.06)
 
         func_nonlin.SetParLimits(0, -10, -0.001)
         func_nonlin.SetParLimits(1, 0, 1.12)
