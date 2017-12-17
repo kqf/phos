13bd3955ef1de6e3c0e0c6d5a810aa28135d85f8
Iohan Petrov <petrov.knu@gmail.com>
2017-12-17 15:53:33 +0100

    Separate eta for pi0 code in overlap_region analysis.

--- neutral-meson-spectra/test/mc/single/overlap_region.py
+++ neutral-meson-spectra/test/mc/single/overlap_region.py
@@ -6,31 +6,21 @@ from spectrum.comparator import Comparator
 from spectrum.broot import BROOT as br
 
 
-# TODO: Change pt region for eta mesons
 class TestOverlapRegion(unittest.TestCase):
 
 
     def setUp(self):
-        self.eff_title = '#varepsilon = #Delta #phi #Delta y/ 2 #pi #frac{Number of reconstructed #pi^{0}}{Number of generated primary #pi^{0}}'
+        self.eff_title = '#varepsilon = #Delta #phi #Delta y/ 2 #pi ' \
+                         '#frac{Number of reconstructed #pi^{0}}{Number of generated primary #pi^{0}}'
 
 
+    @unittest.skip('')
     def test_eff_overlap(self):
         ddir = 'single/nonlin0/'
         files = {
                     ddir + 'LHC17j3b1': (0, 5),
                     ddir + 'LHC17j3b2': (9, 20)
                 }
-
-        # self.efficiency_overlap(files, 'weight1', 'pi0')
-
-        ddir = 'single/nonlin1/'
-        files = {
-                    ddir + 'LHC17j3c1': (0, 7),
-                    ddir + 'LHC17j3c2': (6, 20)
-                }
-
-        self.efficiency_overlap(files, 'weight1', 'eta')
-
 
 
     def efficiency_overlap(self, files, pref, particle):
@@ -59,4 +49,22 @@ class TestOverlapRegion(unittest.TestCase):
             e.logy = 0
 
         diff.compare(effs)
+
+# TODO: Change pt region for eta mesons
+class TestOverlapRegionEta(TestOverlapRegion):
+
+
+    def setUp(self):
+        self.eff_title = '#varepsilon = #Delta #phi #Delta y/ 2 #pi '\
+                         '#frac{Number of reconstructed #eta}{Number of generated primary #eta}'
+
+
+    def test_eff_overlap(self):
+        ddir = 'single/nonlin1/'
+        files = {
+                    ddir + 'LHC17j3c1': (0, 7),
+                    ddir + 'LHC17j3c2': (6, 20)
+                }
+
+        self.efficiency_overlap(files, 'weight1', 'eta')
 
