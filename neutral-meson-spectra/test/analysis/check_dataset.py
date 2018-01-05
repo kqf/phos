d403118a3c80d2fb113154813b0b73bbf65396e1
Iohan Petrov <petrov.knu@gmail.com>
2018-01-05 19:42:35 +0100

    Add test to check input data distributions

---
+++ neutral-meson-spectra/test/analysis/check_dataset.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import sys
+import unittest
+import operator as op
+
+import ROOT
+from spectrum.spectrum import Spectrum
+from spectrum.input import Input
+from spectrum.sutils import gcanvas
+from spectrum.options import Options
+import spectrum.comparator as cmpr
+
+from spectrum.broot import BROOT as br
+
+
+
+# This test is needed to check if the dataset does contain 
+# correct spectrum, invariant mass distribution is ok, etc.
+
+class TestDataSetForConsistency(unittest.TestCase):
+
+    def setUp(self):
+        fname = 'corrected/LHC16i-muon-calo-pass1.root' 
+        inputs = Input(fname, 'PhysTender', 'MassPt')
+        options = Options('old', 'q')
+        self.sp_estimator = Spectrum(inputs, options)
+
+
+    def test_dataset(self):
+        observables = self.sp_estimator.evaluate()
+
+        br.scalew(observables.spectrum)
+        br.scalew(observables.npi0)
+
+        c1 = gcanvas(1./2, resize=True)
+        diff = cmpr.Comparator()
+        for obs in observables:
+            diff.compare(obs)
+
+
+
