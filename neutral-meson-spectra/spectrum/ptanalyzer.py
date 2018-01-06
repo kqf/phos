2d488c299e961505b2254e97cdb8bcb61ab51b63
Iohan Petrov <petrov.knu@gmail.com>
2018-01-06 19:15:38 +0100

    Clean pt-analyzer, remove invariant mass processing from the class

--- neutral-meson-spectra/spectrum/ptanalyzer.py
+++ neutral-meson-spectra/spectrum/ptanalyzer.py
@@ -57,26 +57,11 @@ class PtAnalyzer(object):
         result.npi0.logy = True
         return  result
 
-
-    def properties(self, mass, intgr_ranges):
-        fitfun, background = mass.extract_data() 
-        if not (fitfun and background): return [[0, 0]] * 7
-        # calculate pi0 values
-        area, mmass, sigma = zip(*br.pars(fitfun, 3))
-        npi0 = mass.number_of_mesons(intgr_ranges)
-        nraw = map(lambda x: x / (2. * ROOT.TMath.Pi()), npi0)
-
-        ndf = fitfun.GetNDF() if fitfun.GetNDF() > 0 else 1
-        alpha = (fitfun.GetParameter(3), fitfun.GetParError(3))
-        n = (fitfun.GetParameter(4), fitfun.GetParError(4))
-
-        return mmass, sigma, nraw, (fitfun.GetChisquare() / ndf, 0), npi0, alpha, n
         
 
-    def quantities(self, draw = True, intgr_ranges = None):
-        # Prepare Pt ranges and corresponding M_eff integration intervals
-        if not intgr_ranges: intgr_ranges = [None] * len(self.masses)
-        values = map(lambda x, i: self.properties(x, i), self.masses, intgr_ranges)
+    # TODO: Move All Serialization logic to OutputCreator
+    def quantities(self, draw = True, intgr_ranges = []):
+        values = map(OutputCreator.eval, self.masses, intgr_ranges)
 
         # Create hitograms
         histos = self.histograms(values)
