a44aab2eb517825daef36dab673b3eea975aca7e
Sasha <q.dev.null@gmail.com>
2018-02-05 15:25:04 +0100

    Import ga test train configuration

---
+++ protons/train-tests/generate.C
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>302 Found</title>
+</head><body>
+<h1>Found</h1>
+<p>The document has moved <a href="https://login.cern.ch/adfs/ls/?wa=wsignin1.0&amp;wreply=https%3A%2F%2Ftwiki.cern.ch%2FShibboleth.sso%2FADFS&amp;wct=2018-02-05T12%3A44%3A33Z&amp;wtrealm=https%3A%2F%2Ftwiki.cern.ch%2FShibboleth.sso%2FADFS&amp;wctx=cookie%3A1517834673_71c9">here</a>.</p>
+<hr>
+<address>Apache Server at twiki.cern.ch Port 443</address>
+</body></html>
