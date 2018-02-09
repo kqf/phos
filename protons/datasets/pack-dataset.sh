4ec789722cd92fd1de1aeb4aa40e3521b172a009
Oleksandr Kovalenko <oleksandr.kovalenko@growbots.com>
2018-02-09 19:41:49 +0100

    Add script for packing datasets for train operators

---
+++ protons/datasets/pack-dataset.sh
@@ -0,0 +1,20 @@
+archive="LHC17.tar.gz"
+pattern="LHC17?-pass1.txt"
+prefix="verified-"
+
+
+function main(){
+	# Clean the directory
+	rm $prefix*
+
+	# Add comas at the end of each file
+	for file in $(ls $pattern); do
+		echo $file
+		sed '$!s/$/,/' $file > $prefix$file
+	done
+
+	tar czvf $archive $prefix*
+	rm $prefix*
+}
+
+main
