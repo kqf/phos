4e8cfbe6962b67271497440f90a9d9e70fd3f8be
Oleksandr Kovalenko <oleksandr.kovalenko@growbots.com>
2018-02-18 19:33:18 +0100

    Add a script for corrected yield extraction

---
+++ neutral-meson-spectra/analysis/yield/corrected.py
@@ -0,0 +1,62 @@
+import unittest
+
+# from spectrum.spectrum import Spectrum 
+from spectrum.input import Input 
+from spectrum.options import Options 
+from spectrum.efficiency import Efficiency
+from spectrum.corrected_yield import CorrectedYield
+from spectrum.efficiency import EfficiencyMultirange
+
+from spectrum.comparator import Comparator
+from spectrum.broot import BROOT as br
+from vault.datavault import DataVault
+
+
+
+class YieldEvaluator(object):
+
+
+    def __init__(self, particle):
+        super(YieldEvaluator, self).__init__()
+        self.particle= particle
+
+
+    def efficiency(self, fileranges):
+        efficiency_estimator = EfficiencyMultirange(
+            'hPt_{0}_primary_'.format(self.particle),
+            'efficiency',
+            fileranges,
+            particle=self.particle
+        )
+
+        efficiency = efficiency_estimator.eff()
+        efficiency.SetTitle(
+            '#varepsilon = #Delta #phi #Delta y/ 2 #pi ' \
+            '#frac{Number of reconstructed %s}{Number of generated primary %s}' \
+            % (self.particle, self.particle) 
+        )
+        return efficiency
+
+
+    def transform(self, inp, eff_files):
+        eff = self.efficiency(eff_files)
+        cy_estimator = CorrectedYield(inp, Options(), eff)
+        corrected_spectrum = cy_estimator.evaluate() 
+        return corrected_spectrum
+
+
+class TestCorrectedYield(unittest.TestCase):
+
+    def test_interface(self):
+        inp = Input(DataVault().file("data"), 'Phys', label='data')
+        estimator = YieldEvaluator("#pi^{0}")
+
+        files = {
+            DataVault().file("single #pi^{0}", "low"): (0, 5),
+            DataVault().file("single #pi^{0}", "high"): (9, 20)
+        } 
+        corrected = estimator.transform(inp, files)
+
+        diff = Comparator()
+        diff.compare(corrected)
+
