9f87865607a3b0b626bc18fc4ea7ec9a557c0408
Oleksandr Kovalenko <oleksandr.kovalenko@growbots.com>
2018-03-06 06:56:26 +0100

    Remove obsolete efficiency codes, move efficiency calculation to the analysis folder

--- neutral-meson-spectra/analysis/efficiency/spmc_efficiency.py
+++ neutral-meson-spectra/analysis/efficiency/spmc_efficiency.py
@@ -9,6 +9,28 @@ from spectrum.broot import BROOT as br
 from spectrum.efficiency import Efficiency, EfficiencyMultirange
 from spectrum.options import EfficiencyOptions, MultirangeEfficiencyOptions
 
+from vault.datavault import DataVault
+
+
+class TestEfficiencyPi0(unittest.TestCase):
+
+    def test_pi0_efficiency(self):
+        unified_inputs = {
+            DataVault().file("single #pi^{0}", "low"): (0, 7),
+            DataVault().file("single #pi^{0}", "high"): (7, 20)
+        }
+        evaluate_spmc_efficiency(unified_inputs, "#pi^{0}")
+
+
+class TestEfficiencyEta(unittest.TestCase):
+
+    def test_pi0_efficiency(self):
+        unified_inputs = {
+            DataVault().file("single #eta", "low"): (0, 6),
+            DataVault().file("single #eta", "high"): (6, 20)
+        }
+        evaluate_spmc_efficiency(unified_inputs, "#eta")
+  
 
 def evaluate_spmc_efficiency(unified_inputs, particle):
     
@@ -16,7 +38,7 @@ def evaluate_spmc_efficiency(unified_inputs, particle):
        MultirangeEfficiencyOptions.spmc(unified_inputs, particle) 
     )
 
-    loggs = AnalysisOutput("multirange_efficiency_spmc_{}".format(particle), particle)
+    loggs = AnalysisOutput("composite_efficiency_spmc_{}".format(particle), particle)
 
     efficiency = estimator.transform(
        [Input(filename, "PhysEff") for filename in unified_inputs],
@@ -26,10 +48,9 @@ def evaluate_spmc_efficiency(unified_inputs, particle):
     efficiency.SetTitle(
         "#varepsilon = #Delta #phi #Delta y/ 2 #pi " \
         "#frac{Number of reconstructed %s}{Number of generated primary %s}" \
-            % (particle, particle)
+        % (particle, particle)
     )
     diff = Comparator()
     diff.compare(efficiency)
     loggs.plot(False)
     
-
