0e8d4dfb9a7bd2cde1cd846114ef9102d5842ccd
Oleksandr Kovalenko <oleksandr.kovalenko@growbots.com>
2018-03-07 08:15:28 +0100

    Add tests for background extraction inspection

---
+++ neutral-meson-spectra/test/mc/single/background_extraction.py
@@ -0,0 +1,52 @@
+import unittest
+
+import spectrum.sutils as su
+
+from spectrum.processing import DataSlicer, RangeEstimator, DataExtractor, MassFitter
+from spectrum.output import AnalysisOutput
+from spectrum.input import Input
+from spectrum.options import Options
+from spectrum.pipeline import Pipeline
+from spectrum.comparator import Comparator
+from vault.datavault import DataVault
+
+
+class MassExtractor(object):
+
+    def __init__(self, options=Options()):
+        super(MassExtractor, self).__init__()
+        self.options = options
+        self._loggs = None
+
+    def transform(self, inputs, loggs):
+        pipeline = Pipeline([
+            ("input", inputs),
+            ("slice", DataSlicer(self.options.pt)),
+            ("fitmasses", MassFitter(self.options.invmass)),
+        ])
+
+        output = pipeline.transform(None, loggs)
+        return output
+
+class TestBackgroundSubtraction(unittest.TestCase):
+
+	def test_background_fitting(self):
+		loggs = AnalysisOutput("test_spmc_background", "#pi^{0}")
+
+		estimator = MassExtractor(Options.spmc((7, 20)))
+		masses = estimator.transform(
+			Input(DataVault().file("single #pi^{0}", "high"), "PhysEff"),
+			loggs
+		)
+
+		for i, ptbin in enumerate(masses):
+			# print i
+			# ptbin = masses[21]
+			ptbin.signal.GetXaxis().SetRangeUser(
+				*ptbin.integration_region
+			)
+			ptbin.signal.Draw()
+			su.wait()
+
+
+
