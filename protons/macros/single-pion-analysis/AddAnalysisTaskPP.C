c544ce495656b46d50a03a3bc6e2c8b86cbe7242
Oleksandr Kovalenko <oleksandr.kovalenko@cern.ch>
2018-03-24 16:56:02 +0100

    Remove nonlinearity scan from single pion analysis. As it's too heavy

--- protons/macros/single-pion-analysis/AddAnalysisTaskPP.C
+++ protons/macros/single-pion-analysis/AddAnalysisTaskPP.C
@@ -3,13 +3,13 @@
 void AddAnalysisTaskPP(UInt_t offlineTriggerMask, TString description, TString suff = "", TString badmap = "")
 {
 	LoadAnalysisLibraries();
-	
+
 	AliAnalysisManager * mgr = AliAnalysisManager::GetAnalysisManager();
-	if (!mgr) 
+	if (!mgr)
 	{
 		cerr << "Fatal: There is no analysis manager" << endl;
 		return;
-	}	
+	}
 	// Setup Selections
 	TList * selections = new TList();
 
@@ -18,7 +18,7 @@ void AddAnalysisTaskPP(UInt_t offlineTriggerMask, TString description, TString s
 	AliPP13SelectionWeights data_weights;
 	AliPP13ClusterCuts cuts_pi0 = AliPP13ClusterCuts::GetClusterCuts();
 	AliPP13ClusterCuts cuts_eta = AliPP13ClusterCuts::GetClusterCuts();
-	
+
 	cuts_pi0.fNContributors = 0;
 	cuts_eta.fNContributors = 0;
 	cuts_eta.fAsymmetryCut = 0.7;
@@ -33,15 +33,15 @@ void AddAnalysisTaskPP(UInt_t offlineTriggerMask, TString description, TString s
 	selections->Add(new AliPP13EfficiencySelectionMC("PhysEffPlain", "Physics efficiency for neutral particles, no nonlinearity", cuts_pi0, &mc_weights_only));
 
 	selections->Add(new AliPP13NonlinearitySelection("PhysNonlin", "Physics nonlinearity for neutral particles", cuts_pi0, &mc_weights, kTRUE));
-	selections->Add(new AliPP13NonlinearitySelection("PhysNonlinRaw", "Raw nonlinearity for neutral particles", cuts_pi0, &mc_weights_only, kTRUE));
+	// selections->Add(new AliPP13NonlinearitySelection("PhysNonlinRaw", "Raw nonlinearity for neutral particles", cuts_pi0, &mc_weights_only, kTRUE));
 
-	selections->Add(new AliPP13NonlinearityScanSelection("PhysNonlinScan", "Physics efficiency for neutral particles", cuts_pi0, &mc_weights));
-	selections->Add(new AliPP13MesonSelectionMC("MCStudy", "MC Selection with timing cut", cuts_pi0, &mc_weights));	
+	// selections->Add(new AliPP13NonlinearityScanSelection("PhysNonlinScan", "Physics efficiency for neutral particles", cuts_pi0, &mc_weights));
+	// selections->Add(new AliPP13MesonSelectionMC("MCStudy", "MC Selection with timing cut", cuts_pi0, &mc_weights));
 
 	// Setup task
 	AliAnalysisTaskPP13 * task = new AliAnalysisTaskPP13("PhosProtons", selections);
 
-	if ( !badmap.IsNull() ) 
+	if ( !badmap.IsNull() )
 		task->SetBadMap(badmap);
 
 	// task->GetSelections()->Add
