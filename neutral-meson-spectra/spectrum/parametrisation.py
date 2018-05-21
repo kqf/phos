efa7f57ad45677f01c39c14175834e823769821f
Iohan Petrov <petrov.knu@gmail.com>
2018-05-21 15:32:14 +0200

    Clean the spectrum interface, remove unnecessary imports

--- neutral-meson-spectra/spectrum/parametrisation.py
+++ neutral-meson-spectra/spectrum/parametrisation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
+
 import ROOT
-from broot import BROOT as br
+
 
 class PeakParametrisation(object):
 
@@ -14,7 +15,8 @@ class PeakParametrisation(object):
         # Initiate signal function
         self.signal = self.form_fitting_function(funcname)
         self.background = self.form_background()
-        formula = "{0} + {1}".format(self.signal.GetName(), self.background.GetName())
+        formula = "{0} + {1}".format(self.signal.GetName(),
+                                     self.background.GetName())
         self.fitfun = ROOT.TF1("fitfun", formula, *self.opt.fit_range)
         self.fitfun.SetNpx(1000)
 
@@ -39,19 +41,18 @@ class PeakParametrisation(object):
         self._set_background_parameters(self.fitfun, self.background)
         return self.fitfun, self.background
 
-
     def preliminary_fit(self, hist, fitfun):
         # make a preliminary fit to estimate parameters
         ff = ROOT.TF1("fastfit", "gaus(0) + [3]")
         ff.SetParLimits(0, 0., hist.GetMaximum() * 1.5)
         ff.SetParLimits(1, *self.opt.prel_mass_limits)
         ff.SetParLimits(2, *self.opt.prel_width_limits)
-        ff.SetParameters(hist.GetMaximum()/3., *self.opt.prel_paremeters)
+        ff.SetParameters(hist.GetMaximum() / 3., *self.opt.prel_paremeters)
         hist.Fit(ff, "0QL", "", *self.opt.prel_range)
-        par = [ff.GetParameter(i) if i != 1 else self.opt.fit_mass for i in range(4)]
+        par = [ff.GetParameter(i) if i !=
+               1 else self.opt.fit_mass for i in range(4)]
         fitfun.SetParameters(*par)
         return par
-
 
     def _setup_parameters(self, fitfun, hist):
         fitfun.SetParNames(*self.opt.par_names)
@@ -62,18 +63,16 @@ class PeakParametrisation(object):
         fitfun.SetParLimits(2, *self.opt.fit_width_limits)
         return self.preliminary_fit(hist, fitfun)
 
-    def form_background(self, fname = "background"):
+    def form_background(self, fname="background"):
         if 'pol2' in self.opt.background:
             bf = "[0] + [1]*(x-%.3f) + [2]*(x-%.3f)^2"
             return ROOT.TF1(fname, bf % tuple([self.opt.fit_mass] * 2), *self.opt.fit_range)
 
         if 'pol3' in self.opt.background:
             bf = "[0] + [1]*(x-%.3f) + [2]*(x-%.3f)^2 + [3] * x^(x-%.3f)^3"
-            return ROOT.TF1(fname, bf % tuple([self.opt.fit_mass] * 3) , *self.opt.fit_range)
-
+            return ROOT.TF1(fname, bf % tuple([self.opt.fit_mass] * 3), *self.opt.fit_range)
 
         return ROOT.TF1(fname, self.opt.background + "(0)", *self.opt.fit_range)
-
 
     @staticmethod
     def get(options):
@@ -91,38 +90,35 @@ class CrystalBall(PeakParametrisation):
     def __init__(self, options):
         super(CrystalBall, self).__init__(options)
 
-
-    def form_fitting_function(self, name = 'cball'):
-        alpha, n = '[3]', '[4]' # alpha >= 0, n > 1
+    def form_fitting_function(self, name='cball'):
+        alpha, n = '[3]', '[4]'  # alpha >= 0, n > 1
         a = 'TMath::Exp(-[3] * [3] / 2.) * TMath::Power([4] / [3], [4])'
         b = '[4] / [3] - [3]'
         cff = "(x-[1])/[2] > -%s ? [0]*exp(-(x-[1])*(x-[1])/(2*[2]*[2])) : [0]*%s*(%s-(x-[1])/[2])^(-%s)"
         signal = ROOT.TF1(name, cff % (alpha, a, b, n))
         return signal
 
-
     def _setup_parameters(self, fitfun, hist):
         pars = super(CrystalBall, self)._setup_parameters(fitfun, hist)
         fitfun.SetParLimits(3, *self.opt.cb_alpha_limits)
         fitfun.SetParLimits(4, *self.opt.cb_n_limits)
-        fitfun.SetParameters(*(pars[:3] + [self.opt.cb_alpha, self.opt.cb_n] + pars[3:]))
+        fitfun.SetParameters(
+            *(pars[:3] + [self.opt.cb_alpha, self.opt.cb_n] + pars[3:]))
 
         if not self.opt.relaxed:
             fitfun.FixParameter(3, self.opt.cb_alpha)
             fitfun.FixParameter(4, self.opt.cb_n)
 
 
-
 class Gaus(PeakParametrisation):
     def __init__(self, options):
         super(Gaus, self).__init__(options)
 
-
-    def form_fitting_function(self, name = 'cball'):
+    def form_fitting_function(self, name='cball'):
         signal = ROOT.TF1(name, "gaus(0)")
         return signal
 
-from random import randrange
+
 class CrystalBallEnhanced(CrystalBall):
     def __init__(self, options):
         super(CrystalBallEnhanced, self).__init__(options)
@@ -141,13 +137,14 @@ class CrystalBallEnhanced(CrystalBall):
         npar, nparb = fitfun.GetNpar(), background.GetNpar()
         for i in range(0, npar - nparb):
             fitfun.FixParameter(i,
-                fitfun.GetParameter(i)
-            )
+                                fitfun.GetParameter(i)
+                                )
 
     def form_background(self, fname="background"):
         if 'pol2' in self.opt.background:
             bf = "[0] + [1]*(x-%.3f) + [2]*(x-%.3f)^2"
-            bfunc = ROOT.TF1(fname, bf % tuple([self.opt.fit_mass] * 2), *self.opt.fit_range)
+            bfunc = ROOT.TF1(fname, bf % tuple(
+                [self.opt.fit_mass] * 2), *self.opt.fit_range)
             # bfunc.SetParameters(0, 0, 0)
             bfunc.FixParameter(0, 0)
             bfunc.FixParameter(1, 0)
@@ -156,10 +153,10 @@ class CrystalBallEnhanced(CrystalBall):
 
         if 'pol3' in self.opt.background:
             bf = "[0] + [1]*(x-%.3f) + [2]*(x-%.3f)^2 + [3] * x^(x-%.3f)^3"
-            bfunc = ROOT.TF1(fname, bf % tuple([self.opt.fit_mass] * 3) , *self.opt.fit_range)
+            bfunc = ROOT.TF1(fname, bf % tuple(
+                [self.opt.fit_mass] * 3), *self.opt.fit_range)
             bfunc.SetParameters(0, 0, 0, 0)
             return bfunc
-
 
         bfunc = ROOT.TF1(fname, "pol1(0)", *self.opt.fit_range)
         bfunc.SetParameters(0, 0, 0)
@@ -183,6 +180,7 @@ class CrystalBallEnhanced(CrystalBall):
 
         for i in range(fitfun.GetNpar() - self.background.GetNpar(), fitfun.GetNpar()):
             fitfun.SetParameter(i, 0)
+
 
 class CrystalBallOnly(CrystalBall):
 
@@ -204,13 +202,14 @@ class CrystalBallOnly(CrystalBall):
         npar, nparb = fitfun.GetNpar(), background.GetNpar()
         for i in range(0, npar - nparb):
             fitfun.FixParameter(i,
-                fitfun.GetParameter(i)
-            )
+                                fitfun.GetParameter(i)
+                                )
 
     def form_background(self, fname="background"):
         if 'pol2' in self.opt.background:
             bf = "[0] + [1]*(x-%.3f) + [2]*(x-%.3f)^2"
-            bfunc = ROOT.TF1(fname, bf % tuple([self.opt.fit_mass] * 2), *self.opt.fit_range)
+            bfunc = ROOT.TF1(fname, bf % tuple(
+                [self.opt.fit_mass] * 2), *self.opt.fit_range)
             # bfunc.SetParameters(0, 0, 0)
             bfunc.FixParameter(0, 0)
             bfunc.FixParameter(1, 0)
@@ -219,10 +218,10 @@ class CrystalBallOnly(CrystalBall):
 
         if 'pol3' in self.opt.background:
             bf = "[0] + [1]*(x-%.3f) + [2]*(x-%.3f)^2 + [3] * x^(x-%.3f)^3"
-            bfunc = ROOT.TF1(fname, bf % tuple([self.opt.fit_mass] * 3) , *self.opt.fit_range)
+            bfunc = ROOT.TF1(fname, bf % tuple(
+                [self.opt.fit_mass] * 3), *self.opt.fit_range)
             bfunc.SetParameters(0, 0, 0, 0)
             return bfunc
-
 
         bfunc = ROOT.TF1(fname, "pol1(0)", *self.opt.fit_range)
         bfunc.SetParameters(0, 0, 0)
@@ -243,9 +242,10 @@ class CrystalBallOnly(CrystalBall):
         # if not self.opt.relaxed:
         fitfun.FixParameter(3, self.opt.cb_alpha)
         fitfun.FixParameter(4, self.opt.cb_n)
-
-        for i in range(fitfun.GetNpar() - self.background.GetNpar(), fitfun.GetNpar()):
+        rr = fitfun.GetNpar() - self.background.GetNpar(), fitfun.GetNpar()
+        for i in range(*rr):
             fitfun.SetParameter(i, 0)
+
 
 class CrystalBallOnly(CrystalBall):
     def __init__(self, options):
@@ -266,13 +266,14 @@ class CrystalBallOnly(CrystalBall):
         npar, nparb = fitfun.GetNpar(), background.GetNpar()
         for i in range(0, npar - nparb):
             fitfun.FixParameter(i,
-                fitfun.GetParameter(i)
-            )
+                                fitfun.GetParameter(i)
+                                )
 
     def form_background(self, fname="background"):
         if 'pol2' in self.opt.background:
             bf = "[0] + [1]*(x-%.3f) + [2]*(x-%.3f)^2"
-            bfunc = ROOT.TF1(fname, bf % tuple([self.opt.fit_mass] * 2), *self.opt.fit_range)
+            bfunc = ROOT.TF1(fname, bf % tuple(
+                [self.opt.fit_mass] * 2), *self.opt.fit_range)
             # bfunc.SetParameters(0, 0, 0)
             bfunc.FixParameter(0, 0)
             bfunc.FixParameter(1, 0)
@@ -281,10 +282,10 @@ class CrystalBallOnly(CrystalBall):
 
         if 'pol3' in self.opt.background:
             bf = "[0] + [1]*(x-%.3f) + [2]*(x-%.3f)^2 + [3] * x^(x-%.3f)^3"
-            bfunc = ROOT.TF1(fname, bf % tuple([self.opt.fit_mass] * 3) , *self.opt.fit_range)
+            bfunc = ROOT.TF1(fname, bf % tuple(
+                [self.opt.fit_mass] * 3), *self.opt.fit_range)
             bfunc.SetParameters(0, 0, 0, 0)
             return bfunc
-
 
         bfunc = ROOT.TF1(fname, "pol1(0)", *self.opt.fit_range)
         bfunc.SetParameters(0, 0, 0)
--- neutral-meson-spectra/spectrum/spectrum.py
+++ neutral-meson-spectra/spectrum/spectrum.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python
 
-import ROOT
-import json
 from analysis import Analysis
-from sutils import gcanvas, wait, adjust_canvas, ticks
 from options import Options
-from broot import BROOT as br
 from output import AnalysisOutput
 
 
-ROOT.TH1.AddDirectory(False)
-
 class Spectrum(object):
 
-    def __init__(self, data, options = Options()):
+    def __init__(self, data, options=Options()):
         super(Spectrum, self).__init__()
         self.data = data
         self.model = Analysis(options)
 
     def evaluate(self, label="", loggs=None):
-        local_loggs = AnalysisOutput(self.data.label + label, self.model.options.particle)
+        local_loggs = AnalysisOutput(
+            self.data.label + label, self.model.options.particle)
         output = self.model.transform(self.data, local_loggs)
 
         if loggs:
