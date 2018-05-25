79df4f6892bd46bca23ac47f98b147d0d8b2b329
Iohan Petrov <petrov.knu@gmail.com>
2018-05-25 17:52:33 +0200

    Remove obsolete tests from neutral meson analysis

--- neutral-meson-spectra/analysis/yield/ratio.py
+++ neutral-meson-spectra/analysis/yield/ratio.py
@@ -3,19 +3,16 @@ import unittest
 from spectrum.corrected_yield import YieldRatio
 from spectrum.options import CorrectedYieldOptions
 
-from spectrum.comparator import Comparator
-from spectrum.broot import BROOT as br
 from vault.datavault import DataVault
 
 
 class TestNeutralMesonYieldRatio(unittest.TestCase):
 
-
     def test_yield_ratios(self):
         # Define the inputs and the dataset for #eta mesons
         #
         unified_inputs_eta = {
-            DataVault().input("single #eta updated", "low"):  (0, 6.0),
+            DataVault().input("single #eta updated", "low"): (0, 6.0),
             DataVault().input("single #eta updated", "high"): (6.0, 20)
         }
 
@@ -29,11 +26,10 @@ class TestNeutralMesonYieldRatio(unittest.TestCase):
             unified_inputs=unified_inputs_eta
         )
 
-
         # Define the inputs and the dataset for #pi^{0}
         #
         unified_inputs_pi0 = {
-            DataVault().input("single #pi^{0} corrected weights", "low"):  (0, 7.0),
+            DataVault().input("single #pi^{0} corrected weights", "low"): (0, 7.0),
             DataVault().input("single #pi^{0} corrected weights", "high"): (7.0, 20)
         }
 
@@ -42,12 +38,10 @@ class TestNeutralMesonYieldRatio(unittest.TestCase):
             unified_inputs_pi0
         ]
 
-
         options_pi0 = CorrectedYieldOptions(
             particle="#pi^{0}",
             unified_inputs=unified_inputs_pi0
         )
-
 
         # Make same binning for all neutral mesons
         options_pi0.set_binning(
@@ -59,7 +53,6 @@ class TestNeutralMesonYieldRatio(unittest.TestCase):
             options_eta=options_eta,
             options_pi0=options_pi0
         )
-
 
         estimator.transform(
             [data_eta, data_pi0],
