83c13f66d507f18567aeeb2ba1ecdeb944529c82
Iohan Petrov <petrov.knu@gmail.com>
2018-05-29 23:05:05 +0200

    Update the CBall parameter estimation test

--- neutral-meson-spectra/test/analysis/estimate_cb_parameters.py
+++ neutral-meson-spectra/test/analysis/estimate_cb_parameters.py
@@ -3,80 +3,75 @@ import ROOT
 
 from spectrum.spectrum import Spectrum
 from spectrum.input import Input
-from spectrum.sutils import gcanvas, ticks
+from spectrum.sutils import ticks
 from spectrum.options import Options
 
 
+from spectrum.analysis import Analysis
+from spectrum.transformer import TransformerBase
+from spectrum.pipeline import Pipeline
+from vault.datavault import DataVault
+from spectrum.output import AnalysisOutput
+from spectrum.comparator import Comparator
+
+
+class CbFitOptions(object):
+    def __init__(self, particle):
+        super(CbFitOptions, self).__init__()
+        self.analysis = Options(particle, relaxedcb=True)
+        self.names = ["cball_alpha", "cball_n"]
+        self.functions = [
+            ROOT.TF1("alpha", "pol0", 0, 20),
+            ROOT.TF1("n", "pol0", 0, 20),
+        ]
+
+
+class SelectAndFitHistograms(TransformerBase):
+
+    def __init__(self, options=None):
+        self.options = options
+
+    def transform(self, data, loggs):
+        output = [data._asdict()[n] for n in self.options.names]
+        for hist, func in zip(output, self.options.functions):
+            self._fit_histogram(hist, func)
+        return output
+
+    def _fit_histogram(self, histogram, func):
+        histogram.Fit(func, 'q')
+        histogram.SetLineColor(37)
+        func.SetLineColor(46)
+        print "The scale is", histogram.GetTitle(), func.GetParameter(0)
+
+
+class CballParametersEstimator(TransformerBase):
+    def __init__(self, options, plot=False):
+        super(CballParametersEstimator, self).__init__(plot)
+        self.pipeline = Pipeline([
+            ('analysis', Analysis(options.analysis, plot)),
+            ('cball', SelectAndFitHistograms(options)),
+        ])
+
+
 class CrystalBallParametersPi0(unittest.TestCase):
-    """
-        General idea: Don't use dynamic crystal ball parameters. Use fixed 
-        values (not necessarily the same for) the different p_T slices.
-        Use this code to estimate values of those parameters. Then move these parameters to the code.
-    """
 
     def setUp(self):
-        self.inp = Input('input-data/LHC16.root', 'PhysTender').read()
-        self.particle = 'pi0'
-        spectrum = Spectrum(self.inp, label='relaxedcb parameters', mode='q', options=Options(relaxedcb=True, particle=self.particle))
-        self.alpha, self.n = spectrum.evaluate()[-2:]
+        self.particle = "#pi^{0}"
 
-
-    def fitConstant(self, hist, ofile):
-        canvas = ROOT.TCanvas('cbfit', 'cbfit', 128 * 6 / 2, 96 * 6)
-        ticks(canvas)
-
-        hist.Fit('pol0', 'q')
-        hist.SetLineColor(37)
-        function = hist.GetFunction('pol0')
-        function.SetLineColor(46)
-
-        print hist.GetTitle(), function.GetParameter(0)
-        function.Draw('same')
-        canvas.Update()
-        canvas.SaveAs(ofile)
-        raw_input('Press enter to continue')
-
-
-    def testConstant(self):
-        self.fitConstant(self.alpha, 'results/cb-alpha-fit.pdf')
-        self.fitConstant(self.n, 'results/cb-n-fit.pdf')
-
-
-    def tearDown(self):
-        """
-            Invoke this method to plot the final result 
-            only after all previous tests.
-
-            Use values of CB parameters estimated by testConstant method
-            to run the comparison.
-        """
-
-        c1 = gcanvas(1./2)
-        g = lambda x, y, z, w: Spectrum(x, label=y, mode=z, options=w).evaluate()
-
-        infile = 'input-data/LHC16.root'
-
-        results = [
-                        g(self.inp, 'fixed cb parameters', 'q', Options(relaxedcb = False, particle=self.particle)),
-                        g(self.inp, 'relaxed cb parameters', 'q', Options(relaxedcb = True, particle=self.particle))
-                  ]
-
-        import spectrum.comparator as cmpr
-        diff = cmpr.Comparator(size=(1./2, 1))
-        diff.compare_set_of_histograms(results)
+    def test_parameters(self):
+        options = CbFitOptions(particle=self.particle)
+        estimator = CballParametersEstimator(options, plot=False)
+        hists = estimator.transform(
+            DataVault().input("data"),
+            loggs=AnalysisOutput(
+                "test cball parameters for %s" % self.particle
+            ),
+        )
+        for h in hists:
+            Comparator().compare(h)
 
 
 class CrystalBallParametersEta(CrystalBallParametersPi0):
-    """
-        General idea: Don't use dynamic crystal ball parameters. Use fixed 
-        values (not necessarily the same for) the different p_T slices.
-        Use this code to estimate values of those parameters. Then move these parameters to the code.
-    """
 
     def setUp(self):
-        self.inp = Input('input-data/LHC16.root', 'EtaTender').read()
-        self.particle = 'eta'
-        spectrum = Spectrum(self.inp, label='relaxedcb parameters', mode='q', options=Options(relaxedcb=True, particle=self.particle))
-        self.alpha, self.n = spectrum.evaluate()[-2:]
-
-
+        self.particle = "#eta"
