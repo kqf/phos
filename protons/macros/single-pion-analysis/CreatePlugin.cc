b3c0b228d3a71cba6aa0ed395b691ca0a59566bd
Oleksandr Kovalenko <oleksandr.kovalenko@cern.ch>
2018-06-10 11:22:42 +0200

    Try nonlinearity scan without per run output

--- protons/macros/single-pion-analysis/CreatePlugin.cc
+++ protons/macros/single-pion-analysis/CreatePlugin.cc
@@ -6,13 +6,15 @@
 using std::cout;
 using std::endl;
 
-AliAnalysisGrid * CreatePlugin(const char * pluginmode, TString period, TString dpart, Bool_t useJDL, Bool_t isMC = kTRUE)
+AliAnalysisAlien * CreatePlugin(const char * pluginmode, TString period, TString dpart, Bool_t useJDL, Bool_t isMC = kTRUE)
 {
 	// Maximal size of the dataset that shouldn't be slitted on two halves
 	Int_t msize = 80;
 
 	// Use default setup for the plugin
-	AliAnalysisGrid * plugin = GetPlugin(pluginmode, period, dpart, useJDL, isMC, msize);
+	AliAnalysisAlien * plugin = GetPlugin(pluginmode, period, dpart, useJDL, isMC, msize);
+	plugin->SetOutputToRunNo(kFALSE);
+	
 	TString dir = period.Contains("_extra") ? period : TString(period, isMC ? 10 : 6); // fancy slicing
 
 	// DataDir = /alice/sim/2017/LHC17i7b2/ , DataPettern = /*/AliAOD.root
