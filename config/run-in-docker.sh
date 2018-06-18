ae8a83343343d744d939717a17dfd9577eabc361
Sasha <q.dev.null@gmail.com>
2018-06-18 18:14:41 +0200

    Update the docker scripts for AliPhysics development

--- config/run-in-docker.sh
+++ config/run-in-docker.sh
@@ -1,6 +1,1 @@
-local_path=$HOME/Study/phos/protons/tasks/analysis-task-pp/
-docker_path=/alice/AliPhysics/PWGGA/PHOSTasks/PHOS_LHC16_pp
-image=aliworker:physics
-
-
-docker run -v $local_path:$docker_path -it $image
+docker run --rm -v $HOME/alice/:/alice -it aliworker:physics
