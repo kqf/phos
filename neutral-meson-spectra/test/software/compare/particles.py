a3e6f621862ce4294059d14ada7cc1dece7630e1
Iohan Petrov <petrov.knu@gmail.com>
2018-06-28 21:35:57 +0200

    Fix the tsallis formula for compare tests

--- neutral-meson-spectra/test/software/compare/particles.py
+++ neutral-meson-spectra/test/software/compare/particles.py
@@ -2,6 +2,7 @@ import ROOT
 import json
 
 import sys
+from vault.formulas import FVault
 
 
 class Particles(object):
@@ -12,7 +13,7 @@ class Particles(object):
     def config(self):
         with open('config/test_particles.json') as f:
             particles = json.load(f)
-        data = [self._spectrum(i, *particles[i]) for i in particles]
+        data = [self._spectrum(i, particles[i]) for i in particles]
 
         for hist in data:
             hist.logy = 1
@@ -24,11 +25,13 @@ class Particles(object):
         return data, not skip
 
     @staticmethod
-    def _spectrum(name, a, b, par):
-        from spectrum.sutils import tsallis
-        function = ROOT.TF1('f' + name, lambda x,
-                            p: tsallis(x, p, a, a), 0.3, 15, 3)
+    def _spectrum(name, par):
+        function = ROOT.TF1('f' + name, FVault().func("tsallis"), 0.3, 15, 5)
         function.SetParameters(*par)
+        function.Draw()
+        canvas = ROOT.gROOT.FindObject('c1')
+        canvas.Update()
+        raw_input('')
         title = '%s p_{T} spectrum; p_{T}, GeV/c; #frac{dN}{dp_{T}}' % name
         histogram = ROOT.TH1F(name + '_spectrum', title, 100, 0.3, 15)
         histogram.FillRandom('f' + name, 1000000)
