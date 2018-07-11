4cc0b3b97096006541712dcd430aeff9624add7e
Sasha <q.dev.null@gmail.com>
2018-07-11 12:24:51 +0200

    Update the kinematics (angle) generator structure: remove nparams

--- misc-tools/kinematics/__main__.py
+++ misc-tools/kinematics/__main__.py
@@ -1,5 +1,34 @@
+import ROOT
 from generator.generator import Analysis
-from generator.generator import AngleSelection
+from math import pi 
+
+
+class AngleSelection(object):
+    def __init__(self, ofile="output.root"):
+        super(AngleSelection, self).__init__()
+        self.ofile = ofile
+        title = "Opening angle between two photons; #theta, rad"
+        self.angle = ROOT.TH1F("hAngle", title, 10000, 0, pi / 2)
+        title = "Opening angle between two photons vs momentum"
+        title += "; #theta, rad; p, GeV/c"
+        self.angle_p = ROOT.TH2F(
+            "hAngleMomentum", title, 5000, 0, pi / 2, 1000, 15, 100)
+        title = "Initial #pi^{0} meson momentum (tsallis); p, GeV/c"
+        self.original = ROOT.TH1F("hP", title, 1000, 15, 100)
+
+    def transform(self, particles):
+        (first, second), original = particles
+        angle = first.Angle(second.Vect())
+        self.angle.Fill(angle)
+        self.angle_p.Fill(angle, original.P())
+        self.original.Fill(original.P())
+
+    def write(self):
+        ofile = ROOT.TFile(self.ofile, "recreate")
+        self.angle.Write()
+        self.angle_p.Write()
+        self.original.Write()
+        ofile.Close()
 
 
 def main():
--- misc-tools/kinematics/generator/generator.py
+++ misc-tools/kinematics/generator/generator.py
@@ -1,6 +1,6 @@
 import array
 import json
-from math import pi, sqrt
+from math import sqrt
 
 import ROOT
 import tqdm
@@ -52,30 +52,3 @@ class Analysis(object):
             particles = self.generator.event()
             for selection in selections:
                 selection.transform(particles)
-
-
-class AngleSelection(object):
-    def __init__(self, ofile="output.root"):
-        super(AngleSelection, self).__init__()
-        self.ofile = ofile
-        title = "Opening angle between two photons; #theta, rad"
-        self.angle = ROOT.TH1F("hAngle", title, 10000, 0, pi / 2)
-        title = "Opening angle between two photons vs momentum"
-        title += "; #theta, rad; p, GeV/c"
-        self.angle_p = ROOT.TH2F(
-            "hAngleMomentum", title, 5000, 0, pi / 2, 1000, 15, 100)
-        self.original = ROOT.TH1F("hP", "", 1000, 15, 100)
-
-    def transform(self, particles):
-        (first, second), original = particles
-        angle = first.Angle(second.Vect())
-        self.angle.Fill(angle)
-        self.angle_p.Fill(angle, original.P())
-        self.original.Fill(original.P())
-
-    def write(self):
-        ofile = ROOT.TFile(self.ofile, "recreate")
-        self.angle.Write()
-        self.angle_p.Write()
-        self.original.Write()
-        ofile.Close()
