34a6423a0a8a84c64ca729db7892a22b8c75ca2c
Iohan Petrov <petrov.knu@gmail.com>
2018-07-21 20:54:10 +0200

    Add the dataset with the updated GE for the nonlinearity scan

--- neutral-meson-spectra/test/systematics/nonlinearity_scan.py
+++ neutral-meson-spectra/test/systematics/nonlinearity_scan.py
@@ -29,9 +29,9 @@ class ScanNonlinearities(unittest.TestCase):
     def setUp(self):
         self.nbins = 9
 
-    @unittest.skip('')
+    # @unittest.skip('')
     def test(self):
-        prod = "single #pi^{0} scan nonlinearity3"
+        prod = "single #pi^{0} scan nonlinearity6"
         histnames = form_histnames(self.nbins)
         low = DataVault().input(prod, "low", inputs=histnames)
         high = DataVault().input(prod, "high", inputs=histnames)
@@ -59,7 +59,7 @@ class ScanNonlinearities(unittest.TestCase):
         ofile.Close()
         Comparator().compare(chi2ndf)
 
-    # @unittest.skip('')
+    @unittest.skip('')
     def test_draw_most_optimal_nonlinearity(self):
         production = "single #pi^{0} scan nonlinearity3"
         minimum_index = 51
