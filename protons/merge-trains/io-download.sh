4114ded6e72e54f3c5d01db8b22750f7749ed0fd
Iohan Petrov <petrov.knu@gmail.com>
2018-08-30 11:36:01 +0200

    Add period names to the scripts for train merging

--- protons/merge-trains/io-download.sh
+++ protons/merge-trains/io-download.sh
@@ -1,4 +1,4 @@
-# train=/alice/cern.ch/user/a/alitrain/PWGGA/GA_pp_AOD/
+# MC trains here
 train=/alice/cern.ch/user/a/alitrain/PWGGA/GA_pp_MC_AOD/
 # run=326_20180209-1536_child_
 # run=334_20180217-1427_child_
@@ -8,9 +8,17 @@ train=/alice/cern.ch/user/a/alitrain/PWGGA/GA_pp_MC_AOD/
 # run=963_20180809-1020_child_
 # run=971_20180815-1556_child_
 run=980_20180827-1016_child_
-runlist=merge_runlist_1
-# outdir=results/data/LHC17/iteration0
+# Dataset: LHC17_PYT8_13TeV_anchLHC16x_AOD
+declare -A children=([1]=1 [2]=1 [3]=1 [4]=1 [5]=1 [6]=1 [7]=1 [8]=1 [9]=1 [10]=1)
+declare -A names=([1]=LHC17f6 [2]=LHC17f9 [3]=LHC17d17 [4]=LHC17f5 [5]=LHC17d3 [6]=LHC17e5 [7]=LHC17d20a1 [8]=LHC17d20a2 [9]=LHC17d16 [10]=LHC17d18)
 
+# Data trains here
+# train=/alice/cern.ch/user/a/alitrain/PWGGA/GA_pp_AOD/
+# run=444_20180827-1128_child_
+# This scheme is valid for LHC16 data.
+# fill the map in the following way ([child number]=runlist number)
+# declare -A children=([2]=3 [3]=3 [4]=3 [5]=3 [6]=4 [7]=3 [8]=3 [9]=3)
+# declare -A names=([2]=LHC16g [3]=LHC16h [4]=LHC16i [5]=LHC16j [6]=LHC16k [7]=LHC16l [8]=LHC16o [9]=LHC16p)
 
 function download_file() {
 	alien_cp alien:/$1/AnalysisResults.root $2
@@ -22,14 +30,16 @@ function show_dir() {
 
 function download() {
 	# NB: Always check runlists awailable for merge
-	for i in {1..10}; do
+	for i in ${!children[@]}; do
+		runlist=merge_runlist_${children[$i]}
+		oname=${names[$i]}.root
 		echo "_________________"
 		echo "                 "
 		echo $train$run$i/$runlist
 		{
-			download_file $train$run$i/$runlist child$i.root
+			download_file $train$run$i/$runlist $oname 
 		} || {
-			download_file $train$run$i/merge child$i.root
+			download_file $train$run$i/merge $oname 
 		}
 	done
 }
--- protons/merge-trains/io-upload.sh
+++ protons/merge-trains/io-upload.sh
@@ -1,11 +1,13 @@
-outdir=results/mc/pythia/ep_ratio/
+# outdir=results/data/LHC16/
+outdir=results/mc/pythia/ep_ratio
+# ofile=LHC16.root
 ofile=pythia8.root
 function upload() {
-	hadd $ofile child*.root
+	hadd $ofile *.root
 	alien_mkdir -p $ALIEN_HOME/$outdir/
 	alien_cp -n $ofile alien:$ALIEN_HOME/$outdir/$ofile@ALICE::GSI::SE2
 
-	for i in child*.root; do
+	for i in *.root; do
 		alien_cp -n $i alien:$ALIEN_HOME/$outdir/$i@ALICE::GSI::SE2
 	done
 }
