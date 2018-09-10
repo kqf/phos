d0f2055754a0378428b8544c9e31129923665608
Sasha <q.dev.null@gmail.com>
2018-09-10 22:40:00 +0200

    Fix the steering macros of E/p ratio

--- protons/macros/e2p-ratio/CreatePlugin.cc
+++ protons/macros/e2p-ratio/CreatePlugin.cc
@@ -1,4 +1,4 @@
-#include "../setup/plugin.h"	
+#include "../../setup/alien.h"
 #include "iostream"
 
 using std::cout;
@@ -7,14 +7,14 @@ using std::endl;
 AliAnalysisGrid * CreatePlugin(const char * pluginmode, TString period, TString dpart, Bool_t useJDL, Bool_t isMC)
 {
 	// Maximal size of the dataset that shouldn't be slitted on two halves
-	Int_t msize = 80;	
+	Int_t msize = 80;
 
 	// Use default setup for the plugin
 	AliAnalysisGrid * plugin = GetPlugin(pluginmode, period, dpart, useJDL, isMC, msize);
 
 	// Extract period and reconstruction pass
 	TString dir = period.Contains("_extra") ? period : TString(period, isMC ? 10 : 6); // fancy slicing
-	
+
 	TString reconstruction(period);
 	reconstruction.ReplaceAll(dir + (reconstruction.Contains(dir + "-") ? "-" : "") , "");
 	reconstruction.ReplaceAll("-", "_");
@@ -27,7 +27,7 @@ AliAnalysisGrid * CreatePlugin(const char * pluginmode, TString period, TString 
 	plugin->SetDataPattern("/" + reconstruction + datasuffix + "*/AliAOD.root");
 	cout << "Data pattern " << "/" + reconstruction + "/*.*/AliAOD.root" << endl;
 
-	plugin->SetFileForTestMode ( "filesmc.txt" );
+	plugin->SetFileForTestMode("../../datasets/filesmc.txt");
     //plugin->SetUseSubmitPolicy();
 	return plugin;
 }
--- protons/macros/e2p-ratio/run.C
+++ protons/macros/e2p-ratio/run.C
@@ -1,27 +1,15 @@
-#include "../setup/environment.h"
+#include "../../setup/environment.h"
 
 void run(TString period, const char * runmode = "local", const char * pluginmode = "test", TString dpart = "first", Bool_t isMC = kFALSE, Bool_t useJDL = kTRUE)
 {
     SetupEnvironment();
 
     gROOT->LoadMacro("CreatePlugin.cc+");
-    AliAnalysisGrid * alienHandler = CreatePlugin(pluginmode, period, dpart, useJDL, isMC);
-
-    if (!alienHandler) return;
-
+    AliAnalysisGrid * alien = CreatePlugin(pluginmode, period, dpart, useJDL, isMC);
     AliAnalysisManager * manager  = new AliAnalysisManager("PHOS_PP");
-    AliESDInputHandler * esdH = new AliESDInputHandler();
-    AliAODInputHandler * aodH = new AliAODInputHandler();
+    AliAODInputHandler * aod = new AliAODInputHandler();
 
-    if (isMC)
-    {
-        esdH->SetReadFriends( isMC );
-        esdH->SetNeedField();
-        // manager->SetInputEventHandler( esdH );
-
-    }
-    // manager->SetInputEventHandler( esdH );
-    manager->SetInputEventHandler(aodH);
+    manager->SetInputEventHandler(aod);
 
     if ( isMC )
     {
@@ -31,15 +19,15 @@ void run(TString period, const char * runmode = "local", const char * pluginmode
     }
 
     // Connect plug-in to the analysis manager
-    manager->SetGridHandler(alienHandler);
+    manager->SetGridHandler(alien);
 
     gROOT->LoadMacro ("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C");
 
     Bool_t enablePileupCuts = kTRUE;
     AddTaskPhysicsSelection (isMC, enablePileupCuts);  //false for data, true for MC
 
-    TString files = "";
-    TString pref =  isMC ? "MC": "";
+    gROOT->LoadMacro("AddAnalysisTaskPP.C");
+    TString pref =  isMC ? "MC" : "";
 
     gROOT->LoadMacro("$ALICE_PHYSICS/PWGGA/PHOSTasks/PHOS_PbPb/AddAODPHOSTender.C");
 
@@ -47,23 +35,35 @@ void run(TString period, const char * runmode = "local", const char * pluginmode
     AliPHOSTenderTask * tenderPHOS = AddAODPHOSTender("PHOSTenderTask", "PHOStender", tenderOption, 1, isMC);
 
     AliPHOSTenderSupply * PHOSSupply = tenderPHOS->GetPHOSTenderSupply();
-    PHOSSupply->ForceUsingBadMap("../datasets/BadMap_LHC16-updated.root");
+    PHOSSupply->ForceUsingBadMap("../../datasets/BadMap_LHC16-updated.root");
+
+    gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C");
+    AliAnalysisTaskPIDResponse *taskPID = AddTaskPIDResponse(
+        isMC, 
+        kTRUE,
+        kTRUE, 
+        1,          // reco pass
+        kFALSE, 
+        "",
+        kTRUE,
+        kFALSE,
+        1           // reco pass
+    );
+
+    if (isMC)
+    {
+        // Important: Keep track of this variable
+        // ZS threshold in unit of GeV
+        Double_t zs_threshold = 0.020;
+        PHOSSupply->ApplyZeroSuppression(zs_threshold);
+    }
 
 
-    gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C"); 
-    AliAnalysisTaskPIDResponse *PIDResponse = AddTaskPIDResponse(isMC);
-    PIDResponse->SelectCollisionCandidates(AliVEvent::kINT7);
+    gROOT->LoadMacro("../../setup/values_for_dataset.h+");
+    std::vector<Int_t> cells;
+    values_for_dataset(cells, "BadCells_LHC16", "../datasets/");
 
-    gROOT->LoadMacro("$ALICE_PHYSICS/PWGGA/PHOSTasks/PHOS_EpRatio/AddTaskPHOSEpRatio.C");
-    AddTaskPHOSEpRatio(isMC);
-
-    // Important: Keep track of this variable
-    // ZS threshold in unit of GeV  
-    Double_t zs_threshold = 0.020;
-    PHOSSupply->ApplyZeroSuppression(zs_threshold); 
-
-
-    TString msg = "## Updated parameters for nonlinearity, 20 MeV Zero Supression ";
+    TString msg = "## Real data, no TOF cut efficiency.";
 
     if (tenderOption)
     {
@@ -71,12 +71,18 @@ void run(TString period, const char * runmode = "local", const char * pluginmode
         msg += tenderOption;
     }
 
+    Bool_t isTest = TString(pluginmode).Contains("test");
+    AddAnalysisTaskPP(AliVEvent::kINT7, period + pref + msg, "", "", cells, isMC, isTest);
+
+    gROOT->LoadMacro("$ALICE_PHYSICS/PWGGA/PHOSTasks/PHOS_EpRatio/AddTaskPHOSEpRatio.C");
+    AddTaskPHOSEpRatio(isMC);
+
+
     if ( !manager->InitAnalysis( ) ) return;
     manager->PrintStatus();
 
 
-    cout << "Downloading files " << files << endl;
-    alienHandler->SetOutputFiles(AliAnalysisManager::GetCommonFileName());
+    alien->SetOutputFiles("AnalysisResults.root");
     manager->StartAnalysis (runmode);
     gObjectTable->Print( );
 }
