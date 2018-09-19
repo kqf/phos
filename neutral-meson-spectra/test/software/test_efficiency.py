9f5413457d0c7bb2ca7c9897b2b4a4447d903c80
Sasha <q.dev.null@gmail.com>
2018-09-19 19:53:16 +0200

    Remove unified inputs from the efficiency calculations

--- neutral-meson-spectra/test/software/test_efficiency.py
+++ neutral-meson-spectra/test/software/test_efficiency.py
@@ -21,15 +21,12 @@ class TestEfficiency(unittest.TestCase):
         self.assertGreater(efficiency.GetEntries(), 0)
 
     def test_composite(self):
-        unified_inputs = {
-            DataVault().input("single #pi^{0}", "low"): (0.0, 8.0),
-            DataVault().input("single #pi^{0}", "high"): (4.0, 20.0)
-        }
-
         estimator = Efficiency(CompositeEfficiencyOptions("#pi^{0}"))
-
         efficiency = estimator.transform(
-            unified_inputs.keys(),
-            "testin composite efficiency"
+            [
+                DataVault().input("single #pi^{0}", "low"),
+                DataVault().input("single #pi^{0}", "high"),
+            ],
+            "testing composite efficiency"
         )
         self.assertGreater(efficiency.GetEntries(), 0)
