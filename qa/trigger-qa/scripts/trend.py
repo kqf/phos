0873ae66836545ce68e2b41327eb47f3d62cc6c0
Sasha <q.dev.null@gmail.com>
2018-11-18 14:56:52 +0100

    Rewrite trend plots wiht pipelines + pandas

--- qa/trigger-qa/scripts/trend.py
+++ qa/trigger-qa/scripts/trend.py
@@ -1,67 +1,147 @@
 import ROOT
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import make_pipeline
 import pandas as pd
 import plotting
-pd.set_option('display.expand_frame_repr', False)
-
+ROOT.TH1.AddDirectory(False)
 filepath = "../../../neutral-meson-spectra/" \
     "input-data/data/LHC16/trigger_qa/iteration2/LHC16g-pass1.root"
 
 
-def remove_empty_runs(hist):
-    runs, counts = [], []
-    for i in range(1, hist.GetNbinsX() + 1):
-        if not hist.GetBinContent(i) > 0:
-            continue
-        runs.append(hist.GetBinCenter(i))
-        counts.append(hist.GetBinContent(i))
-    reduced = ROOT.TH1F(
-        hist.GetName() + "_reduced",
-        hist.GetTitle(),
-        len(runs), 0, len(runs))
-
-    for i, (r, c) in enumerate(zip(runs, counts)):
-        reduced.SetBinContent(i + 1, c)
-        reduced.GetXaxis().SetBinLabel(i + 1, str(int(r)))
-    return reduced
+def from_list(histname, lst, get=False):
+    def reader(index):
+        if get:
+            return lst.Get("{}{}".format(histname, str(index)))
+        return lst.FindObject("{}{}".format(histname, str(index)))
+    return reader
 
 
-def average(hists):
-    total = hists[0].Clone("average")
-    total.Reset()
-    for hist in hists:
-        total.Add(hist)
-    total.Scale(1. / len(hists))
-    return total
+class HistReader(BaseEstimator, TransformerMixin):
+    def __init__(self, in_col, out_col,
+                 filepath, pattern, listpath=None):
+        self.in_col = in_col
+        self.out_col = out_col
+        self.filepath = filepath
+        self.pattern = pattern
+        self.listpath = listpath
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        infile = ROOT.TFile(self.filepath)
+        lst = infile.Get(self.listpath) if self.listpath else infile
+        if self.in_col == "index":
+            X[self.out_col] = X.index.map(from_list(self.pattern, lst, True))
+            return X
+        X[self.out_col] = X[self.in_col].apply(from_list(self.pattern, lst))
+        return X
 
 
-def scale_for_acceptance(hists, badmap_fname="BadMap_LHC16-updated.root"):
-    if not badmap_fname:
-        return hists
-    badmap = ROOT.TFile(badmap_fname)
-    for i, hist in enumerate(hists):
-        mod = i + 1
-        sm = badmap.Get("PHOS_BadMap_mod{}".format(mod))
-        bad_channels = sm.GetEntries()
-        all_channels = sm.GetNbinsX() * sm.GetNbinsY()
-        hist.Scale(1. / (all_channels - bad_channels))
-    return hist
+class EmptyBinRemover(BaseEstimator, TransformerMixin):
+    def __init__(self, in_col, out_col):
+        super(EmptyBinRemover, self).__init__()
+        self.in_col = in_col
+        self.out_col = out_col
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X[self.out_col] = X[self.in_col].apply(self.remove_empty_runs)
+        return X
+
+    @staticmethod
+    def remove_empty_runs(hist):
+        runs, counts = [], []
+        for i in range(1, hist.GetNbinsX() + 1):
+            if not hist.GetBinContent(i) > 0:
+                continue
+            runs.append(hist.GetBinCenter(i))
+            counts.append(hist.GetBinContent(i))
+        reduced = ROOT.TH1F(
+            hist.GetName() + "_reduced",
+            hist.GetTitle(),
+            len(runs), 0, len(runs))
+
+        for i, (r, c) in enumerate(zip(runs, counts)):
+            reduced.SetBinContent(i + 1, c)
+            reduced.GetXaxis().SetBinLabel(i + 1, str(int(r)))
+        return reduced
+
+
+class AcceptanceScaler(BaseEstimator, TransformerMixin):
+
+    def __init__(self, in_cols, out_col):
+        self.in_cols = in_cols
+        self.out_col = out_col
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X[self.out_col] = X[self.in_cols].apply(
+            lambda x: self.scale(*x), axis=1)
+        return X
+
+    @staticmethod
+    def scale(hist, badmap):
+        if not badmap:
+            print "No badmap found for ", hist.GetName()
+            return hist
+        bad_channels = badmap.GetEntries()
+        all_channels = badmap.GetNbinsX() * badmap.GetNbinsY()
+
+        scaled = hist.Clone("{}_acceptance".format(hist.GetName()))
+        scaled.SetTitle("{} scaled for acceptance".format(hist.GetTitle()))
+        scaled.Scale(1. / (all_channels - bad_channels))
+        return scaled
+
+
+class AverageScaler(BaseEstimator, TransformerMixin):
+    def __init__(self, in_col, out_col, info=None):
+        self.in_col = in_col
+        self.out_col = out_col
+        self.info = info
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        average = self.average(X[self.in_col])
+        payload = pd.DataFrame(
+            [{
+                self.out_col: average,
+                "name": "average"
+            }],
+            index=["average"])
+        return X.append(payload, sort=True)
+
+    @staticmethod
+    def average(hists):
+        total = hists[0].Clone("average")
+        total.Reset()
+        for hist in hists:
+            total.Add(hist)
+        total.Scale(1. / len(hists))
+        return total
 
 
 def main(nmodules=4):
-    l0 = ROOT.TFile(filepath).Get("PHOSTriggerQAResultsL0")
-    outputs = pd.DataFrame(index=map("SM{}".format, range(1, nmodules + 1)))
-    histname = "hRunMatchedTriggers"
-    outputs[histname] = [
-        l0.FindObject("{}{}".format(histname, str(i)))
-        for i in outputs.index
-    ]
-    outputs["cleaned"] = outputs[histname].apply(remove_empty_runs)
-    scale_for_acceptance(outputs["cleaned"].values)
-    outputs = outputs.append(pd.DataFrame([{
-        "cleaned": average(outputs["cleaned"].values)
-    }], index=["average"]), sort=True)
+    badmap_fname = "BadMap_LHC16-updated.root"
+    hist, lst = "hRunMatchedTriggers", "PHOSTriggerQAResultsL0"
+    analysis = make_pipeline(
+        HistReader("name", "raw", filepath, hist, lst),
+        HistReader("index", "badmap", badmap_fname, "PHOS_BadMap_mod"),
+        EmptyBinRemover("raw", "cleaned"),
+        AcceptanceScaler(["cleaned", "badmap"], "scaled"),
+        AverageScaler("scaled", "scaled")
+    )
 
-    plotting.plot(outputs["cleaned"].values, outputs.index)
+    outputs = pd.DataFrame(index=map(str, range(1, nmodules + 1)))
+    outputs["name"] = outputs.index.map("SM{}".format)
+    outputs = analysis.transform(outputs)
+    plotting.plot(outputs["scaled"], outputs["name"])
 
 
 if __name__ == '__main__':
