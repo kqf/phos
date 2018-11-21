607e56c3410ec17634c23b7272c4c2e3137bfc94
Sasha <q.dev.null@gmail.com>
2018-11-21 18:39:35 +0100

    Add multirun dataset for memory tests

--- protons/datasets/download-from-grid.sh
+++ protons/datasets/download-from-grid.sh
@@ -1,7 +1,12 @@
-export datapath=2016/LHC16k/000257682/pass1/AOD/
 
 # Run this code inside /data folder
-for i in {001..010}; do
-	mkdir -p $datapath/$i/
-	alien_cp alien://alice/data/$datapath/$i/AliAOD.root $datapath/$i/AliAOD.root
+# for i in {001..010}; do
+# 	mkdir -p $datapath/$i/
+# 	alien_cp alien://alice/data/$datapath/$i/AliAOD.root $datapath/$i/AliAOD.root
+# done
+
+for run in 257684 257685 257687 257688 257689 257692 257697 257724 257725; do
+	datapath=2016/LHC16k/000$run/pass1/AOD/001/
+	mkdir -p $datapath/
+	alien_cp alien://alice/data/$datapath/AliAOD.root $datapath/AliAOD.root
 done
