29b462ae522d16293eb2d5bed6f8b15d15ad810a
Sasha <q.dev.null@gmail.com>
2018-12-24 14:50:30 +0100

    Separate input data and estimators for yield extraction

--- neutral-meson-spectra/tests/software/test_cyield.py
+++ neutral-meson-spectra/tests/software/test_cyield.py
@@ -1,6 +1,6 @@
-import unittest
 import pytest
 
+from lazy_object_proxy import Proxy
 from spectrum.options import CompositeCorrectedYieldOptions
 from spectrum.options import CorrectedYieldOptions
 from spectrum.corrected_yield import CorrectedYield
@@ -8,46 +8,40 @@ from spectrum.corrected_yield import CorrectedYield
 from vault.datavault import DataVault
 from tools.feeddown import data_feeddown
 
+YIELD_DATA = Proxy(
+    lambda: (
+        DataVault().input("data", histname="MassPtSM0"),
+        data_feeddown(),
+    )
+)
 
-class TestCorrectedYieldInterface(unittest.TestCase):
+PYTHIA8_DATA = Proxy(
+    lambda: (
+        YIELD_DATA,
+        DataVault().input("pythia8"),
+    )
+)
+SPMC_DATA = Proxy(
+    lambda: (
+        YIELD_DATA, (
+            DataVault().input("single #pi^{0}", "low", "PhysEff"),
+            DataVault().input("single #pi^{0}", "high", "PhysEff"),
+        )
+    )
+)
 
-    @pytest.mark.onlylocal
-    def test_yield(self):
-        yield_data = (
-            DataVault().input("data", histname="MassPtSM0"),
-            data_feeddown(),
-        )
-        data = (
-            yield_data,
-            DataVault().input("pythia8"),
-        )
 
-        estimator = CorrectedYield(
-            CorrectedYieldOptions(
-                particle="#pi^{0}"
-            )
-        )
-        estimator.transform(data, {})
+@pytest.mark.onlylocal
+def test_corrected_yield():
+    estimator = CorrectedYield(
+        CorrectedYieldOptions(particle="#pi^{0}")
+    )
+    estimator.transform(PYTHIA8_DATA, {})
 
-    @pytest.mark.onlylocal
-    def test_composite_yield(self):
-        production = "single #pi^{0}"
-        spmc_inputs = (
-            DataVault().input(production, "low", "PhysEff"),
-            DataVault().input(production, "high", "PhysEff"),
-        )
-        yield_data = (
-            DataVault().input("data", histname="MassPtSM0"),
-            data_feeddown(),
-        )
-        data = (
-            yield_data,
-            spmc_inputs
-        )
 
-        estimator = CorrectedYield(
-            CompositeCorrectedYieldOptions(
-                particle="#pi^{0}"
-            )
-        )
-        estimator.transform(data, {})
+@pytest.mark.onlylocal
+def test_composite_yield():
+    estimator = CorrectedYield(
+        CompositeCorrectedYieldOptions(particle="#pi^{0}")
+    )
+    estimator.transform(SPMC_DATA, {})
