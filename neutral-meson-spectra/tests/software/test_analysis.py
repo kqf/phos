002e75124dea4f6fee923604c4c1529ea2169c2d
Sasha <q.dev.null@gmail.com>
2018-12-24 14:43:34 +0100

    Remove unittest from analysis check

--- neutral-meson-spectra/tests/software/test_analysis.py
+++ neutral-meson-spectra/tests/software/test_analysis.py
@@ -1,40 +1,37 @@
-import unittest
 import pytest
 
+from lazy_object_proxy import Proxy
 from spectrum.output import AnalysisOutput
 from spectrum.options import Options, CompositeOptions
 from spectrum.analysis import Analysis
 from vault.datavault import DataVault
 
 # from spectrum.comparator import Comparator
+INPUT_DATA = Proxy(
+    lambda: DataVault().input("data", histname="MassPtSM0")
+)
+
+SPMC_DATA = Proxy(
+    lambda: (
+        DataVault().input("single #pi^{0}", "low", "PhysEff"),
+        DataVault().input("single #pi^{0}", "high", "PhysEff"),
+    )
+)
 
 
-class TestAnalysis(unittest.TestCase):
+@pytest.mark.onlylocal
+def test_simple():
+    analysis = Analysis(Options(particle="#eta"))
+    loggs = AnalysisOutput("test the single analysis")
+    output = analysis.transform(INPUT_DATA, loggs=loggs)
+    # loggs.plot()
+    assert len(output) > 0
 
-    @pytest.mark.onlylocal
-    def test_simple(self):
-        analysis = Analysis(Options(particle="#eta"))
 
-        loggs = AnalysisOutput("test the single analysis")
-        output = analysis.transform(
-            DataVault().input("data", histname="MassPtSM0"),
-            loggs=loggs
-        )
-        loggs.plot()
-        self.assertGreater(len(output), 0)
-
-    @unittest.skip('')
-    @pytest.mark.onlylocal
-    def test_composite(self):
-        analysis = Analysis(CompositeOptions("#pi^{0}"))
-
-        loggs = AnalysisOutput("test the composite analysis")
-        output = analysis.transform(
-            (
-                DataVault().input("single #pi^{0}", "low", "PhysEff"),
-                DataVault().input("single #pi^{0}", "high", "PhysEff"),
-            ),
-            loggs=loggs
-        )
-        loggs.plot()
-        self.assertGreater(len(output), 0)
+@pytest.mark.onlylocal
+def test_composite():
+    analysis = Analysis(CompositeOptions("#pi^{0}"))
+    loggs = AnalysisOutput("test the composite analysis")
+    output = analysis.transform(SPMC_DATA, loggs=loggs)
+    # loggs.plot()
+    assert len(output) > 0
