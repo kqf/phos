c137970dd7a6da0506f34580a40f3aa3ce0e3602
Sasha <q.dev.null@gmail.com>
2018-12-27 15:35:02 +0100

    Compare published results on neutral pion production

---
+++ neutral-meson-spectra/analysis/yield/energies.py
@@ -0,0 +1,32 @@
+import json
+from spectrum.broot import BROOT as br
+from spectrum.pipeline import TransformerBase
+from spectrum.pipeline import ComparePipeline
+from spectrum.output import AnalysisOutput
+
+
+class HepdataInput(TransformerBase):
+    def __init__(self, table_name="Table 1", histname="Hist1D_y1", plot=False):
+        super(HepdataInput, self).__init__(plot)
+        self.table_name = table_name
+        self.histname = histname
+
+    def transform(self, link, loggs):
+        filename = "{}.root".format(link)
+        br.io.hepdata(link, filename)
+        return br.io.read(filename, "Table 1", "Hist1D_y1")
+
+
+def define_datasets():
+    with open("config/different-energies.json") as f:
+        data = json.load(f)
+    labels, links = zip(*data.iteritems())
+    return labels, links
+
+
+def test_downloads_from_hepdata():
+    labels, links = define_datasets()
+    previous = [(l, HepdataInput()) for l in labels]
+    loggs = AnalysisOutput("compare yields")
+    ComparePipeline(previous, plot=True).transform(links, loggs=loggs)
+    loggs.plot()
