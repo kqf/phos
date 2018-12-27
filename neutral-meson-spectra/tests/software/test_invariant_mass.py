c87e9daf7e1a70bd2897bb06783b864ea4229e79
Sasha <q.dev.null@gmail.com>
2018-12-27 12:40:15 +0100

    Replace outdated invariant mass interface from checks

--- neutral-meson-spectra/tests/software/test_invariant_mass.py
+++ neutral-meson-spectra/tests/software/test_invariant_mass.py
@@ -1,11 +1,8 @@
-
 import sys
-import unittest
 import pytest
 
-from spectrum.sutils import wait
+from lazy_object_proxy import Proxy
 from spectrum.options import Options
-from spectrum.invariantmass import InvariantMass
 from spectrum.processing import DataSlicer, MassFitter, RangeEstimator
 from spectrum.processing import InvariantMassExtractor
 from spectrum.output import AnalysisOutput
@@ -14,48 +11,33 @@ from spectrum.pipeline import Pipeline
 from vault.datavault import DataVault
 
 
-class TestInvariantMass(unittest.TestCase):
+DATASET = Proxy(
+    lambda: DataVault().input("data", "stable")
+)
 
-    def setUp(self):
-        self.stop = 'discover' not in sys.argv
-        self.stop = self.stop and 'pytest' not in sys.argv[0]
-        self.input = DataVault().input(
-            "data",
-            "stable",
-            label='testinvmass'
-        )
-        self.particles = {
-            'pi0': (8, 9),
-            'eta': (0.8, 1.4)
-        }
 
-    def draw(self, particle, func, title):
-        bin, nrebin = self.particles[particle]
-        mass = InvariantMass(self.input, bin, nrebin,
-                             Options(particle=particle))
-        mass.extract_data()
-        # NB: Add this to be able to see the significance
-        mass.area_error = 10, 0.05
-        func(mass)
-        wait(self.stop)
+@pytest.fixture()
+def stop():
+    stop = "discover" not in sys.argv
+    stop = stop and "pytest" not in sys.argv[0]
+    return stop
 
-    def draw_multiple(self, particle):
-        option = Options(particle=particle)
 
-        pipeline = Pipeline([
-            ('slice', DataSlicer(option.pt)),
-            ('extract', InvariantMassExtractor(option.invmass)),
-            ('fit', MassFitter(option.invmass.use_mixed)),
-            ("ranges", RangeEstimator(option.spectrum)),
-        ])
+@pytest.mark.parametrize("particle", [
+    "#pi^{0}",
+    "#eta"
+])
+def test_draws_multiple(stop, particle):
+    option = Options(particle=particle)
 
-        loggs = AnalysisOutput("test-multirage-plotter", particle)
-        masses = pipeline.transform(self.input, loggs)
-        loggs.update({"invariant-masses": masses})
-        loggs.plot(stop=self.stop)
+    pipeline = Pipeline([
+        ("slice", DataSlicer(option.pt)),
+        ("extract", InvariantMassExtractor(option.invmass)),
+        ("fit", MassFitter(option.invmass.use_mixed)),
+        ("ranges", RangeEstimator(option.spectrum)),
+    ])
 
-    # @unittest.skip('')
-    @pytest.mark.onlylocal
-    def test_multiple_plots(self):
-        for p in self.particles:
-            self.draw_multiple(p)
+    loggs = AnalysisOutput("test multirage plotter")
+    masses = pipeline.transform(DATASET, loggs)
+    loggs.update({"invariant-masses": masses})
+    loggs.plot(stop=stop)
