5e850d08445a154143560066ff7c15f79aaff74e
Sasha <q.dev.null@gmail.com>
2018-12-29 18:09:52 +0100

    Fix inputs in pi0/eta efficiency ratio

--- neutral-meson-spectra/analysis/efficiency/ratio.py
+++ neutral-meson-spectra/analysis/efficiency/ratio.py
@@ -1,5 +1,6 @@
-import unittest
+import pytest
 
+from lazy_object_proxy import Proxy
 from spectrum.pipeline import ComparePipeline
 from spectrum.efficiency import Efficiency
 from spectrum.options import CompositeEfficiencyOptions
@@ -7,25 +8,30 @@ from spectrum.output import AnalysisOutput
 
 from vault.datavault import DataVault
 
+ETA_INPUTS = Proxy(
+    lambda: (
+        DataVault().input("single #eta", "low", "PhysEff"),
+        DataVault().input("single #eta", "high", "PhysEff"),
+    )
+)
 
-class TestFakeEfficiencyPi0(unittest.TestCase):
+PI0_INPUTS = Proxy(
+    lambda: (
+        DataVault().input("single #pi^{0}", "low", "PhysEff"),
+        DataVault().input("single #pi^{0}", "high", "PhysEff"),
+    )
+)
 
-    def test_efficiency_ratio(self):
-        eta_inputs = (
-            DataVault().input("single #eta", "low", "PhysEff"),
-            DataVault().input("single #eta", "high", "PhysEff"),
-        )
-        pi0_inputs = (
-            DataVault().input("single #pi^{0}", "low", "PhysEff"),
-            DataVault().input("single #pi^{0}", "high", "PhysEff"),
-        )
-        ptrange = "config/pt-same.json"
-        opt_eta = CompositeEfficiencyOptions("#eta", ptrange=ptrange)
-        opt_pi0 = CompositeEfficiencyOptions("#pi^{0}", ptrange=ptrange)
-        estimator = ComparePipeline([
-            ("#eta", Efficiency(opt_eta)),
-            ("#pi^{0}", Efficiency(opt_pi0)),
-        ])
-        loggs = AnalysisOutput("efficiency_ratio")
-        estimator.transform((eta_inputs, pi0_inputs), loggs)
-        loggs.plot()
+
+@pytest.mark.onlylocal
+def test_efficiency_ratio(self):
+    ptrange = "config/pt-same.json"
+    opt_eta = CompositeEfficiencyOptions("#eta", ptrange=ptrange)
+    opt_pi0 = CompositeEfficiencyOptions("#pi^{0}", ptrange=ptrange)
+    estimator = ComparePipeline([
+        ("#eta", Efficiency(opt_eta)),
+        ("#pi^{0}", Efficiency(opt_pi0)),
+    ])
+    loggs = AnalysisOutput("efficiency_ratio")
+    estimator.transform((ETA_INPUTS, PI0_INPUTS), loggs)
+    loggs.plot()
