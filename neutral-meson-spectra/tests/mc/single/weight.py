9790fd032c2ea4f6712676cfdb88d895f2486ad2
Sasha <q.dev.null@gmail.com>
2019-01-01 14:41:02 +0100

    ClFix weights calculation for pion spmc

--- neutral-meson-spectra/tests/mc/single/weight.py
+++ neutral-meson-spectra/tests/mc/single/weight.py
@@ -1,37 +1,45 @@
 import ROOT
-import unittest
+import pytest  # noqa
 
+from lazy_object_proxy import Proxy
 from spectrum.options import CompositeCorrectedYieldOptions
 from spectrum.corrected_yield import CorrectedYield
-# from spectrum.comparator import Comparator
-# from spectrum.broot import BROOT as br
+from spectrum.output import AnalysisOutput
 
 from vault.datavault import DataVault
 from vault.formulas import FVault
+from tools.feeddown import data_feeddown
 
 
-class TestCorrectedYield(unittest.TestCase):
-
-    # @unittest.skip('')
-    def test_corrected_yield_for_pi0(self):
-        prod = "single #pi^{0}"
-        inputs = (
-            DataVault().input(prod, "low", listname="PhysEff"),
-            DataVault().input(prod, "high", listname="PhysEff"),
+DATASET = Proxy(
+    lambda:
+    (
+        (
+            DataVault().input("data", listname="Phys", histname="MassPtSM0"),
+            data_feeddown(),
+        ),
+        (
+            DataVault().input("single #pi^{0}", "low", "PhysEff"),
+            DataVault().input("single #pi^{0}", "high", "PhysEff"),
         )
+    )
+)
 
-        tsallis = ROOT.TF1("tsallis", FVault().func("tsallis"), 0, 10)
-        tsallis.SetParameters(0.014960701090585591,
-                              0.287830380417601, 9.921003040859755)
-        tsallis.FixParameter(3, 0.135)
-        tsallis.FixParameter(4, 0.135)
 
-        options = CompositeCorrectedYieldOptions(particle="#pi^{0}")
-        options.fitfunc = tsallis
-        CorrectedYield(options).transform(
-            (
-                DataVault().input("data"),
-                inputs
-            ),
-            "corrected yield #pi^{0}"
-        )
+def fitfunction():
+    tsallis = ROOT.TF1("tsallis", FVault().func("tsallis"), 0, 10)
+    tsallis.SetParameters(0.014960701090585591,
+                          0.287830380417601,
+                          9.921003040859755)
+    tsallis.FixParameter(3, 0.135)
+    tsallis.FixParameter(4, 0.135)
+    return tsallis
+
+
+@pytest.mark.onlylocal
+def test_corrected_yield_for_pi0():
+    options = CompositeCorrectedYieldOptions(particle="#pi^{0}")
+    options.fitfunc = fitfunction()
+    CorrectedYield(options).transform(
+        DATASET,
+        loggs=AnalysisOutput("corrected yield #pi^{0}"))
