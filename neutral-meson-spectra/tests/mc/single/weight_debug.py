8f0cb4439e04f78efe06a5c03946db6c21ca932f
Sasha <q.dev.null@gmail.com>
2019-01-02 07:44:28 +0100

    Clean input data in tsallis weights debug analysis

--- neutral-meson-spectra/tests/mc/single/weight_debug.py
+++ neutral-meson-spectra/tests/mc/single/weight_debug.py
@@ -1,6 +1,7 @@
 import ROOT
-import unittest
+import pytest
 
+from lazy_object_proxy import Proxy
 from spectrum.pipeline import Pipeline, ComparePipeline
 from spectrum.pipeline import OutputFitter
 from spectrum.input import SingleHistInput
@@ -13,6 +14,19 @@ from spectrum.pipeline import TransformerBase
 from vault.datavault import DataVault
 from vault.formulas import FVault
 
+DATASET = Proxy(
+    lambda: DataVault().input("single #pi^{0}", "low", listname="PhysEff")
+)
+
+# NB: This dataset is now obsolete
+COMPARE_DATASETS = Proxy(
+    lambda:
+    (
+        DataVault().input("single #pi^{0}", "low", listname="PhysEff"),
+        DataVault().input("single alternative", "low", listname="PhysEff"),
+    )
+)
+
 
 class Attributes(TransformerBase):
     def transform(self, data, loggs):
@@ -21,58 +35,47 @@ class Attributes(TransformerBase):
         return data
 
 
-class TestCorrectedYield(unittest.TestCase):
+def fitfunc():
+    tsallis = ROOT.TF1(
+        "tsallis",
+        FVault().func("tsallis", "standard"), 0, 8)
 
-    def test_corrected_yield_for_pi0(self):
-        mcdata = DataVault().input(
-            "single #pi^{0} iteration d3 nonlin11", "low",
-            # "single #pi^{0}", "low",
-            listname="PhysEff")
+    parameters = [0.2622666606436988,
+                  0.08435275173194286,
+                  7.356520553419461]
 
-        tsallis = ROOT.TF1(
-            "tsallis",
-            FVault().func("tsallis", "standard"), 0, 8)
+    for i, par in enumerate(parameters):
+        tsallis.FixParameter(i, par)
+    # tsallis.SetParameter(0, parameters[0])
 
-        parameters = [0.2622666606436988,
-                      0.08435275173194286,
-                      7.356520553419461]
+    # tsallis.SetParameters(0.014960701090585591,
+    # 0.287830380417601, 9.921003040859755)
+    tsallis.FixParameter(3, 0.135)
+    tsallis.FixParameter(4, 0.135)
+    return tsallis
 
-        for i, par in enumerate(parameters):
-            tsallis.FixParameter(i, par)
-        # tsallis.SetParameter(0, parameters[0])
 
-        # tsallis.SetParameters(0.014960701090585591,
-        # 0.287830380417601, 9.921003040859755)
-        tsallis.FixParameter(3, 0.135)
-        tsallis.FixParameter(4, 0.135)
+def test_corrected_yield_for_pi0():
+    options = Options()
+    options.fitfunc = fitfunc()
+    estimator = Pipeline([
+        ("data", SingleHistInput(
+            "hPt_#pi^{0}_primary_standard", norm=True)),
+        ("attributes", Attributes()),
+        ("fit", OutputFitter(options)),
+    ])
 
-        options = Options()
-        options.fitfunc = tsallis
-        estimator = Pipeline([
-            ("data", SingleHistInput(
-                "hPt_#pi^{0}_primary_standard", norm=True)),
-            ("attributes", Attributes()),
-            ("fit", OutputFitter(options)),
-        ])
+    estimator.transform(DATASET, AnalysisOutput("corrected yield #pi^{0}"))
 
-        estimator.transform(mcdata, AnalysisOutput("corrected yield #pi^{0}"))
 
-    @unittest.skip('')
-    def test_corrected_different_spectra(self):
-        original = DataVault().input(
-            "single #pi^{0}", "low",
-            listname="PhysEff")
-
-        mcdata = DataVault().input(
-            "single #pi^{0} iteration d3 nonlin10", "low",
-            listname="PhysEff3")
-
-        estimator = ComparePipeline([
-            ("mcdata", SingleHistInput(
-                "hPt_#pi^{0}_primary_standard", norm=False)),
-            ("original", SingleHistInput(
-                "hPt_#pi^{0}_primary_standard", norm=False)),
-        ], True)
-        estimator.transform(
-            [mcdata, original],
-            AnalysisOutput("corrected yield #pi^{0}"))
+@pytest.mark.skip('Obsolete dataset')
+def test_corrected_different_spectra():
+    estimator = ComparePipeline([
+        ("mcdata", SingleHistInput(
+            "hPt_#pi^{0}_primary_standard", norm=False)),
+        ("original", SingleHistInput(
+            "hPt_#pi^{0}_primary_standard", norm=False)),
+    ], True)
+    estimator.transform(
+        COMPARE_DATASETS,
+        AnalysisOutput("corrected yield #pi^{0}"))
