5ca7ff9718a1a63505115601393dfa1b7b219e08
Sasha <q.dev.null@gmail.com>
2019-01-04 16:57:32 +0100

    Fix inputs for the yield extraction uncertainty

--- neutral-meson-spectra/tests/systematics/yields.py
+++ neutral-meson-spectra/tests/systematics/yields.py
@@ -6,28 +6,30 @@ from vault.datavault import DataVault
 from uncertainties.yields import YieldExtractioin
 from uncertainties.yields import YieldExtractioinUncertanityOptions
 from spectrum.comparator import Comparator
+from tools.feeddown import data_feeddown
 
-EFFICIENCY_DATA = (
-    DataVault().input("single #pi^{0}", "low"),
-    DataVault().input("single #pi^{0}", "high"),
-)
 
-CYIELD_DATA = (
-    DataVault().input("data"),
-    EFFICIENCY_DATA
-)
+def cyield_data():
+    data_input = (
+        DataVault().input("data", histname="MassPtSM0"),
+        data_feeddown(),
+    )
+    mc_inputs = (
+        DataVault().input("single #pi^{0}", "low", listname="PhysEff"),
+        DataVault().input("single #pi^{0}", "high", listname="PhysEff"),
+    )
+    return data_input, mc_inputs
 
 
 @pytest.mark.interactive
 @pytest.mark.onlylocal
 def test_yield_extraction_uncertanity_pion():
-
     options = YieldExtractioinUncertanityOptions(
         CompositeCorrectedYieldOptions(particle="#pi^{0}")
     )
     estimator = YieldExtractioin(options)
     output = estimator.transform(
-        CYIELD_DATA,
+        cyield_data(),
         loggs=AnalysisOutput("corrected yield #pi^{0}")
     )
     Comparator().compare(output)
