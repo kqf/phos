d8b61c51e87e6ef8e6c3ec48f86b5834db58b5b9
Sasha <q.dev.null@gmail.com>
2019-01-05 16:53:47 +0100

    Add 2d hist support hist2array for root_numpy subsitute

---
+++ qa/trigger-qa/analysis/tests/test_root_numpy.py
@@ -0,0 +1,28 @@
+import ROOT
+import pytest
+from trigger.utils import hist2array
+import numpy as np
+
+
+def badmap():
+    hist = ROOT.TH2F("PHOS_BadMap_mod1", "", 64, 0.5, 63.5, 56, 0.5, 55.5)
+    for i in range(hist.GetNbinsX()):
+        for j in range(hist.GetNbinsY()):
+            hist.SetBinContent(i + 1, j + 1, 1000 * i + j)
+    return hist
+
+
+def hist1d():
+    hist = ROOT.TH1F("testing_vector", "", 64, 0.5, 63.5)
+    for i in range(hist.GetNbinsX()):
+        hist.SetBinContent(i + 1, i)
+    return hist
+
+
+@pytest.mark.parametrize("hist", [
+    hist1d(),
+    badmap(),
+])
+def test_hist2array(hist):
+    import root_numpy as rnp
+    np.testing.assert_almost_equal(rnp.hist2array(hist), hist2array(hist))
--- qa/trigger-qa/analysis/trigger/utils.py
+++ qa/trigger-qa/analysis/trigger/utils.py
@@ -1,6 +1,18 @@
 import numpy as np
 
 
+def hist2array2d(hist):
+    data = [[
+        hist.GetBinContent(i + 1, j + 1)
+        for j in range(hist.GetNbinsY())]
+        for i in range(hist.GetNbinsX())]
+
+    return np.asarray(data)
+
+
 def hist2array(hist):
+    # Ugly but efficient hack against ROOT typing
+    if "2" in str(type(hist)):
+        return hist2array2d(hist)
     start, stop = 1, hist.GetNbinsX() + 1
     return np.array([hist.GetBinContent(i) for i in range(start, stop)])
