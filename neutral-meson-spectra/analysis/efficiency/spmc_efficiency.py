2dbcb3a631e90749d80f5007d520ec15f949149c
Sasha <q.dev.null@gmail.com>
2019-01-14 21:25:53 +0100

    Use lazy datasets for efficiency calculation

--- neutral-meson-spectra/analysis/efficiency/spmc_efficiency.py
+++ neutral-meson-spectra/analysis/efficiency/spmc_efficiency.py
@@ -1,5 +1,6 @@
 import pytest
 
+from lazy_object_proxy import Proxy
 from spectrum.efficiency import Efficiency
 from spectrum.options import CompositeEfficiencyOptions
 from spectrum.output import AnalysisOutput
@@ -9,25 +10,31 @@ from spectrum.comparator import Comparator  # noqa
 from tools.validate import validate
 from vault.datavault import DataVault
 
-PION_INPUT = (
-    DataVault().input("single #pi^{0}", "low", "PhysEff"),
-    DataVault().input("single #pi^{0}", "high", "PhysEff"),
+PION_INPUT = Proxy(
+    lambda:
+    (
+        DataVault().input("single #pi^{0}", "low", "PhysEff"),
+        DataVault().input("single #pi^{0}", "high", "PhysEff"),
+    )
 )
 
-ETA_INPUT = (
-    DataVault().input("single #eta", "low"),
-    DataVault().input("single #eta", "high"),
+ETA_INPUT = Proxy(
+    lambda:
+    (
+        DataVault().input("single #eta", "low"),
+        DataVault().input("single #eta", "high"),
+    )
 )
 
 
-@pytest.mark.skip("TODO: Update me")
+# @pytest.mark.skip("TODO: Update me")
 @pytest.mark.onlylocal
 def test_pi0_efficiency():
     efficiency = evaluate_spmc_efficiency(PION_INPUT, "#pi^{0}")
     validate(br.hist2dict(efficiency), "spmc_efficiency/#pi^{0}")
 
 
-# @pytest.mark.skip("")
+@pytest.mark.skip("")
 def test_eta_efficiency():
     evaluate_spmc_efficiency(ETA_INPUT, "#eta")
 
@@ -39,7 +46,4 @@ def evaluate_spmc_efficiency(inputs, particle):
         inputs,
         loggs
     )
-    # diff = Comparator()
-    # diff.compare(efficiency)
-    # loggs.plot()
     return efficiency
