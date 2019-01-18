6c61a01a9770a4fb92e785c2ea6761c91b972e90
Sasha <q.dev.null@gmail.com>
2019-01-18 19:23:46 +0100

    Add CLING preprocessor instructions for launch qa and pp macro

--- protons/macros/launch-qa-and-pp/run.C
+++ protons/macros/launch-qa-and-pp/run.C
@@ -1,3 +1,13 @@
+#ifdef __CLING__
+
+// Tell ROOT where to find AliPhysics headers
+R__ADD_INCLUDE_PATH($ALICE_PHYSICS)
+#include <PWGGA/PHOSTasks/PHOS_PbPb/AddAODPHOSTender.C>
+#include <OADB/macros/AddTaskPhysicsSelection.C>
+#include <PWGGA/PHOSTasks/PHOS_PbPb/AddAODPHOSTender.C>
+#include <PWGGA/PHOSTasks/PHOS_EpRatio/AddTaskPHOSEpRatio.C>
+#endif
+
 #include "../../setup/environment.h"
 
 void run(TString period, const char * runmode = "local", const char * pluginmode = "test", TString dpart = "first", Bool_t isMC = kFALSE, Bool_t useJDL = kTRUE)
