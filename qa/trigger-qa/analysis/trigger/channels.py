Fix tru index calculation for trigger patches

qa/trigger-qa/analysis/trigger/channels.py
@@ -56,7 +56,7 @@ def save_maps(patches, filename="trigger-bad-map.root"):
     ofile.Close()
 
 
-def fit_channels(triggers):
+def fit_channels(triggers, filename):
     counts = np.asarray(triggers).reshape(-1)
     counts = counts[counts > 0]
 
@@ -75,9 +75,8 @@ def fit_channels(triggers):
     freq.Draw()
     plotting.decorate_pad(ROOT.gPad)
     ROOT.gPad.Update()
-    ROOT.gPad.SaveAs("fitted.pdf")
-    raw_input()
-    return fitf.GetParameter(1), fitf.GetParameter(2)
+    ROOT.gPad.SaveAs(filename)
+    return fitf.GetParameter(0), fitf.GetParameter(0)
 
 
 def draw_line(hist, position):
@@ -122,7 +121,9 @@ def channels_tru(filepath, nmodules=4, ntrus=8):
     for sm_index, patches in enumerate(trigger_patches):
         for itru in range(1, ntrus + 1):
             tru = select_tru(patches, itru)
-            mu, sigma = fit_channels(tru)
+            # plot_matrix(tru, histograms[0])
+            filename = "fitted-sm{}-tru{}.pdf".format(sm_index + 1, itru)
+            mu, sigma = fit_channels(tru, filename)
             title = "{} good channels: {} < # hits < {}".format(
                 "TRU", 1, mu + 3 * (sigma ** 0.5)
             )

qa/trigger-qa/analysis/trigger/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+TRIGGER_MODULE_SHAPE = (16, 28)
+
 
 def hist2array2d(hist):
     data = [[
@@ -56,12 +58,41 @@ def tru(cell_x, cell_z):
     return -111
 
 
+def tru_from_patch(patch_x, patch_z):
+    # Return TRU region number for given cell.
+    # patch_x: [0-63], patch_z: [0-55]
+
+    # RCU0: TRU 1,2
+    if 0 <= patch_x < 4:
+        if 0 <= patch_z < 14:
+            return 2
+        return 1
+
+    # RCU1: TRU 3,4
+    if 4 <= patch_x < 8:
+        if 0 <= patch_z < 14:
+            return 4
+        return 3
+
+    # RCU2: TRU 5,6
+    if 8 <= patch_x < 12:
+        if 0 <= patch_z < 14:
+            return 6
+        return 5
+
+    # RCU3: TRU 7,8
+    if 12 <= patch_x < 16:
+        if 0 <= patch_z < 14:
+            return 8
+        return 7
+
+    return -111
+
+
 def select_tru(hist, number):
     trumasks = [[
-        tru(i, j)
+        tru_from_patch(i, j)
         for j in range(hist.shape[1])]
         for i in range(hist.shape[0])]
-    print(trumasks)
-
     trumask = np.asarray(trumasks) == number
     return hist * trumask