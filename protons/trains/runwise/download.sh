b94f6967c5cf59b190434dc03c5af2a93f3542f0
Sasha <q.dev.null@gmail.com>
2019-02-11 21:18:29 +0100

    Fix macros for train runwise downloads

--- protons/trains/runwise/download.sh
+++ protons/trains/runwise/download.sh
@@ -1,34 +1,35 @@
 # Index corresponds to each period
+MAINDIR=/alice/data/2016
+TRAIN=696_20190205-1725
+TRAIN_PATH=pass1/AOD208/PWGGA/GA_pp_AOD/${TRAIN}_child_
 
-declare -a periods=("LHC17c" "LHC17e" "LHC17f" "LHC17g" "LHC17i" "LHC17j" "LHC17k" "LHC17l" "LHC17m" "LHC17o")
-nperiods=${#periods[@]}
-
-echo "Downloading from $nperiods"
+declare -A children=([4]=5 [5]=5 [6]=5 [7]=5 [8]=5 [9]=5 [10]=5 [11]=5)
+declare -A names=([4]=LHC16g [5]=LHC16h [6]=LHC16i [7]=LHC16j [8]=LHC16k [9]=LHC16l [10]=LHC16o [11]=LHC16p)
 
 function download_from_grid()
 {
-    counter=1
-	MHOME=/alice/data/2017/$1/
-	TRAIN=/pass1/PWGGA/GA_pp_AOD/334_20180217-1427_child_$3/
-    for run in $(alien_ls $MHOME/); do
-    	filepath=$MHOME/$run/$TRAIN/AnalysisResults.root
+    outdir=$1
+    workdir=${MAINDIR}/${outdir}/
+    train_child=${TRAIN_PATH}$2/
+    echo ${workdir}
+    for run in $(alien_ls ${workdir}/); do
+    	filepath=${workdir}/${run}/${train_child}/AnalysisResults.root
     	output=$(alien_ls "$filepath") 
         if [ "$output" == "AnalysisResults.root" ]; then
-        	echo "Downloading run " $run
-	        alien_cp alien://$filepath $2/$((counter++)).$run.root
+        	echo "Downloading run " ${run}
+	        alien_cp alien://${filepath} ${outdir}/${run}.root
+            # echo alien://${filepath} ${outdir}/${run}.root
         fi
-        # echo $run
-        # until alien_cp alien://$MHOME/$1/output/$run/$2 $3/$((counter++)).$run.root; do echo 'Trying again in 10 sec.'; sleep 10; done
     done
 }
 
 function main() {
-    for (( i=4; i<${nperiods}+1; i++ ));
-    do
-        echo "Processing: " $i "/" ${nperiods} " : " ${periods[$i-1]}
-        mkdir -p ${periods[$i-1]}
-        download_from_grid ${periods[$i-1]} ${periods[$i-1]} $i
-        hadd ${periods[$i-1]}.root ${periods[$i-1]}/*.root
+    for child in ${!children[@]}; do
+        period=${names[$child]}
+        echo "Processing: " ${period}
+        mkdir -p ${period}
+        download_from_grid ${period} $child
+        echo ${period}.root ${period}/*.root
     done
 }
 
