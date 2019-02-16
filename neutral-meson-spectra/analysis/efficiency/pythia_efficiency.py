46c558a5fee6012e3814fd1b0720a6d04c8490d9
Sasha <q.dev.null@gmail.com>
2019-02-16 17:09:28 +0100

    Add pythia8 scripts for efficiency calculation

---
+++ neutral-meson-spectra/analysis/efficiency/pythia_efficiency.py
@@ -0,0 +1,29 @@
+import pytest
+
+from lazy_object_proxy import Proxy
+from spectrum.efficiency import Efficiency
+from spectrum.options import EfficiencyOptions
+from spectrum.output import AnalysisOutput
+# from spectrum.broot import BROOT as br
+from spectrum.comparator import Comparator  # noqa
+
+# from tools.validate import validate
+from vault.datavault import DataVault
+
+DATASET = Proxy(
+    lambda: DataVault().input("pythia8", "ep_ratio_3", "PhysEff")
+)
+
+
+@pytest.mark.onlylocal
+@pytest.mark.parametrize("data, particle", [
+    (DATASET, "#pi^{0}"),
+    # (ETA_INPUT, "#eta"),
+])
+def test_efficiency(data, particle):
+    options = EfficiencyOptions(particle)
+    loggs = AnalysisOutput("efficiency_spmc_{}".format(particle))
+    efficiency = Efficiency(options).transform(data, loggs)
+    # validate(br.hist2dict(efficiency), "spmc_efficiency/" + particle)
+    Comparator().compare(efficiency)
+    return efficiency
