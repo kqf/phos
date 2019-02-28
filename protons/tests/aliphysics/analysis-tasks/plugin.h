6b96d2c0bb92cbe53fcfaa6fa58ee85e2a36feda
Sasha <q.dev.null@gmail.com>
2019-02-28 07:59:26 +0100

    Clean aliphysics trains tests

--- protons/tests/aliphysics/analysis-tasks/plugin.h
+++ protons/tests/aliphysics/analysis-tasks/plugin.h
@@ -1,5 +1,4 @@
 #include "../../../setup/alien.h"
-#include "../../../setup/values_for_dataset.h"
 #include "iostream"
 
 using std::cout;
@@ -12,10 +11,6 @@ AliAnalysisGrid * CreatePlugin(const char * pluginmode, TString period, TString 
 
 	// Use default setup for the plugin
 	AliAnalysisGrid * plugin = GetPlugin(pluginmode, period, dpart, useJDL, isMC, msize);
-	std::vector<Int_t> v; //
-	values_for_dataset(v, period, "../../../datasets/");
-	for (UInt_t i = 0; i < v.size(); ++i)
-		plugin->AddRunNumber(v[i]);	
 	// Extract period and reconstruction pass
 	TString dir = period.Contains("_extra") ? period : TString(period, isMC ? 10 : 6); // fancy slicing
 	
--- protons/tests/aliphysics/analysis-tasks/run.C
+++ protons/tests/aliphysics/analysis-tasks/run.C
@@ -1,54 +1,47 @@
 #include "../../../setup/environment.h"
-#include <PWGGA/PHOSTasks/PHOS_LHC16_pp/macros/AddAnalysisTaskPP.C>
-// #include "../../../tasks/analysis-task-pp/macros/AddAnalysisTaskPP.C"
+#include "../../../tasks/analysis-task-pp/macros/AddAnalysisTaskPP.C"
 #include "plugin.h"
+// #include <PWGGA/PHOSTasks/PHOS_LHC16_pp/macros/AddAnalysisTaskPP.C>
 
-void run(
-    TString period,
-    const char * runmode = "local",
-    const char * pluginmode = "test",
-    TString dpart = "first",
-    Bool_t isMC = kFALSE,
-    Bool_t useJDL = kTRUE
-)
+void run(TString period, const char * runmode = "local", const char * pluginmode = "test", TString dpart = "first", Bool_t isMC = kFALSE, Bool_t useJDL = kTRUE)
 {
-    SetupEnvironment();
-
+    AliAnalysisGrid * alien = CreatePlugin(pluginmode, period, dpart, useJDL, isMC);
     AliAnalysisManager * manager  = new AliAnalysisManager("PHOS_PP");
+    AliAODInputHandler * aod = new AliAODInputHandler();
 
-    AliAnalysisGrid * alienHandler = CreatePlugin(pluginmode, period, dpart, useJDL, isMC);
+    manager->SetInputEventHandler(aod);
+
     if ( isMC )
     {
         AliMCEventHandler * mchandler = new AliMCEventHandler();
         mchandler->SetReadTR ( kFALSE ); // Don't read track references
         manager->SetMCtruthEventHandler ( mchandler );
     }
+
     // Connect plug-in to the analysis manager
-    manager->SetGridHandler(alienHandler);
-
-    AliAODInputHandler * aodH = new AliAODInputHandler();
-    manager->SetInputEventHandler(aodH);
-
+    manager->SetGridHandler(alien);
 
     Bool_t enablePileupCuts = kTRUE;
     AddTaskPhysicsSelection (isMC, enablePileupCuts);  //false for data, true for MC
 
-
-
+    TString pref =  isMC ? "MC" : "";
     TString tenderOption = isMC ? "Run2Default" : "";
     AliPHOSTenderTask * tenderPHOS = AddAODPHOSTender("PHOSTenderTask", "PHOStender", tenderOption, 1, isMC);
+
     AliPHOSTenderSupply * PHOSSupply = tenderPHOS->GetPHOSTenderSupply();
-    // AliAnalysisTaskPIDResponse *taskPID = AddTaskPIDResponse(
-    //     isMC, 
-    //     kTRUE,
-    //     kTRUE, 
-    //     1,          // reco pass
-    //     kFALSE, 
-    //     "",
-    //     kTRUE,
-    //     kFALSE,
-    //     1           // reco pass
-    // );
+    // PHOSSupply->ForceUsingBadMap("../../../datasets/BadMap_LHC16-updated.root");
+
+    AliAnalysisTaskPIDResponse *taskPID = AddTaskPIDResponse(
+        isMC, 
+        kTRUE,
+        kTRUE, 
+        1,          // reco pass
+        kFALSE, 
+        "",
+        kTRUE,
+        kFALSE,
+        1           // reco pass
+    );
 
     if (isMC)
     {
@@ -57,13 +50,25 @@ void run(
         Double_t zs_threshold = 0.020;
         PHOSSupply->ApplyZeroSuppression(zs_threshold);
     }
-    TString pref =  isMC ? "MC" : "";
-    AddAnalysisTaskPP(isMC, "test");
+
+    TString msg = "## Real data, no TOF cut efficiency.";
+    if (tenderOption)
+    {
+        msg += " with tender option ";
+        msg += tenderOption;
+    }
+
+    // AddTaskPHOSEpRatio(isMC);
+    AliAnalysisTaskPP13 * task = AddAnalysisTaskPP(isMC, period + pref + msg);
+    task->SelectCollisionCandidates(AliVEvent::kINT7);
+    AddTaskPHOSEpRatio(isMC);
+
 
     if ( !manager->InitAnalysis( ) ) return;
-    
     manager->PrintStatus();
-    alienHandler->SetOutputFiles("AnalysisResults.root");
+
+
+    alien->SetOutputFiles("AnalysisResults.root");
     manager->StartAnalysis (runmode);
     gObjectTable->Print( );
 }
