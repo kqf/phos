7798be6ec65a37445ba7ed91a9061ee9ddaee737
Sasha <q.dev.null@gmail.com>
2019-03-17 19:40:10 +0100

    Fix download scripts

--- protons/trains/io-download.sh
+++ protons/trains/io-download.sh
@@ -16,8 +16,8 @@ train=/alice/cern.ch/user/a/alitrain/PWGGA/GA_pp_MC_AOD/
 # declare -A children=([1]=1 [2]=1 [3]=1 [4]=1 [5]=1 [6]=1 [7]=1 [8]=1 [9]=1 [10]=1)
 # declare -A names=([1]=LHC17f6 [2]=LHC17f9 [3]=LHC17d17 [4]=LHC17f5 [5]=LHC17d3 [6]=LHC17e5 [7]=LHC17d16 [8]=LHC17d18 [9]=LHC18f1 [10]=LHC18d8)
 
-Dataset: PYT8_13TeV_anchLHC16_AOD209_extra
-run=1296_20190212-1804_child_
+# Dataset: PYT8_13TeV_anchLHC16_AOD209_extra
+# run=1296_20190212-1804_child_
 run=1386_20190315-1048_child_
 declare -A children=([1]=1 [2]=1 [3]=1 [4]=1 [5]=1 [6]=1 [7]=1 [8]=1 [9]=1 [10]=1)
 declare -A names=([1]=LHC17f6_extra [2]=LHC17f9_extra [3]=LHC17d17_extra [4]=LHC17f5_extra [5]=LHC17d3_extra [6]=LHC17e5_extra [7]=LHC17d16_extra [8]=LHC17d18_extra [9]=LHC18f1_extra [10]=LHC18d8_extra)
@@ -44,8 +44,8 @@ declare -A names=([1]=LHC17f6_extra [2]=LHC17f9_extra [3]=LHC17d17_extra [4]=LHC
 # run=736_20190307-1551_child_
 # This scheme is valid for LHC16 data.
 # fill the map in the following way ([child number]=runlist number)
-declare -A children=([4]=5 [5]=5 [6]=5 [7]=5 [8]=5 [9]=5 [10]=5 [11]=5)
-declare -A names=([4]=LHC16g [5]=LHC16h [6]=LHC16i [7]=LHC16j [8]=LHC16k [9]=LHC16l [10]=LHC16o [11]=LHC16p)
+# declare -A children=([4]=5 [5]=5 [6]=5 [7]=5 [8]=5 [9]=5 [10]=5 [11]=5)
+# declare -A names=([4]=LHC16g [5]=LHC16h [6]=LHC16i [7]=LHC16j [8]=LHC16k [9]=LHC16l [10]=LHC16o [11]=LHC16p)
 
 # LHC17 data
 #
