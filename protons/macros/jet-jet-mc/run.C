ac35b25935d03e0e8aa01795410a4b417a640aa6
Sasha <q.dev.null@gmail.com>
2019-03-22 17:36:13 +0100

    Update paths to environment in jet-jet steering macro

--- protons/macros/jet-jet-mc/run.C
+++ protons/macros/jet-jet-mc/run.C
@@ -1,7 +1,6 @@
 #include "../../setup/environment.h"
 #include "plugin.h"
 #include "task.h"
-#include "../setup/environment.h"
 
 void run(TString period, const char * runmode = "local", const char * pluginmode = "test", TString dpart = "first", Bool_t isMC = kFALSE, Bool_t useJDL = kTRUE)
 {
@@ -33,41 +32,31 @@ void run(TString period, const char * runmode = "local", const char * pluginmode
     TString files = "";
     TString pref =  isMC ? "MC" : "";
 
-
     TString tenderOption = isMC ? "Run2Default" : "";
     AliPHOSTenderTask * tenderPHOS = AddAODPHOSTender("PHOSTenderTask", "PHOStender", tenderOption, 1, isMC);
 
-    AliPHOSTenderSupply * PHOSSupply = tenderPHOS->GetPHOSTenderSupply();
-
-    // TODO: Move bad map outside this directory
-    //
-
-    PHOSSupply->ForceUsingBadMap("../datasets/BadMap_LHC16-updated.root");
+    AliPHOSTenderSupply * supply = tenderPHOS->GetPHOSTenderSupply();
+    supply->ForceUsingBadMap("../../datasets/BadMap_LHC16-updated.root");
 
     if (isMC)
     {
         // Important: Keep track of this variable
         // ZS threshold in unit of GeV
         Double_t zs_threshold = 0.020;
-        PHOSSupply->ApplyZeroSuppression(zs_threshold);
+        supply->ApplyZeroSuppression(zs_threshold);
     }
 
-
-    // There is no need to download QA when we use don't use JDL
-    // if (useJDL)
-    // files += AddTaskCaloCellsQAPt(AliVEvent::kINT7, cells);
-
     TString msg = "## Jet-Jet MC ## nonlinearity applied, 20 MeV Zero Supression ";
-
     if (tenderOption)
     {
         msg += " with tender option ";
         msg += tenderOption;
     }
 
-    files += AddAnalysisTaskPP(AliVEvent::kINT7, period + pref + msg, "Tender", "", isMC);
+    AliAnalysisTaskPP13 * task = AddAnalysisTaskPP(isMC, period + pref + msg);
+    task->SetCollisionCandidates(AliVEvent::kINT7);
 
-    if ( !manager->InitAnalysis( ) ) return;
+    if (!manager->InitAnalysis()) return;
     manager->PrintStatus();
 
 
