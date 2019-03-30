35ad0d9e36eaa408a787620ee21e1d611a6a5d73
Sasha <q.dev.null@gmail.com>
2019-03-30 18:10:46 +0100

    Fix a typo in the default tender configuration

--- protons/macros/trigger-qa/run.C
+++ protons/macros/trigger-qa/run.C
@@ -2,7 +2,7 @@
 #include "../../setup/tender.h"
 #include "plugin.h"
 // #include "task.h"
-#include "task.h"
+// #include "task.h"
 
 
 void run(TString period, const char * runmode = "local", const char * pluginmode = "test", TString dpart = "first", Bool_t isMC = kFALSE, Bool_t useJDL = kTRUE)
--- protons/setup/tender.h
+++ protons/setup/tender.h
@@ -1,3 +1,6 @@
+#ifndef TENDER_H
+#define TENDER_H
+
 AliPHOSTenderTask * AddPHOSTender(Bool_t isMC, TString & msg)
 {
     TString decalibration = isMC ? "Run2Default" : "";
@@ -20,13 +23,14 @@ AliPHOSTenderTask * AddPHOSTender(Bool_t isMC, TString & msg)
         // Important: Keep track of this variable
         // ZS threshold in unit of GeV
         Double_t zs_threshold = 0.020;
-        PHOSSupply->ApplyZeroSuppression(zs_threshold);
+        supply->ApplyZeroSuppression(zs_threshold);
     }
 
     msg += " with tender option: ";
     msg += decalibration;
     msg += " Nonlinearity version:";
     msg += nonlinearity;
-
     return tender;
 }
+
+#endif
