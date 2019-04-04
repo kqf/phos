efcf61a954cefbb11c20883bd706e58dbe9bacd4
Sasha <q.dev.null@gmail.com>
2019-04-04 20:39:13 +0200

    Replace the tender for isolated bunches with the standard one

--- protons/macros/isolated-bunches/run.C
+++ protons/macros/isolated-bunches/run.C
@@ -6,74 +6,21 @@ void run(TString period, const char * runmode = "local", const char * pluginmode
 {
     // SetupEnvironment();
 
-    AliAnalysisGrid * alien = CreatePlugin(pluginmode, period, dpart, useJDL, isMC);
     AliAnalysisManager * manager = new AliAnalysisManager("PHOS_PP");
-    AliAODInputHandler * aod = new AliAODInputHandler();
-
-    manager->SetInputEventHandler(aod);
-
-    if(isMC)
-    {
-        AliMCEventHandler * mchandler = new AliMCEventHandler();
-        mchandler->SetReadTR(kFALSE); // Don't read track references
-        manager->SetMCtruthEventHandler(mchandler);
-    }
-
-    // Connect plug-in to the analysis manager
-    manager->SetGridHandler(alien);
-
+    manager->SetGridHandler(CreatePlugin(pluginmode, period, dpart, useJDL, isMC));
+    manager->SetInputEventHandler(new AliAODInputHandler());
 
     Bool_t enablePileupCuts = kTRUE;
     AddTaskPhysicsSelection(isMC, enablePileupCuts);  //false for data, true for MC
 
+    TString msg = message("Isolated photons no timing cut", period);
+    AddPHOSTender(isMC, msg);
 
-    TString tenderOption = isMC ? "Run2Default" : "";
-    AliPHOSTenderTask * tenderPHOS = AddAODPHOSTender("PHOSTenderTask", "PHOStender", tenderOption, 1, isMC);
-
-    AliPHOSTenderSupply * supply = tenderPHOS->GetPHOSTenderSupply();
-    supply->ForceUsingBadMap("../../datasets/BadMap_LHC16-updated.root");
-
-    TString nonlinearity = isMC ? "Run2Tune" : "Run2TuneMC";
-    supply->SetNonlinearityVersion(nonlinearity); 
-
-
-    AliAnalysisTaskPIDResponse *taskPID = AddTaskPIDResponse(
-        isMC, 
-        kTRUE,
-        kTRUE, 
-        1,          // reco pass
-        kFALSE, 
-        "",
-        kTRUE,
-        kFALSE,
-        1           // reco pass
-    );
-
-    if(isMC)
-    {
-        // Important: Keep track of this variable
-        // ZS threshold in unit of GeV
-        Double_t zs_threshold = 0.020;
-        supply->ApplyZeroSuppression(zs_threshold);
-    }
-
-    TString msg = "## Real data, no TOF cut efficiency.";
-    if(tenderOption)
-    {
-        msg += " with tender option ";
-        msg += tenderOption;
-    }
-
-    AliAnalysisTaskPP13 * task = AddAnalysisTaskPP(period + msg, period);
+    AliAnalysisTaskPP13 * task = AddAnalysisTaskPP(msg, period);
     task->SelectCollisionCandidates(AliVEvent::kINT7);
 
     manager->InitAnalysis();
     manager->PrintStatus();
-
-    TString files = AliAnalysisManager::GetCommonFileName();
-    cout << "Output files " << files << endl;
-    alien->SetOutputFiles(files);
-
     manager->StartAnalysis(runmode);
     gObjectTable->Print();
 }
