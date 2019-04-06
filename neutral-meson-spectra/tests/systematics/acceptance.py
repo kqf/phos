58eb49c50b40b6a15f5f3b66ee0dc4a99c414b2c
Sasha <q.dev.null@gmail.com>
2019-04-06 21:02:58 +0200

    Update the acceptance study histograms

--- neutral-meson-spectra/tests/systematics/acceptance.py
+++ neutral-meson-spectra/tests/systematics/acceptance.py
@@ -6,25 +6,34 @@ from spectrum.comparator import Comparator
 from tools.feeddown import data_feeddown
 
 
-def cyield_data(particle):
-    mc_production = "single %s" % particle
+def cyield_data(particle, cut):
+    mc_production = "single %s acceptance" % particle
     data_input = (
-        DataVault().input("data", histname="MassPtSM0"),
+        DataVault().input("data", "acceptance",
+                          listname="Phys{}".format(cut),
+                          histname="MassPtSM0"),
         data_feeddown(),
     )
     mc_inputs = (
-        DataVault().input(mc_production, "low", listname="PhysEff"),
-        DataVault().input(mc_production, "high", listname="PhysEff"),
+        DataVault().input(mc_production, "low",
+                          listname="Phys{}".format(cut),
+                          histname="MassPtSM0"
+                          ),
+        DataVault().input(mc_production, "high",
+                          listname="Phys{}".format(cut),
+                          histname="MassPtSM0"
+                          ),
     )
     return data_input, mc_inputs
 
 
 def acceptance_data(particle):
     return (
-        cyield_data(particle),
+        cyield_data(particle, cut=0),
         (
-            cyield_data(particle),
-            cyield_data(particle),
+            cyield_data(particle, cut=1),
+            cyield_data(particle, cut=2),
+            cyield_data(particle, cut=3),
         ),
     )
 
