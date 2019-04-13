c5eb3b237d45fb0ad2162b2317da2fe379f98b56
Sasha <q.dev.null@gmail.com>
2019-04-13 11:07:48 +0200

    Add scripts for nonlinearity scan

---
+++ protons/macros/nonlinearity-scan/run.C
@@ -0,0 +1,28 @@
+#include "../../setup/environment.h"
+#include "../../setup/tender.h"
+#include "plugin.h"
+#include "task.h"
+
+void run(TString period, const char * runmode = "local", const char * pluginmode = "test", TString dpart = "first", Bool_t isMC = kTRUE, Bool_t useJDL = kTRUE)
+{
+    AliAnalysisManager * manager = new AliAnalysisManager("PHOS_PP");
+    manager->SetGridHandler(CreatePlugin(pluginmode, period, dpart, useJDL));
+    manager->SetInputEventHandler(new AliAODInputHandler());
+
+    // Bool_t enablePileupCuts = kTRUE;
+    // AddTaskPhysicsSelection(isMC, enablePileupCuts);
+
+    TString msg = message("Nonlinearity scan", period);
+    AddPHOSTender(isMC, msg);
+
+    // NB: This is a local copy of steering macro
+    Bool_t acceptance = kTRUE;
+    AliAnalysisTaskPP13 * task = AddAnalysisTaskPP(msg, acceptance);
+    // Don't apply PhysicsSelection for SPMC
+    // task->SelectCollisionCandidates(AliVEvent::kINT7);
+
+    manager->InitAnalysis();
+    manager->PrintStatus();
+    manager->StartAnalysis(runmode);
+    gObjectTable->Print();
+}
---
+++ protons/macros/nonlinearity-scan/task.h
@@ -0,0 +1,49 @@
+#include "../../setup/sources.h"
+
+AliAnalysisTaskPP13 * AddAnalysisTaskPP(TString description, Bool_t calculate_acceptance = kFALSE)
+{
+	// LoadAnalysisLibraries();
+
+	AliAnalysisManager * mgr = AliAnalysisManager::GetAnalysisManager();
+	if (!mgr)
+	{
+		throw "Fatal: There is no analysis manager";
+	}
+	// Setup Selections
+	TList * selections = new TList();
+
+	// Applying no weights
+	//
+	AliPP13ClusterCuts cuts_pi0 = AliPP13ClusterCuts::GetClusterCuts();
+	cuts_pi0.fNContributors = 0;
+
+	AliPP13SelectionWeightsMC & mc_weights = dynamic_cast<AliPP13SelectionWeightsMC &>(
+		AliPP13SelectionWeights::Init(AliPP13SelectionWeights::kSinglePi0MC)
+	);
+
+	mc_weights.fNonGlobal = 1.0; // Take into account the right scale
+	mc_weights.fNonA = -0.455062;
+    mc_weights.fNonSigma = 0.133812;
+
+	selections->Add(new AliPP13EfficiencySelectionMC("PhysEff", "Physics efficiency for neutral particles fully corrected", cuts_pi0, &mc_weights));
+	selections->Add(new AliPP13NonlinearityScanSelection("PhysNonlinScan", "Physics efficiency for neutral particles", cuts_pi0, &mc_weights));
+
+	// Setup task
+	AliAnalysisTaskPP13 * task = new AliAnalysisTaskPP13("PhosProtons", selections);
+	mgr->AddTask(task);
+	mgr->ConnectInput (task, 0, mgr->GetCommonInputContainer());
+	AliAnalysisDataContainer * coutput = 0;
+	for (Int_t i = 0; i < task->GetSelections()->GetEntries(); ++ i)
+	{
+		AliPP13PhysicsSelection * fSel = dynamic_cast<AliPP13PhysicsSelection *> (task->GetSelections()->At(i));
+		fSel->SetTitle(description);
+		cout << fSel->GetTitle() << endl;
+
+		coutput = mgr->CreateContainer(fSel->GetName(),
+									   TList::Class(),
+									   AliAnalysisManager::kOutputContainer,
+									   AliAnalysisManager::GetCommonFileName());
+		mgr->ConnectOutput(task, i + 1, coutput);
+	}
+	return task;
+}
