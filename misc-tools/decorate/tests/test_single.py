28e7f6c9a280ca9e2ac0bc071b18f2acaa8adda9
Sasha <q.dev.null@gmail.com>
2019-04-15 20:48:03 +0200

    Fix single tests for drawtools

--- misc-tools/decorate/tests/test_single.py
+++ misc-tools/decorate/tests/test_single.py
@@ -1,47 +1,54 @@
-#!/usr/bin/python
-
-
+import os
 import ROOT
 import json
-from tests.general_test import TestImages, GeneralTest
+import pytest
+from drawtools.style import Styler
 
 
-class SingleImage(TestImages, GeneralTest):
-
-    def save_config(self):
-        conffile = 'config/test_single.json'
-        filename = 'input/testfile.root'
-        histname = 'hHistogram'
-        pfile = 'results/test.pdf'
-        data = {
-            "histograms":
+@pytest.fixture
+def config():
+    conffile = 'config/test_single.json'
+    filename = 'input/testfile.root'
+    histname = 'hHistogram'
+    pfile = 'results/test.pdf'
+    data = {
+        "histograms":
+        {
+            filename + '/' + histname:
             {
-                filename + '/' + histname:
-                {
-                    "label": "test 1",
-                    "color": 37,
-                    "title": "Test;x;y",
-                    "yprecision": "/ %.2f MeV"
-                }
-            },
-            "canvas":
-            {
-                "size": 5,
-                "logy": 1,
-                "gridy": 1,
-                "gridx": 1,
-                "legend": [0.7, 0.7, 0.89, 0.88],
-                "output": pfile
+                "label": "test 1",
+                "color": 37,
+                "title": "Test;x;y",
+                "yprecision": "/ %.2f MeV"
             }
+        },
+        "canvas":
+        {
+            "size": 5,
+            "logy": 1,
+            "gridy": 1,
+            "gridx": 1,
+            "legend": [0.7, 0.7, 0.89, 0.88],
+            "output": pfile
         }
+    }
 
-        with open(conffile, 'w') as outfile:
-            json.dump(data, outfile)
-        return conffile, filename, histname
+    with open(conffile, 'w') as outfile:
+        json.dump(data, outfile)
 
-    def save_histogram(self, filename, histname):
-        ofile = ROOT.TFile(filename, "update")
-        histogram = ROOT.TH1F(histname, "Testing ...", 1000, -3, 3)
-        histogram.FillRandom('gaus', 100000)
-        histogram.Write()
-        ofile.Close()
+    save_histogram(filename, histname)
+    yield conffile
+    os.remove(filename)
+    os.remove(conffile)
+
+
+def save_histogram(filename, histname):
+    ofile = ROOT.TFile(filename, "update")
+    histogram = ROOT.TH1F(histname, "Testing ...", 1000, -3, 3)
+    histogram.FillRandom('gaus', 100000)
+    histogram.Write()
+    ofile.Close()
+
+
+def test_styles(config):
+    Styler(config).draw()
