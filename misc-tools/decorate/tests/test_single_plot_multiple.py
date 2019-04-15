a332db16f6ca9ecc20eddd25a9e6f009e4b9d75b
Sasha <q.dev.null@gmail.com>
2019-04-15 20:54:06 +0200

    Remove single plot with multiple histograms

--- misc-tools/decorate/tests/test_single_plot_multiple.py
+++ misc-tools/decorate/tests/test_single_plot_multiple.py
@@ -1,50 +1,59 @@
-#!/usr/bin/python
-
-
+import os
 import ROOT
 import json
-from tests.general_test import TestImages, GeneralTest
+import pytest
+from drawtools.style import Styler
 
 
-class SingleImageMultiplePlots(TestImages, GeneralTest):
-
-    def save_config(self):
-        conffile = 'config/test_single_multiple.json'
-        filename = 'input/testfile_singlemultiple.root'
-        histnames = ['hFirst', 'hSecond', 'hThird', 'hFourth']
-        pfile = 'results/test.pdf'
-        data = {
-            "histograms":
+@pytest.fixture
+def config():
+    conffile = 'config/test_single_multiple.json'
+    filename = 'input/testfile_singlemultiple.root'
+    histnames = ['hFirst', 'hSecond', 'hThird', 'hFourth']
+    pfile = 'results/test.pdf'
+    data = {
+        "histograms":
+        {
+            filename + '/' + histname:
             {
-                filename + '/' + histname:
-                {
-                    "label": "plot %d" % i,
-                    "color": 1000 + i,
-                    "title": "Testing same plots ;x - quantity; y - observable"
-                }
-                for i, histname in enumerate(histnames)
-            },
-            "canvas":
-            {
-                "size": 5,
-                "logy": 1,
-                "gridx": 1,
-                "gridy": 1, "legend": [0.78, 0.7, 0.89, 0.88],
-                "output": pfile
+                "label": "plot %d" % i,
+                "color": 1000 + i,
+                "title": "Testing same plots ;x - quantity; y - observable"
             }
+            for i, histname in enumerate(histnames)
+        },
+        "canvas":
+        {
+            "size": 5,
+            "logy": 1,
+            "gridx": 1,
+            "gridy": 1, "legend": [0.78, 0.7, 0.89, 0.88],
+            "output": pfile
         }
+    }
 
-        with open(conffile, 'w') as outfile:
-            json.dump(data, outfile)
+    with open(conffile, 'w') as outfile:
+        json.dump(data, outfile)
 
-        return conffile, filename, histnames
+    yield conffile, histnames
 
-    def save_histogram(self, filename, histnames):
-        ofile = ROOT.TFile(filename, "update")
-        for i, histname in enumerate(histnames):
-            f = ROOT.TF1('f%d' % i, 'TMath::Exp(- %0.2f * x)' % (i / 10.), 0, 20)
-            histogram = ROOT.TH1F(
-                histname, "Testing %dth iteration ..." % i, 1000, 0, 20)
-            histogram.FillRandom('f%d' % i, 100000)
-            histogram.Write()
-        ofile.Close()
+    os.remove(conffile)
+    os.remove(filename)
+
+
+def save_histogram(filename, histnames):
+    ofile = ROOT.TFile(filename, "update")
+    for i, histname in enumerate(histnames):
+        f = ROOT.TF1('f%d' % i, 'TMath::Exp(- %0.2f * x)' % (i / 10.), 0, 20)
+        histogram = ROOT.TH1F(
+            histname, "Testing %dth iteration ..." % i, 1000, 0, 20)
+        histogram.FillRandom('f%d' % i, 100000)
+        histogram.Write()
+        del f
+    ofile.Close()
+
+
+@pytest.mark.skip("")
+def test_styles(config):
+    conffile, histnames = config
+    Styler(config).draw()
