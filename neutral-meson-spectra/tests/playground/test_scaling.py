a0bb94132b6e5af29a0528c76f30b8c2c82c0add
Sasha <q.dev.null@gmail.com>
2019-04-27 20:02:21 +0200

    Test algorithm for input data scaling

---
+++ neutral-meson-spectra/tests/playground/test_scaling.py
@@ -0,0 +1,40 @@
+import pytest
+
+from lazy_object_proxy import Proxy
+from spectrum.output import AnalysisOutput
+from spectrum.options import Options
+from spectrum.analysis import Analysis
+from vault.datavault import DataVault
+
+from spectrum.comparator import Comparator
+
+INPUT_DATA = Proxy(
+    lambda: DataVault().input("data", histname="MassPtSM0")
+)
+
+
+def scale_data(data, scale=2):
+    real, mixed = data.transform()
+    real.Scale(scale)
+
+    class ScaledInput(object):
+        def transform(self):
+            return (real, mixed)
+    return ScaledInput()
+
+
+@pytest.mark.onlylocal
+@pytest.mark.parametrize("particle", [
+    "#pi^{0}",
+    # "#eta",
+])
+def test_simple(particle):
+    loggs = AnalysisOutput("test the single analysis")
+
+    original = Analysis(Options(particle=particle)).transform(
+        INPUT_DATA, loggs=loggs)
+
+    scaled = Analysis(Options(particle=particle)).transform(
+        scale_data(INPUT_DATA), loggs=loggs)
+
+    Comparator().compare(original.spectrum, scaled.spectrum)
