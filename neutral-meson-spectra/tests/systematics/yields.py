82e9cab28981d890573d6732dc7b9d35d94dd0ce
Sasha <q.dev.null@gmail.com>
2019-05-01 12:12:42 +0200

    Use the official dataset for the final yield systematic s calculation

--- neutral-meson-spectra/tests/systematics/yields.py
+++ neutral-meson-spectra/tests/systematics/yields.py
@@ -2,24 +2,9 @@ import pytest
 
 from spectrum.options import CompositeCorrectedYieldOptions
 from spectrum.output import AnalysisOutput
-from vault.datavault import DataVault
-from uncertainties.yields import YieldExtractioin
+from uncertainties.yields import YieldExtractioin, yield_extraction_data
 from uncertainties.yields import YieldExtractioinUncertanityOptions
 from spectrum.comparator import Comparator
-from tools.feeddown import data_feeddown
-
-
-def cyield_data(particle="#pi^{0}"):
-    data_input = (
-        DataVault().input("data", histname="MassPtSM0"),
-        data_feeddown(dummy=particle == "#eta"),
-    )
-    production = "single %s" % particle
-    mc_inputs = (
-        DataVault().input(production, "low", listname="PhysEff"),
-        DataVault().input(production, "high", listname="PhysEff"),
-    )
-    return data_input, mc_inputs
 
 
 @pytest.mark.thesis
@@ -27,7 +12,7 @@ def cyield_data(particle="#pi^{0}"):
 @pytest.mark.onlylocal
 @pytest.mark.parametrize("particle", [
     "#pi^{0}",
-    "#eta",
+    # "#eta",
 ])
 def test_yield_extraction_uncertanity_pion(particle):
     options = YieldExtractioinUncertanityOptions(
@@ -35,7 +20,7 @@ def test_yield_extraction_uncertanity_pion(particle):
     )
     estimator = YieldExtractioin(options)
     output = estimator.transform(
-        cyield_data(particle),
+        yield_extraction_data(),
         loggs=AnalysisOutput("corrected yield %s" % particle)
     )
     Comparator().compare(output)
