bea103c7efc4df7906df4f675b6b92c80af16adf
Sasha <q.dev.null@gmail.com>
2019-05-10 22:05:03 +0200

    Add tests for corrected yields

--- neutral-meson-spectra/tests/mc/test_ratio.py
+++ neutral-meson-spectra/tests/mc/test_ratio.py
@@ -8,6 +8,11 @@ from spectrum.output import AnalysisOutput
 
 from vault.datavault import DataVault
 
+from spectrum.corrected_yield import YieldRatio, CorrectedYield
+from spectrum.options import CompositeCorrectedYieldOptions
+from spectrum.comparator import Comparator
+from tools.feeddown import data_feeddown
+
 
 @pytest.fixture
 def dataset():
@@ -47,3 +52,64 @@ def test_efficiency(dataset):
     loggs = AnalysisOutput("efficiency_ratio")
     estimator.transform(dataset, loggs)
     loggs.plot()
+
+
+def ratio_inputs():
+
+    # Define the inputs and the dataset for #eta mesons
+    #
+    inputs_eta = (
+        DataVault().input("single #eta", "low"),
+        DataVault().input("single #eta", "high"),
+    )
+
+    data_eta = (
+        (
+            DataVault().input("data", histname="MassPtSM0"),
+            data_feeddown(dummy=True),
+        ),
+        inputs_eta
+    )
+
+    options_eta = CompositeCorrectedYieldOptions(particle="#eta")
+
+    # Define the inputs and the dataset for #pi^{0}
+    #
+    prod = "single #pi^{0}"
+    inputs_pi0 = (
+        DataVault().input(prod, "low", "PhysEff"),
+        DataVault().input(prod, "high", "PhysEff"),
+    )
+
+    data_pi0 = (
+        (
+            DataVault().input("data", histname="MassPtSM0"),
+            data_feeddown(),
+        ),
+        inputs_pi0
+    )
+
+    options_pi0 = CompositeCorrectedYieldOptions(particle="#pi^{0}")
+    # Make same binning for all neutral mesons
+    options_pi0.set_binning(
+        options_eta.analysis.pt.ptedges,
+        options_eta.analysis.pt.rebins
+    )
+    return data_eta, options_eta, data_pi0, options_pi0
+
+
+def test_yield_ratio(ratio_inputs):
+    data_eta, options_eta, data_pi0, options_pi0 = ratio_inputs
+
+    estimator = YieldRatio(
+        options_eta=options_eta,
+        options_pi0=options_pi0,
+        plot=True
+    )
+
+    loggs = AnalysisOutput("eta to pion ratio")
+    output = estimator.transform(
+        (data_eta, data_pi0),
+        loggs=loggs
+    )
+    Comparator().compare(output)
