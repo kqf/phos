cb57fb0e82ae9b7998c9b62dfe0e7f3fe714a25d
Sasha <q.dev.null@gmail.com>
2019-05-10 20:43:18 +0200

    Add pion/eta mc ratio

--- neutral-meson-spectra/tests/mc/test_ratio.py
+++ neutral-meson-spectra/tests/mc/test_ratio.py
@@ -1,8 +1,9 @@
 import pytest
 
 from spectrum.analysis import Analysis
+from spectrum.efficiency import Efficiency
 from spectrum.pipeline import Pipeline, ComparePipeline, HistogramSelector
-from spectrum.options import Options
+from spectrum.options import Options, EfficiencyOptions
 from spectrum.output import AnalysisOutput
 
 from vault.datavault import DataVault
@@ -28,6 +29,20 @@ def test_compare_raw_yields(dataset):
             "analysis", Analysis(Options(particle="#pi^{0}", ptrange=ptrange)),
             "spectrum", HistogramSelector("spectrum")
         ])),
+    ])
+    loggs = AnalysisOutput("raw_yield_ratio")
+    estimator.transform(dataset, loggs)
+    loggs.plot()
+
+
+@pytest.mark.onlylocal
+def test_efficiency(dataset):
+    ptrange = "config/pt-same.json"
+    estimator = ComparePipeline([
+        ("#pi^{0}", Efficiency(
+            EfficiencyOptions(particle="#eta", ptrange=ptrange))),
+        ("#eta", Efficiency(
+            EfficiencyOptions(particle="#pi^{0}", ptrange=ptrange))),
     ])
     loggs = AnalysisOutput("efficiency_ratio")
     estimator.transform(dataset, loggs)
