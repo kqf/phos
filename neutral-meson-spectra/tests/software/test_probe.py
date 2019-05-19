4d66d8f4b7e930fc9ee76dce13452c17e24da030
Sasha <q.dev.null@gmail.com>
2019-05-19 08:51:11 +0200

    Test tag and probe methods on the latest dataset

--- neutral-meson-spectra/tests/software/test_probe.py
+++ neutral-meson-spectra/tests/software/test_probe.py
@@ -1,27 +1,26 @@
 import pytest
 
-from lazy_object_proxy import Proxy
 from tools.probe import TagAndProbe
 from vault.datavault import DataVault
 from spectrum.options import ProbeTofOptions
 from spectrum.output import AnalysisOutput
 
 
-DATASET = Proxy(
-    lambda: (
-        DataVault().input("data", "uncorrected", "TagAndProbleTOFOnlyTender",
+@pytest.fixture
+def data():
+    return (
+        DataVault().input("data", listname="TagAndProbleTOF",
                           histname="MassEnergyTOF_SM0"),
-        DataVault().input("data", "uncorrected", "TagAndProbleTOFOnlyTender",
+        DataVault().input("data", listname="TagAndProbleTOF",
                           histname="MassEnergyAll_SM0"),
-    ),
-)
+    )
 
 
 @pytest.mark.onlylocal
-def test_interface():
+def test_interface(data):
     probe = TagAndProbe(ProbeTofOptions())
     eff = probe.transform(
-        DATASET,
+        data,
         loggs=AnalysisOutput("Test the tag and probe interface")
     )
     assert eff.GetEntries() > 0
