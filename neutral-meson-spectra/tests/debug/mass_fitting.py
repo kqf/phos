1850d1a294d2168804aed9f3da25b5b91771684e
Sasha <q.dev.null@gmail.com>
2019-05-30 18:51:23 +0200

    Fix mass fitting tests

--- neutral-meson-spectra/tests/debug/mass_fitting.py
+++ neutral-meson-spectra/tests/debug/mass_fitting.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import pytest
 import ROOT
 
@@ -5,31 +6,43 @@ from spectrum.comparator import Comparator
 from spectrum.broot import BROOT as br
 
 
-@pytest.mark.skip('')
-def test_masss_fitting(self):
-    filename = "mass.root"
+@pytest.fixture
+def mass(filename="mass.root"):
     infile = ROOT.TFile(filename)
+    histogram = infile.Get("mass_data_copied")
+    return histogram
+
+
+@pytest.fixture
+def width(filename="width.root"):
+    infile = ROOT.TFile(filename)
+    histogram = infile.Get("width_data_copied")
+    return histogram
+
+
+@pytest.mark.skip('')
+@pytest.mark.interactive
+@pytest.mark.onlylocal
+def test_masss_fitting(mass):
     fitf = ROOT.TF1("mass",
                     "TMath::Exp([0] * x) * [1] + [2]", 0, 20)
     fitf.SetParameters(*[-0.38271249157687853,
                          0.0014294353484205393,
                          0.13681699068487807])
-    histogram = infile.Get("mass_data_copied")
-    histogram.Fit(fitf, "R")
-    print br.pars(fitf)
-    Comparator().compare(histogram)
+    mass.Fit(fitf, "R")
+    print("Mass parameters", br.pars(fitf))
+    Comparator().compare(mass)
 
 
-def test_width_fitting(self):
-    filename = "width.root"
-    infile = ROOT.TFile(filename)
+@pytest.mark.interactive
+@pytest.mark.onlylocal
+def test_width_fitting(width):
     fitf = ROOT.TF1("width",
                     "sqrt([0] ** 2 + ([1] / x) ** 2 + ([2] * x) ** 2)", 2, 20)
     fitf.SetParameters(*[0.004073830333585946,
                          0.008147508911668301,
                          0.00019036093293625441])
-    histogram = infile.Get("width_data_copied")
-    histogram.Fit(fitf, "R")
-    print fitf.GetChisquare() / fitf.GetNDF()
-    print br.pars(fitf)
-    Comparator().compare(histogram)
+    width.Fit(fitf, "R")
+    print("Width chi^2/ndf", fitf.GetChisquare() / fitf.GetNDF())
+    print("Width parameters", br.pars(fitf))
+    Comparator().compare(width)
