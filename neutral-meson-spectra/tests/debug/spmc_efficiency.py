c2e8f1fc18cd9849637054fe93dba8cc1f86d8de
Sasha <q.dev.null@gmail.com>
2019-05-31 15:41:58 +0200

    Update the datasets in spmc efficiency overlap debugging

--- neutral-meson-spectra/tests/debug/spmc_efficiency.py
+++ neutral-meson-spectra/tests/debug/spmc_efficiency.py
@@ -9,47 +9,44 @@ from spectrum.output import AnalysisOutput
 from vault.datavault import DataVault
 
 
-@pytest.mark.skip('')
-def test_artificial_efficiency(self):
+def artificial_data(self):
     inputs = (
         "LHC16-single-low.root",
         "LHC16-single.root"
     )
-    evaluate_spmc_efficiency(inputs, "#pi^{0}")
+    return inputs
 
 
-# @pytest.mark.skip('')
-def test_pi0_efficiency(self):
-    production = "single #pi^{0}"
+def pi0_dataset():
     inputs = (
-        DataVault().input(production, "low", "PhysEff"),
-        DataVault().input(production, "high", "PhysEff"),
+        DataVault().input("single #eta", "low", "PhysEff"),
+        DataVault().input("single #eta", "high", "PhysEff"),
     )
-    evaluate_spmc_efficiency(inputs, "#pi^{0}")
+    return inputs
 
 
-@pytest.mark.skip('')
-def test_eta_efficiency(self):
-    # production = "single #eta updated nonlinearity"
-    production = "single #eta new tender"
+def eta_dataset():
     inputs = (
-        DataVault().input(production, "low"),
-        DataVault().input(production, "high")
+        DataVault().input("single #eta", "low"),
+        DataVault().input("single #eta", "high")
     )
-    evaluate_spmc_efficiency(inputs, "#eta")
+    return inputs
 
 
-def evaluate_spmc_efficiency(inputs, particle):
+@pytest.mark.onlylocal
+@pytest.mark.interactive
+@pytest.mark.parametrize("particle, dataset", [
+    ("#pi^{0}", pi0_dataset()),
+    ("#eta", eta_dataset()),
+])
+def test_spmc_efficiency(inputs, particle):
     options = CompositeEfficiencyOptions(particle)
     # for options in options.suboptions:
     # options.analysis.signalp.relaxed = True
     # options.analysis.backgroundp.relaxed = True
 
     loggs = AnalysisOutput("composite_efficiency_spmc_{}".format(particle))
-    efficiency = Efficiency(options).transform(
-        inputs,
-        loggs
-    )
+    efficiency = Efficiency(options).transform(inputs, loggs)
     diff = Comparator()
     diff.compare(efficiency)
     loggs.plot()
