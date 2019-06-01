4a1ec20de90ba301d5009e9529d6e2d3cbdc0259
Sasha <q.dev.null@gmail.com>
2019-06-01 14:59:11 +0200

    Refactor debug efficiency tsallist test

--- neutral-meson-spectra/tests/debug/tsallis.py
+++ neutral-meson-spectra/tests/debug/tsallis.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import pytest  # noqa
 import ROOT
 
@@ -8,15 +9,20 @@ from spectrum.input import SingleHistInput
 from spectrum.broot import BROOT as br
 
 
-def test_weight_like_debug():
+@pytest.fixture
+def low_pt():
     input_low = DataVault().input(
         "debug efficiency", "low", n_events=1e6,
         histnames=('hSparseMgg_proj_0_1_3_yx', ''))
+    return input_low
 
+
+@pytest.onlylocal
+@pytest.interactive
+def test_weight_like_debug(low_pt, rrange=(0, 20)):
     # Define the transformations
-    nominal_low = SingleHistInput("hGenPi0Pt_clone").transform(input_low)
+    nominal_low = SingleHistInput("hGenPi0Pt_clone").transform(low_pt)
 
-    rrange = 0, 20
     tsallis = ROOT.TF1("f", FVault().func("tsallis"), *rrange)
     parameters = [0.6216964179825611 / 0.0989488446585,
                   0.1327837274571318,
@@ -30,31 +36,39 @@ def test_weight_like_debug():
     tsallis.SetLineColor(ROOT.kRed + 1)
 
     br.scalew(nominal_low)
+
     nominal_low.Scale(1. / nominal_low.Integral())
     nominal_low.Fit(tsallis)
     nominal_low.logy = True
-    print tsallis.GetChisquare() / tsallis.GetNDF()
-    print br.pars(tsallis)
+
+    print(tsallis.GetChisquare() / tsallis.GetNDF())
+    print(br.pars(tsallis))
     Comparator().compare(nominal_low)
-    print tsallis.Integral(0, 20)
+    print(tsallis.Integral(0, 20))
 
 
-def test_different_inputs():
+@pytest.fixture
+def data():
     prod = "single #pi^{0} debug7"
     ll = "debug-ledger.json"
-    production = SingleHistInput("hPt_#pi^{0}_primary_").transform(
-        DataVault(ll).input(prod, "high", listname="PhysEff")
-    )
+    return DataVault(ll).input(prod, "high", listname="PhysEff")
+
+
+@pytest.fixture
+def high_pt():
+    return DataVault().input(
+        "debug efficiency", "high", n_events=1e6,
+        histnames=('hSparseMgg_proj_0_1_3_yx', ''))
+
+
+@pytest.mark.onlylocal
+@pytest.mark.interactive
+def test_different_inputs(data, high_pt):
+    production = SingleHistInput("hPt_#pi^{0}_primary_").transform(data)
     production.label = "data"
     production.logy = True
     br.scalew(production)
 
-    nominal = SingleHistInput("hGenPi0Pt_clone").transform(
-        DataVault().input(
-            "debug efficiency", "high", n_events=1e6,
-            histnames=('hSparseMgg_proj_0_1_3_yx', ''))
-    )
+    nominal = SingleHistInput("hGenPi0Pt_clone").transform(high_pt)
     nominal.label = "nominal"
-    # br.scalew(nominal)
-
     Comparator().compare(*br.rebin_as(production, nominal))
