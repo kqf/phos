6520ca535ec3a32ef88ad6009458727723fcf528
Sasha <q.dev.null@gmail.com>
2019-06-04 18:57:09 +0200

    Run feddown scripts onlylocal

--- neutral-meson-spectra/analysis/efficiency/feeddown.py
+++ neutral-meson-spectra/analysis/efficiency/feeddown.py
@@ -1,16 +1,19 @@
+from __future__ import print_function
+
+import pytest
 from tools.feeddown import FeeddownEstimator, data_feeddown
 
 from spectrum.options import FeeddownOptions
-from spectrum.output import AnalysisOutput
+from spectrum.output import open_loggs
 from spectrum.comparator import Comparator
 
 
+@pytest.mark.onlylocal
 def test_feeddown_correction():
     options = FeeddownOptions()
     estimator = FeeddownEstimator(options)
-    loggs = AnalysisOutput("feeddown correction")
-    output = estimator.transform(data_feeddown(), loggs)
-    # loggs.plot()
-    print "first bin", output.GetBinContent(1)
-    Comparator().compare([output])
+    with open_loggs("feeddown correction", save=False) as loggs:
+        output = estimator.transform(data_feeddown(), loggs)
+        print("first bin", output.GetBinContent(1))
+        Comparator().compare([output])
     assert output.GetEntries() > 0
