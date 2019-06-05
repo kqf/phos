caa6cde0c1f16e69b14e48092d7c4b61530a085b
Sasha <q.dev.null@gmail.com>
2019-06-05 19:56:44 +0200

    Use the single data source for efficiency calculation

--- neutral-meson-spectra/analysis/efficiency/pythia_efficiency.py
+++ neutral-meson-spectra/analysis/efficiency/pythia_efficiency.py
@@ -1,30 +1,28 @@
 import pytest
 
-from lazy_object_proxy import Proxy
 from spectrum.efficiency import Efficiency
 from spectrum.options import EfficiencyOptions
-from spectrum.output import AnalysisOutput
+from spectrum.output import open_loggs
 # from spectrum.broot import BROOT as br
 from spectrum.comparator import Comparator  # noqa
 
 # from tools.validate import validate
 from vault.datavault import DataVault
 
-DATASET = Proxy(
-    lambda: DataVault().input("pythia8", listname="PhysEff")
-)
+
+@pytest.fixture
+def data():
+    return DataVault().input("pythia8", listname="PhysEff")
 
 
 @pytest.mark.thesis
 @pytest.mark.onlylocal
-@pytest.mark.parametrize("data, particle", [
-    (DATASET, "#pi^{0}"),
-    # (ETA_INPUT, "#eta"),
+@pytest.mark.parametrize("particle", [
+    "#pi^{0}",
+    "#eta",
 ])
 def test_efficiency(data, particle):
     options = EfficiencyOptions(particle)
-    loggs = AnalysisOutput("efficiency_spmc_{}".format(particle))
-    efficiency = Efficiency(options).transform(data, loggs)
-    # validate(br.hist2dict(efficiency), "spmc_efficiency/" + particle)
-    Comparator().compare(efficiency)
-    return efficiency
+    with open_loggs("efficiency pytia8 {}".format(particle)) as loggs:
+        efficiency = Efficiency(options).transform(data, loggs)
+        Comparator().compare(efficiency)
