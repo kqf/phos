6dc1d63cc4a52f09a1333948cb024441abfa54f0
Sasha <q.dev.null@gmail.com>
2019-06-10 21:20:12 +0200

    Test the newer set of parameters for the nonlinearaity function

--- neutral-meson-spectra/tests/mc/single/nonlinearity.py
+++ neutral-meson-spectra/tests/mc/single/nonlinearity.py
@@ -4,7 +4,7 @@ import pytest
 
 from vault.datavault import DataVault
 from spectrum.options import CompositeNonlinearityOptions
-from spectrum.output import AnalysisOutput  # noqa
+from spectrum.output import open_loggs  # noqa
 from tools.mc import Nonlinearity
 from tools.mc import Decalibration
 from spectrum.broot import BROOT as br
@@ -22,6 +22,7 @@ def data():
     )
 
 
+@pytest.fixture
 def nonlinearity_function():
     # func_nonlin = ROOT.TF1(
     #     "func_nonlin", FVault().func("nonlinearity"), 0, 20)
@@ -31,21 +32,23 @@ def nonlinearity_function():
         "func_nonlin",
         "[2]*(1.+[0]/(1. + TMath::Power(x/[1],2)))", 0, 100)
     func_nonlin.SetParameters(-0.06, 0.7, 1.015)
+    # func_nonlin.SetParameters(-0.04, 0.73, 1.025)
     return func_nonlin
 
 
 @pytest.mark.onlylocal
 @pytest.mark.interactive
-def test_spmc_nonlinearity(data):
+def test_spmc_nonlinearity(data, nonlinearity_function):
     options = CompositeNonlinearityOptions()
-    options.fitf = nonlinearity_function()
+    options.fitf = nonlinearity_function
 
-    nonlinearity = Nonlinearity(options, plot=True).transform(
-        data, AnalysisOutput("spmc nonlinearity"))
-    print("Fit parameters:", br.pars(options.fitf))
-    Comparator().compare(nonlinearity)
+    with open_loggs("spmc nonlinearity parametrization") as loggs:
+        nonlinearity = Nonlinearity(options, plot=True).transform(data, loggs)
+        print("Fit parameters:", br.pars(options.fitf))
+        Comparator().compare(nonlinearity)
 
-    width = Decalibration(options, plot=True).transform(
-        data, AnalysisOutput("spmc width"))
-    Comparator().compare(width)
+    with open_loggs("spmc width parametrization") as loggs:
+        width = Decalibration(options, plot=True).transform(data, loggs)
+        Comparator().compare(width)
+
     assert width.GetEntries() > 0
