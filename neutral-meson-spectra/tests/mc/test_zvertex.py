30b83b4fed6363abc65516b0d5990b143a3150aa
Sasha <q.dev.null@gmail.com>
2019-06-11 12:01:32 +0200

    Include the pythia8 with the Zvertex distribution to the data/MC comparison

--- neutral-meson-spectra/tests/mc/test_zvertex.py
+++ neutral-meson-spectra/tests/mc/test_zvertex.py
@@ -3,19 +3,26 @@ import pytest
 
 from spectrum.input import SingleHistInput
 from spectrum.pipeline import ComparePipeline
+from spectrum.output import open_loggs
 
 from vault.datavault import DataVault
 
 
+@pytest.fixture
+def data():
+    return (
+        DataVault().input("data", listname="Qual", label="data"),
+        DataVault().input("pythia8", listname="Qual", label="pythia8"),
+    )
+
+
 @pytest.mark.onlylocal
 @pytest.mark.interactive
-def test_compare_pipeline():
-    # TODO: Add Qual selection to the
-    data = (
-        DataVault().input("data", listname="Qual", label="data"),
-        # DataVault().input("pythia8", listname="Qual", label="pythia8"),
-    )
+def test_compare_pipeline(data):
     pipeline = ComparePipeline([
         (dataset.label, SingleHistInput("hZvertex"))
-        for dataset in data])
-    pipeline.transform(data, {})
+        for dataset in data
+    ])
+
+    with open_loggs("Z vertex") as loggs:
+        pipeline.transform(data, loggs)
