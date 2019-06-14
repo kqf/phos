14b9002128c692ca20ef12c6b0ab35feeb0d5818
Sasha <q.dev.null@gmail.com>
2019-06-14 19:37:39 +0200

    Update the single particle mc generation

--- neutral-meson-spectra/tests/playground/test_simulate_algorithm.py
+++ neutral-meson-spectra/tests/playground/test_simulate_algorithm.py
@@ -4,18 +4,29 @@ from spectrum.analysis import Analysis
 from spectrum.options import Options
 from spectrum.input import Input
 from spectrum.comparator import Comparator
+from spectrum.output import open_loggs
 
 from spectrum.broot import BROOT as br
 from tests.playground.phspace import InclusiveGenerator
 from vault.datavault import DataVault
 
 
+@pytest.fixture
+def data():
+    return DataVault().file("data")
+
+
+@pytest.fixture
+def data_spmc():
+    return DataVault().file("single #pi^{0}", "low")
+
+
 @pytest.mark.skip("")
 @pytest.mark.onlylocal
-def test_recreates_the_same_shape():
+def test_recreates_the_same_shape(data):
     gen_file_name = "LHC16-fake.root"
     generator = InclusiveGenerator(
-        DataVault().file("data"),
+        data,
         "config/test_algorithm.json",
         gen_file_name=gen_file_name,
         flat=True
@@ -24,9 +35,11 @@ def test_recreates_the_same_shape():
     generated = generator.generate(1000)
     generated.priority = 1
     generated.logy = 1
-    reconstructed = Analysis(Options()).transform(
-        Input(gen_file_name, generator.selname), {}
-    )
+
+    with open_loggs("reconstructing generated spectra") as loggs:
+        reconstructed = Analysis(Options()).transform(
+            Input(gen_file_name, generator.selname), loggs
+        )
     Comparator.compare(
         map(br.scalew, [reconstructed, generated])
     )
@@ -34,9 +47,9 @@ def test_recreates_the_same_shape():
 
 @pytest.mark.skip("")
 @pytest.mark.onlylocal
-def test_generate_mc():
+def test_generate_mc(data_spmc):
     generator = InclusiveGenerator(
-        DataVault().file("single #pi^{0}", "low"),
+        data_spmc,
         "config/test_algorithm.json",
         selname="PhysEff",
         gen_file_name="LHC16-single.root",
