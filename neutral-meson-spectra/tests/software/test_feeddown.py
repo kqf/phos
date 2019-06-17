0db1b1d64eee37bcd0f00a9d1aea3de7ff78fd1b
Sasha <q.dev.null@gmail.com>
2019-06-17 19:10:38 +0200

    Remove loggs from the feeddown tests

--- neutral-meson-spectra/tests/software/test_feeddown.py
+++ neutral-meson-spectra/tests/software/test_feeddown.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from tools.feeddown import FeeddownEstimator, data_feeddown
 from spectrum.options import FeeddownOptions
-from spectrum.output import AnalysisOutput
+from spectrum.output import open_loggs
 try:
     import root_numpy as rp
 except ValueError:
@@ -12,11 +12,9 @@ except ValueError:
 
 @pytest.mark.onlylocal
 def test_pion():
-    estimator = FeeddownEstimator(FeeddownOptions())
-    output = estimator.transform(
-        data_feeddown(),
-        AnalysisOutput("feeddown correction")
-    )
+    estimator = FeeddownEstimator(FeeddownOptions(particle="#pi^{0}"))
+    with open_loggs() as loggs:
+        output = estimator.transform(data_feeddown(), loggs)
     assert output.GetEntries() > 0
 
 
@@ -24,15 +22,15 @@ def test_pion():
 def test_handles_non_pions_with_data():
     estimator = FeeddownEstimator(FeeddownOptions(particle="#eta"))
     with pytest.raises(IOError):
-        estimator.transform(data_feeddown(), "")
+        with open_loggs() as loggs:
+            estimator.transform(data_feeddown(), loggs)
 
 
 @pytest.mark.onlylocal
 def test_eta():
     estimator = FeeddownEstimator(FeeddownOptions(particle="#eta"))
-    output = estimator.transform(
-        None,
-        AnalysisOutput("feeddown correction")
-    )
+    with open_loggs() as loggs:
+        output = estimator.transform(None, loggs)
+
     answer = rp.hist2array(output)
     assert np.all(answer == np.ones_like(answer))
