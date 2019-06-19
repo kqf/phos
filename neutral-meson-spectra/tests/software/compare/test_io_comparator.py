5efd11547f1af5b63228362a6c82e780ff18b3ca
Sasha <q.dev.null@gmail.com>
2019-06-19 20:33:02 +0200

    Update the io tests

--- neutral-meson-spectra/tests/software/compare/test_io_comparator.py
+++ neutral-meson-spectra/tests/software/compare/test_io_comparator.py
@@ -1,3 +1,4 @@
+import pytest
 import os
 
 import ROOT
@@ -5,13 +6,23 @@ import spectrum.sutils as su
 from spectrum.comparator import Comparator
 
 
-def test_saves_output(data, stop, fname="test_io_deleteme.root"):
-    ofile = ROOT.TFile(fname, "recreate")
-    ofile.mkdir("test1/test2/")
-    Comparator(stop=stop).compare(data)
-    ofile.cd("test1/test2/")
-    su.gcanvas().Write()
-    ofile.Write()
-    infile = ROOT.TFile(fname)
-    infile.ls()
-    os.remove(fname)
+@pytest.fixture
+def fname():
+    name = "test_io_deleteme.root"
+    yield name
+    os.remove(name)
+
+
+def test_saves_output(data, stop, fname):
+    with su.rfile(fname) as ofile:
+        ofile.mkdir("test1/test2/")
+        Comparator(stop=stop).compare(data)
+
+        ofile.cd("test1/test2/")
+        su.gcanvas().Write()
+
+    with su.rfile(fname, "read") as infile:
+        infile = ROOT.TFile(fname)
+        infile.ls()
+
+    assert os.path.isfile(fname)
