07e6f4a64e792d8b53ba3bbc532f8ab61b23b5a2
Sasha <q.dev.null@gmail.com>
2019-06-23 20:49:33 +0200

    Update the sutils tests

--- neutral-meson-spectra/tests/software/test_sutils.py
+++ neutral-meson-spectra/tests/software/test_sutils.py
@@ -15,47 +15,44 @@ def stop():
 
 
 def test_draws_histogram(stop):
-    hist = ROOT.TH1F(
-        'histWait', 'Test: Drawing with no global canvas', 600, -3, 3)
-    hist.FillRandom('gaus')
-    hist.Draw()
-    st.wait(stop=stop)
+    with st.canvas():
+        hist = ROOT.TH1F(
+            'histWait', 'Test: Drawing with no global canvas', 600, -3, 3)
+        hist.FillRandom('gaus')
+        hist.Draw()
 
 
 def test_draws_rescaled(stop):
-    st.gcanvas(x=1., resize=True)
-    hist = ROOT.TH1F(
-        'histNormal', 'Test: Drawing normal scale first', 600, -3, 3)
-    hist.FillRandom('gaus')
-    hist.Draw()
-    st.wait(stop=stop)
+    with st.canvas(x=1., resize=True, stop=stop):
+        hist = ROOT.TH1F(
+            'histNormal', 'Test: Drawing normal scale first', 600, -3, 3)
+        hist.FillRandom('gaus')
+        hist.Draw()
 
-    st.gcanvas(x=1. / 2, resize=True)
-    hist = ROOT.TH1F(
-        'histHalfSize', 'Test: Drawing half-size scale', 600, -3, 3)
-    hist.FillRandom('gaus')
-    hist.SetLineColor(37)
-    hist.Draw()
-    st.wait(stop=stop)
+    with st.canvas(x=1. / 2, resize=True, stop=stop):
+        hist = ROOT.TH1F(
+            'histHalfSize', 'Test: Drawing half-size scale', 600, -3, 3)
+        hist.FillRandom('gaus')
+        hist.SetLineColor(37)
+        hist.Draw()
 
-    st.gcanvas(x=1., resize=True)
-    hist = ROOT.TH1F('histNormalAgain',
-                     'Test: Drawing normal scale again', 600, -3, 3)
-    hist.FillRandom('gaus')
-    hist.SetLineColor(46)
-    hist.Draw()
-    st.wait(stop=stop)
+    with st.canvas(x=1., resize=True, stop=stop):
+        hist = ROOT.TH1F('histNormalAgain',
+                         'Test: Drawing normal scale again', 600, -3, 3)
+        hist.FillRandom('gaus')
+        hist.SetLineColor(46)
+        hist.Draw()
 
 
 def test_doesnt_draw_outiside_the_scope(stop):
     def drawfunc():
-        st.gcanvas(x=1., resize=True)
-        hist = ROOT.TH1F(
-            'histInnerScopeFail',
-            'Test: you are not supposed to see this histogram',
-            600, -3, 3)
-        hist.FillRandom('gaus')
-        hist.Draw()
+        with st.canvas():
+            hist = ROOT.TH1F(
+                'histInnerScopeFail',
+                'Test: you are not supposed to see this histogram',
+                600, -3, 3)
+            hist.FillRandom('gaus')
+            hist.Draw()
     drawfunc()
     t = ROOT.TText(.5, .5, "This pad should be empty")
     t.SetTextAlign(22)
@@ -68,14 +65,11 @@ def test_doesnt_draw_outiside_the_scope(stop):
 
 def test_draws_outside_the_scope(stop):
     def drawfunc():
-        st.gcanvas(x=1., resize=True)
-        hist = ROOT.TH1F(
-            'histInnerScopeOk',
-            'Test: Drawing histogram outiside the inner scope',
-            600, -3, 3)
-        hist.FillRandom('gaus')
-        hist.Draw()
+        with st.canvas():
+            hist = ROOT.TH1F(
+                'histInnerScopeOk',
+                'Test: Drawing histogram outiside the inner scope',
+                600, -3, 3)
+            hist.FillRandom('gaus')
         return hist
-
     hist = drawfunc()  # noqa root needs this object
-    st.wait(stop=stop)
