10bc525a2da08e7bfc1f00540a14749a83d8684d
Sasha <q.dev.null@gmail.com>
2019-06-24 09:33:18 +0200

    Register exclude marks in the conftest

--- neutral-meson-spectra/tests/conftest.py
+++ neutral-meson-spectra/tests/conftest.py
@@ -7,3 +7,10 @@ def stop():
     not_discover = 'discover' not in sys.argv
     not_pytest = 'pytest' not in sys.argv[0]
     return not_discover and not_pytest
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "onlylocal: mark test to run only as they require the data",
+        "markers", "interactive: exclude scripts with ROOT pop-up windows",
+    )
