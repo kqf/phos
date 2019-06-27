876cc2a9c67de3976a0b4e1b416b33049d63fc34
Sasha <q.dev.null@gmail.com>
2019-06-27 22:10:53 +0200

    Add logging in the nonlinearity uncertainty

--- neutral-meson-spectra/tests/systematics/nonlinearity.py
+++ neutral-meson-spectra/tests/systematics/nonlinearity.py
@@ -1,26 +1,31 @@
 import pytest
 
 from spectrum.comparator import Comparator
-from spectrum.output import AnalysisOutput
+from spectrum.output import open_loggs
 
 from uncertainties.nonlinearity import Nonlinearity, nonlinearity_scan_data
 from uncertainties.nonlinearity import CompositeNonlinearityUncertaintyOptions
 
 
+@pytest.fixture
+def nbins():
+    return 2
+
 # TODO: Look at generated histogram in different selection
 #       fix this asap
+
 
 @pytest.mark.thesis
 @pytest.mark.interactive
 @pytest.mark.onlylocal
-def test_nonlinearity_uncertainty():
-    nbins = 2
+def test_nonlinearity_uncertainty(nbins):
     prod = "single #pi^{0} nonlinearity scan"
     options = CompositeNonlinearityUncertaintyOptions(nbins=nbins)
     options.factor = 1.
 
-    chi2ndf = Nonlinearity(options).transform(
-        nonlinearity_scan_data(nbins, prod),
-        loggs=AnalysisOutput("testing the scan interface")
-    )
+    with open_loggs() as loggs:
+        chi2ndf = Nonlinearity(options).transform(
+            nonlinearity_scan_data(nbins, prod),
+            loggs
+        )
     Comparator().compare(chi2ndf)
