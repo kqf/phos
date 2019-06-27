fb82cc80da6417a62a99b2574b8a3c6d63c9f748
Sasha <q.dev.null@gmail.com>
2019-06-27 22:07:17 +0200

    Update Global energy scale scripts

--- neutral-meson-spectra/tests/systematics/gscale.py
+++ neutral-meson-spectra/tests/systematics/gscale.py
@@ -1,5 +1,5 @@
 import pytest
-from spectrum.output import AnalysisOutput
+from spectrum.output import open_loggs
 from uncertainties.gscale import GScale, GScaleOptions, ge_scale_data
 from spectrum.comparator import Comparator
 
@@ -13,8 +13,6 @@ from spectrum.comparator import Comparator
 ])
 def test_interface_composite(particle):
     estimator = GScale(GScaleOptions(particle=particle), plot=False)
-    uncertanity = estimator.transform(
-        ge_scale_data(particle),
-        loggs=AnalysisOutput("test composite corr. yield interface")
-    )
+    with open_loggs() as loggs:
+        uncertanity = estimator.transform(ge_scale_data(particle), loggs)
     Comparator().compare(uncertanity)
