32ce3b7791a688fa6e630092eac9aac5bd1dc8b2
Sasha <q.dev.null@gmail.com>
2019-07-02 20:23:29 +0200

    Add eta pion comparison ratio for spmc

---
+++ neutral-meson-spectra/tests/mc/single/test_eta_pion_ratio.py
@@ -0,0 +1,32 @@
+import pytest
+from spectrum.comparator import Comparator
+from spectrum.input import SingleHistInput
+from spectrum.pipeline import ComparePipeline
+from spectrum.pipeline import TransformerBase
+from spectrum.output import open_loggs
+from vault.datavault import DataVault
+
+
+@pytest.fixture
+def data():
+    return (
+        DataVault().input("single #pi^{0}", "high", listname="PhysEff"),
+        DataVault().input("single #pi^{0}", "high", listname="PhysEff"),
+    )
+
+
+class EtaPionRatio(TransformerBase):
+    def __init__(self, options, plot=False):
+        super(EtaPionRatio, self).__init__()
+        self.pipeline = ComparePipeline([
+            ("#eta", SingleHistInput("hPtLong_#eta")),
+            ("#pi^{0}", SingleHistInput("hPtLong_#pi^{0}")),
+        ], plot)
+
+
+@pytest.mark.onlylocal
+@pytest.mark.interactive
+def test_calculate_eta_pion_ratio(data):
+    with open_loggs("eta pion ratio generated") as loggs:
+        ratio = EtaPionRatio(None).transform(data, loggs)
+        Comparator().compare(ratio)
