e6248040c9ad14b1ee0f658536882eba62989bfd
Sasha <q.dev.null@gmail.com>
2019-07-05 07:46:16 +0200

    Add the corrected yield tests for pion and eta with ffake pythia

--- neutral-meson-spectra/tests/mc/eta_pion_ratio/raw.py
+++ neutral-meson-spectra/tests/mc/eta_pion_ratio/raw.py
@@ -3,8 +3,10 @@ import pytest
 from spectrum.efficiency import Efficiency
 from spectrum.pipeline import ComparePipeline
 from spectrum.options import EfficiencyOptions, CompositeEfficiencyOptions
-from spectrum.options import Options
+from spectrum.options import Options, CorrectedYieldOptions
 from spectrum.output import open_loggs
+from spectrum.corrected_yield import YieldRatio
+from tools.feeddown import data_feeddown
 
 from vault.datavault import DataVault
 from spectrum.comparator import Comparator
@@ -33,7 +35,7 @@ class RawSpectrum(TransformerBase):
         ])
 
 
-# @pytest.mark.skip("")
+@pytest.mark.skip("")
 @pytest.mark.onlylocal
 @pytest.mark.interactive
 def test_raw_yield_ratio(pythia, data):
@@ -56,34 +58,63 @@ def test_raw_yield_ratio(pythia, data):
     Comparator(labels=["data", "pythia"]).compare(data_ratio, pythia_ratio)
 
 
-@pytest.mark.skip("")
+@pytest.fixture
+def options():
+    options_eta = CorrectedYieldOptions(particle="#eta")
+    options_pi0 = CorrectedYieldOptions(particle="#pi^{0}")
+    # Make same binning for all neutral mesons
+    options_pi0.set_binning(
+        options_eta.analysis.pt.ptedges,
+        options_eta.analysis.pt.rebins
+    )
+    return options_eta, options_pi0
+
+
+@pytest.fixture
+def data_eta(pythia):
+    # Define the inputs and the dataset for #eta mesons
+    #
+    data = (
+        (
+            DataVault().input("data", histname="MassPtSM0"),
+            data_feeddown(dummy=True),
+        ),
+        pythia
+    )
+    return data
+
+
+@pytest.fixture
+def data_pion(pythia):
+    data = (
+        (
+            DataVault().input("data", histname="MassPtSM0"),
+            data_feeddown(),
+        ),
+        pythia
+    )
+    return data
+
+
+@pytest.fixture
+def dataset(data_eta, data_pion):
+    return data_eta, data_pion
+
+
 @pytest.mark.onlylocal
 @pytest.mark.interactive
-def test_efficiency_ratio(pythia, spmc):
-    ptrange = "config/pt-same.json"
-    estimator = ComparePipeline([
-        ("#eta", Efficiency(
-            EfficiencyOptions(particle="#eta", ptrange=ptrange, scale=1))),
-        ("#pi^{0}", Efficiency(
-            EfficiencyOptions(particle="#pi^{0}", ptrange=ptrange, scale=1))),
-    ], plot=True)
+def test_yield_ratio(dataset, options):
+    options_eta, options_pi0 = options
+
+    estimator = YieldRatio(
+        options_eta=options_eta,
+        options_pi0=options_pi0,
+        plot=True
+    )
 
     with open_loggs() as loggs:
-        pythia_ratio = estimator.transform(pythia, loggs)
-
-    # Now the similar code for SPMC
-    estimator = ComparePipeline([
-        ("#eta", Efficiency(
-            CompositeEfficiencyOptions(particle="#eta", ptrange=ptrange,
-                                       scale=1))),
-        ("#pi^{0}", Efficiency(
-            CompositeEfficiencyOptions(particle="#pi^{0}", ptrange=ptrange,
-                                       scale=1))),
-    ], plot=True)
-    with open_loggs() as loggs:
-        spmc_ratio = estimator.transform(spmc, loggs)
-
-    Comparator(labels=["spmc", "pythia"]).compare(spmc_ratio, pythia_ratio)
+        output = estimator.transform(dataset, loggs)
+    Comparator().compare(output)
 
 
 @pytest.mark.skip("")
