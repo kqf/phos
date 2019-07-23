854decdb7a6fc50ba5507939851bd670c76f2d6e
Oleksandr Kovalenko <oleksandr.kovalenko@cern.ch>
2019-07-23 20:39:01 +0200

    Add the preliminary LHC18 pass datasets

--- protons/datasets/scripts/list-period.sh
+++ protons/datasets/scripts/list-period.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-YEAR=2017
-PERIOD=LHC17c
-PASS='pass1'
+YEAR=2018
+PERIODS="LHC18b LHC18c LHC18d LHC18e LHC18f LHC18g LHC18h LHC18i LHC18j LHC18k LHC18l LHC18m LHC18n LHC18o LHC18p"
+PASS="pass1"
 
 
 function find_runs()
 {
-    alien_find /alice/data/$YEAR/$PERIOD -z -name AliAOD.root | grep -e "/pass1/" | cut -d '/' -f 6 | sort | uniq
+    period=$1
+    alien_find /alice/data/$YEAR/$period -z -name AliAOD.root | grep -e "/pass1/" | cut -d '/' -f 6 | sort | uniq
 }
 
-find_runs
+
+function main()
+{
+    for period in $PERIODS
+    do
+        echo $period
+        find_runs $period > ../$period-$PASS.txt
+        wc -l ../$period-$PASS.txt
+    done
+}
+
+
+main
