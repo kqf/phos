1953f43592d1eb74c2f8667a644fbf3f7afd0047
Sasha <q.dev.null@gmail.com>
2019-07-26 20:27:04 +0200

    Fix the download script for qa files

--- qa/cell-qa/scripts/download_trains.sh
+++ qa/cell-qa/scripts/download_trains.sh
@@ -1,17 +1,21 @@
 
 function download_from_grid()
 {
+	mhome=$1
+    odir=$2
+    train=$3
+
+    echo "Downloading the data from grid: $mhome $odir $train"
+
     counter=1
-	MHOME=$1/
-    TRAIN=$3
-    for run in $(alien_ls $MHOME/); do
-    	filepath=$MHOME/$run/$TRAIN/CaloCellsQA.root
+    for run in $(alien_ls $mhome/); do
+    	filepath=$mhome/$run/$train/CaloCellsQA.root
         echo "Downloading from:"
         echo $filepath
     	output=$(alien_ls "$filepath") 
         if [ "$output" == "CaloCellsQA.root" ]; then
         	echo "Downloading run " $run
-	        alien_cp alien://$filepath $2/$((counter++)).$run.root
+	        alien_cp alien://$filepath $odir/$((counter++)).$run.root
         fi
         # echo $run
     done
