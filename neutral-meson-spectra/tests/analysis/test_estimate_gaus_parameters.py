f64891f6d9bbb1dd1c5dd7288da01d12d69fd3cd
Sasha <q.dev.null@gmail.com>
2019-08-25 22:17:31 +0200

    Add cball parameters estimation

---
+++ neutral-meson-spectra/tests/analysis/test_estimate_gaus_parameters.py
@@ -0,0 +1,37 @@
+import pytest
+
+from spectrum.output import open_loggs
+from spectrum.options import Options
+from spectrum.analysis import Analysis
+from spectrum.pipeline import ComparePipeline
+from vault.datavault import DataVault
+
+
+@pytest.fixture
+def data():
+    return [
+        DataVault().input("data", histname="MassPtSM0"),
+        DataVault().input("data", histname="MassPtSM0"),
+    ]
+
+
+@pytest.fixture
+def options(particle):
+    return {
+        "gaus": Options(particle=particle, signal="config/data/cball.json"),
+        "cball": Options(particle=particle, signal="config/data/gaus.json"),
+    }
+
+
+@pytest.mark.onlylocal
+@pytest.mark.parametrize("particle", [
+    "#pi^{0}",
+    # "#eta",
+])
+def test_simple(particle, data, options):
+    estimator = ComparePipeline([
+        (name, Analysis(options[name]))
+        for name in options
+    ])
+    with open_loggs("estimate gaus parameters") as loggs:
+        estimator.transform(data, loggs=loggs)
