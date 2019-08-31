a07cd94c4404aeca5e0e6fb99ec1b69a26f155ac
Sasha <q.dev.null@gmail.com>
2019-08-31 19:55:03 +0200

    Add the generating/fitting tests to playground

---
+++ misc-tools/playground/fit.py
@@ -0,0 +1,39 @@
+import ROOT
+from contextlib import contextmanager
+
+
+def fitfunctions():
+    formula = "[0] + [1]*(x-{mass:.3f}) + [2]*(x-{mass:.3f})^2"
+    background = ROOT.TF1("background", formula.format(mass=0.135), 0, 1.5)
+    signal = ROOT.TF1("asignal", "gaus", 0, 1.5)
+    return signal, background
+
+
+@contextmanager
+def canvas():
+    canvas = ROOT.TCanvas("canvas", "", 500, 500)
+    yield canvas
+    canvas.Update()
+    input()
+
+
+def main():
+    infile = ROOT.TFile.Open("data.root")
+    hist = infile.invmass
+    signal, background = fitfunctions()
+
+    fitf = ROOT.TF1("func", "{}(0) + {}(3)".format(
+        signal.GetName(), background.GetName()), 0., 1.5)
+    print(fitf)
+    fitf.SetParameters(*[0.98, 0.1368, 0.008, 0.0131, -0.0016, 0])
+    with canvas():
+        initial = fitf.Clone()
+        initial.SetLineColor(ROOT.kOrange + 1)
+
+        # hist.Fit(fitf, "R", *[0.06, 0.22])
+        # hist.Draw()
+        initial.Draw()
+
+
+if __name__ == '__main__':
+    main()
---
+++ misc-tools/playground/generate.py
@@ -0,0 +1,40 @@
+import ROOT
+from contextlib import contextmanager
+
+
+@contextmanager
+def canvas():
+    canvas = ROOT.TCanvas("canvas", "", 500, 500)
+    yield canvas
+    canvas.Update()
+    input()
+
+
+@contextmanager
+def tfile(filename, option="RECREATE"):
+    ofile = ROOT.TFile(filename, option)
+    yield ofile
+    ofile.Write()
+    ofile.Close()
+
+
+def main():
+    hist = ROOT.TH1F("invmass", "Peak distribution", 750, 0, 1.5)
+    formula = "[0] + [1]*(x-{mass:.3f}) + [2]*(x-{mass:.3f})^2"
+    background = ROOT.TF1("background", formula.format(mass=0.135), 0, 1.5)
+    background.SetParameters(*[0.0131, -0.0016, 0])
+    hist.FillRandom(background.GetHistogram(), 100000)
+
+    signal = ROOT.TF1("signal", "gaus(0)", 0, 1.5)
+    signal.SetParameters(*[0.0098, 0.1368, 0.008])
+    hist.FillRandom(signal.GetHistogram(), 10000)
+
+    # with canvas():
+    #     hist.Draw()
+
+    with tfile("data.root"):
+        hist.Write()
+
+
+if __name__ == '__main__':
+    main()
