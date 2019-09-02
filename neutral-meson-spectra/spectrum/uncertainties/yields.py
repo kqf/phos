71805183dbdbd97141128ed52f195f9768027818
Sasha <q.dev.null@gmail.com>
2019-09-02 12:46:03 +0200

    Change all parameters of the yield extraction simultaneously

--- neutral-meson-spectra/spectrum/uncertainties/yields.py
+++ neutral-meson-spectra/spectrum/uncertainties/yields.py
@@ -4,6 +4,7 @@ import joblib
 
 from spectrum.broot import BROOT as br
 from spectrum.options import Options, CompositeCorrectedYieldOptions
+from spectrum.options import CompositeEfficiencyOptions
 from spectrum.comparator import Comparator
 from spectrum.corrected_yield import CorrectedYield
 from spectrum.pipeline import TransformerBase
@@ -51,15 +52,31 @@ def _calculate_yields(conf, data):
                     msg = "n#sigma = {} {} {} {}"
                     label = msg.format(nsigmas, par, bckgr, flab)
 
-                    options = Options()
+                    options = Options(
+                        signal=conf.signals[par],
+                        background=conf.signals[par]
+                    )
                     options.calibration.dead = True
                     options.calibration.nsigmas = nsigmas
-                    options.invmass.signal.fitf = par
                     options.invmass.signal.background = bckgr
                     options.invmass.signal.fit_range = frange
                     options.invmass.background.fit_range = frange
+                    options.invmass.signal.background = bckgr
 
                     cyield.analysis = options
+                    cyield.efficiency = CompositeEfficiencyOptions(
+                        conf.particle,
+                        signal=conf.signals[par],
+                        background=conf.signals[par]
+                    )
+                    for eff in cyield.efficiency.suboptions:
+                        eff.analysis.calibration.dead = True
+                        eff.analysis.calibration.nsigmas = nsigmas
+                        eff.analysis.invmass.signal.background = bckgr
+                        eff.analysis.invmass.signal.fit_range = frange
+                        eff.analysis.invmass.background.fit_range = frange
+                        eff.analysis.invmass.signal.background = bckgr
+
                     with open_loggs() as loggs_local:
                         estimator = CorrectedYield(cyield)
                         spectrum = estimator.transform(data, loggs_local)
@@ -85,10 +102,10 @@ class YieldExtractioinUncertanityOptions(object):
             "pol2",
             "pol1"
         ]
-        self.signals = [
-            "CrystalBall",
-            "Gaus"
-        ]
+        self.signals = {
+            "CrystalBall": "config/data/cball.json",
+            "Gaus": "config/data/gaus.json",
+        }
         self.nsigmas = [
             2,
             3
