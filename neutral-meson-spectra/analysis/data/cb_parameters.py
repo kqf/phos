417fc12d6914c25ca40c7d4336763fe9bf5f4ba6
Sasha <q.dev.null@gmail.com>
2019-11-12 07:40:44 +0100

    Add cb analysis to thesis plots

--- neutral-meson-spectra/analysis/data/cb_parameters.py
+++ neutral-meson-spectra/analysis/data/cb_parameters.py
@@ -1,13 +1,14 @@
 import pytest
 
 import ROOT
+import spectrum.sutils as su
 from spectrum.analysis import Analysis
-from spectrum.comparator import Comparator
 from spectrum.options import Options
 from spectrum.output import open_loggs
 from spectrum.pipeline import Pipeline
 from spectrum.pipeline import TransformerBase
 from vault.datavault import DataVault
+from spectrum.plotter import plot
 
 
 @pytest.fixture
@@ -18,7 +19,7 @@ def data():
 class CbFitOptions(object):
     def __init__(self, particle):
         super(CbFitOptions, self).__init__()
-        self.analysis = Options(particle)
+        self.analysis = Options(particle, pt="config/pt-same.json")
         self.analysis.invmass.signal.relaxed = True
         self.analysis.invmass.background.relaxed = True
 
@@ -27,23 +28,27 @@ class CbFitOptions(object):
             ROOT.TF1("alpha", "pol0", 0, 20),
             ROOT.TF1("n", "pol0", 0, 20),
         ]
+        self.limits = (0., 4.), (0., 6.)
 
 
 class SelectAndFitHistograms(TransformerBase):
 
     def __init__(self, options=None):
-        self.options = options
+        self.opt = options
 
     def transform(self, data, loggs):
-        output = [data._asdict()[n] for n in self.options.names]
-        for hist, func in zip(output, self.options.functions):
+        output = [data._asdict()[n] for n in self.opt.names]
+        args = zip(output, self.opt.functions, self.opt.limits)
+        for hist, func, limits in args:
             self._fit_histogram(hist, func)
+            hist.ylimits = limits
         return output
 
     def _fit_histogram(self, histogram, func):
         histogram.Fit(func, 'q')
-        histogram.SetLineColor(ROOT.kBlue + 1)
-        func.SetLineColor(ROOT.kRed + 1)
+        histogram.SetLineColor(ROOT.kRed + 1)
+        func.SetLineColor(ROOT.kBlue + 1)
+        func.SetLineWidth(2)
         val, err = func.GetParameter(0), func.GetParError(0)
         print("The scale is", histogram.GetTitle(), val, err)
 
@@ -57,18 +62,32 @@ class CballParametersEstimator(TransformerBase):
         ])
 
 
-@pytest.mark.interactive
+@pytest.mark.thesis
 @pytest.mark.onlylocal
+@pytest.mark.interactive
 @pytest.mark.parametrize("particle", [
     "#pi^{0}",
-    "#eta"
+    # "#eta"
 ])
 def test_cball_parameters(particle, data):
     estimator = CballParametersEstimator(
         CbFitOptions(particle=particle), plot=False)
 
     with open_loggs("test cball parameters {}".format(particle)) as loggs:
-        hists = estimator.transform(data, loggs=loggs)
+        outputs = estimator.transform(data, loggs=loggs)
 
-    for h in hists:
-        Comparator().compare(h)
+    for hist in outputs:
+        hist.SetTitle("Data")
+        func = hist.GetListOfFunctions()[0]
+        func.SetTitle("Constant fit")
+        func.SetLineStyle(9)
+        func.SetLineColor(ROOT.kBlack)
+        oname = "results/analysis/data/{}_{}.pdf"
+        plot(
+            [hist, func],
+            logy=False,
+            ylimits=hist.ylimits,
+            legend_pos=(0.58, 0.7, 0.68, 0.85),
+            ltitle="{} #rightarrow #gamma#gamma".format(particle),
+            oname=oname.format(func.GetName(), su.spell(particle)),
+        )
