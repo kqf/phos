613accb6bcee0b601fb90b75ada69986abda1e76
Sasha <q.dev.null@gmail.com>
2019-11-15 15:00:45 +0100

    Clean the plots for TOF sutdy

--- neutral-meson-spectra/analysis/data/tof.py
+++ neutral-meson-spectra/analysis/data/tof.py
@@ -7,16 +7,8 @@ from spectrum.options import ProbeTofOptions
 from vault.datavault import DataVault
 from spectrum.output import open_loggs
 from spectrum.tools.probe import TagAndProbe
-from spectrum.tools.validate import validate # noqa
+from spectrum.tools.validate import validate  # noqa
 from spectrum.plotter import plot
-
-"""
-- [x] Fix titles
-- [x] Exclude LHC18
-- [ ] Try different binning
-- [ ] Try different parameters
-
-"""
 
 
 @pytest.fixture
@@ -44,34 +36,20 @@ def data_old():
 @pytest.fixture
 def fitfunc():
     tof_eff = ROOT.TF1(
-        "tof_eff",
-        # "[2] * (1.+[0]*TMath::Exp(-TMath::Power(x/[1], 2.) / 2.)) "
+        "tof",
         "1 - [2] / (1 + TMath::Exp(x * [0] + [1]))"
         " - [3] * TMath::Exp(x * [4])", 0, 20
     )
+    tof_eff.SetTitle("Fit")
+    tof_eff.SetLineColor(ROOT.kBlack)
+    tof_eff.SetLineStyle(9)
+    tof_eff.SetParNames(*["A", "B", "C", "D"])
 
-    tof_eff.SetParNames("A", "#sigma", "Eff_{scale}")
-    # tof_eff.FixParameter(0, -1.30534e+00)
-    # tof_eff.FixParameter(1, 1.02604e+01)
-    # tof_eff.FixParameter(2, 5.70061e-01)
-    # tof_eff.FixParameter(3, 1.06068e+00)
-    # tof_eff.FixParameter(4, -1.62810e+00)
-
-    # Previous
     tof_eff.SetParameter(0, -1.02802e+00)
     tof_eff.SetParameter(1, 8.51857e+00)
     tof_eff.SetParameter(2, 6.19420e-01)
     tof_eff.SetParameter(3, 1.30498e+00)
     tof_eff.SetParameter(4, -1.78716e+00)
-
-    # tof_eff.FixParameter(0, -1.02802e+00)
-    # tof_eff.FixParameter(1, 8.51857e+00)
-    # tof_eff.FixParameter(2, 6.19420e-01)
-    # tof_eff.FixParameter(3, 2.15384e+00)
-    # tof_eff.FixParameter(4, -2.90812e+00)
-    tof_eff.SetTitle("Fit")
-    tof_eff.SetLineColor(ROOT.kBlack)
-    tof_eff.SetLineStyle(9)
     return tof_eff
 
 
@@ -83,16 +61,25 @@ def efficiency(data):
     return efficiency
 
 
-@pytest.mark.interactive
+@pytest.fixture
+def oname():
+    return "results/analysis/data/tof.pdf"
+
+
+@pytest.mark.thesis
 @pytest.mark.onlylocal
-def test_estimate_tof_efficiency(efficiency, fitfunc):
+@pytest.mark.interactive
+def test_estimate_tof_efficiency(efficiency, oname, fitfunc):
     bins = br.edges(efficiency)
     fitfunc.SetRange(min(bins), max(bins))
     efficiency.Fit(fitfunc, "RWW")
+    br.report(fitfunc)
     plot(
         [efficiency, fitfunc],
         logy=False,
         csize=(126, 126),
-        legend_pos=(0.25, 0.7, 0.38, 0.85),
+        legend_pos=(0.20, 0.7, 0.35, 0.85),
+        ltitle="#pi^{0} #rightarrow #gamma #gamma",
+        oname=oname
     )
     # validate(br.hist2dict(efficiency), "efficiency_tag")
