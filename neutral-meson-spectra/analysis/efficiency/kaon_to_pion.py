272cfd1c99ed3dada2b08e529c53f13997efd912
Sasha <q.dev.null@gmail.com>
2019-11-20 07:54:36 +0100

    Move the definition of the a K/pi nalysis to a separate function

--- neutral-meson-spectra/analysis/efficiency/kaon_to_pion.py
+++ neutral-meson-spectra/analysis/efficiency/kaon_to_pion.py
@@ -1,26 +1,23 @@
 import pytest
 
 import ROOT
-
-from spectrum.tools.kaon2pion import KaonToPionRatioMC
+import spectrum.broot as br
+from spectrum.pipeline import RebinTransformer
 from spectrum.tools.kaon2pion import KaonToPionDoubleRatio
 from spectrum.tools.kaon2pion import DoubleK2POptions, K2POptions
 from spectrum.output import open_loggs
 from vault.datavault import DataVault
-from spectrum.comparator import Comparator
+from spectrum.plotter import plot
 
 
 @pytest.fixture
-def real_data():
-    return (
+def data():
+    real_data = (
         DataVault().input("kaon2pion"),
         DataVault().input("kaon2pion"),
     )
 
-
-@pytest.fixture
-def mc_data():
-    return (
+    mc_data = (
         (
             DataVault().input("pythia8", "kaon2pion"),
             DataVault().input("pythia8", "kaon2pion"),
@@ -30,35 +27,60 @@ def mc_data():
             DataVault().input("pythia8", "kaon2pion"),
         ),
     )
+    return real_data, mc_data
 
 
-@pytest.fixture
-def data(real_data, mc_data):
-    return (real_data, mc_data)
-
-
-@pytest.mark.skip("")
-def test_draw_ratio_mc(mc_data):
-    options = K2POptions(
-        kaons=["hPt_K^{+}_", "hPt_K^{-}_"],
-        pions=["hPt_#pi^{+}_", "hPt_#pi^{-}_"]
-    )
-    return KaonToPionRatioMC(options).transform(mc_data, {})
-
-
-@pytest.mark.thesis
-def test_ratio(data):
+def reduce_func(inputs, loggs):
     fitfunc = ROOT.TF1(
         "feeddown_ratio",
         "[3] * x *(([4] + [5]) * x - [5]) + "
-        "[0] * (1 + [1] * TMath::Exp(-x * x/ [2]))", 0, 20)
-    fitfunc.SetParameter(0, 1.53561e-02)
-    fitfunc.SetParameter(1, -4.69350e-01)
-    fitfunc.SetParameter(2, 2.38042e-01)
-    fitfunc.SetParameter(3, -8.01155e-04)
-    fitfunc.SetParameter(4, 6.30860e-01)
-    fitfunc.SetParameter(5, -7.21683e-01)
+        "[0] * (1 + [1] * TMath::Exp(-x * x/ [2]))", 0.5, 20)
+    fitfunc.SetTitle("Fit")
+    fitfunc.SetLineColor(ROOT.kBlack)
+    fitfunc.SetLineStyle(9)
+    fitfunc.SetParameter(0, 3.109659395803652)
+    fitfunc.SetParameter(1, -0.39094679433665736)
+    fitfunc.SetParameter(2, 20.553216100613582)
+    fitfunc.SetParameter(3, -0.02262708740433334)
+    fitfunc.SetParameter(4, 16.09234627672746)
+    fitfunc.SetParameter(5, -17.17670171607285)
+    data, mc_ = inputs
+    data.SetTitle(
+        "Data; p_{T} (GeV/#it{c});"
+        "(#frac{K^{+} + K^{-}}{#pi^{+} + #pi^{-}})^{data}"
+    )
+    mc_.SetTitle(
+        "MC; p_{T} (GeV/#it{c});"
+        "(#frac{K^{+} + K^{-}}{#pi^{+} + #pi^{-}})^{mc}"
+    )
+    mc = RebinTransformer(True, br.edges(data)).transform(mc_, loggs)
+    # plot(
+    #     # [data, mc],
+    #     inputs,
+    #     xlimits=(0.3, 20),
+    #     ytitle="#frac{K^{+} + K^{-}}{#pi^{+} + #pi^{-}}",
+    #     logy=False,
+    #     logx=False,
+    #     yoffset=1.6,
+    # )
 
+    double_ratio = br.ratio(data, mc)
+    double_ratio.Fit(fitfunc, "RQWW")
+    double_ratio.SetTitle("Double ratio")
+    plot(
+        [double_ratio, fitfunc],
+        xlimits=(0.3, 20),
+        logy=False,
+        logx=True,
+    )
+    for i, (p, e) in enumerate(zip(*br.pars(fitfunc))):
+        print("fitfunc.SetParameter({}, {})".format(i, p))
+    return double_ratio
+
+
+@pytest.mark.thesis
+@pytest.mark.onlylocal
+def test_ratio(data):
     options = DoubleK2POptions(
         data=K2POptions(
             kaons="hstat_kaon_pp13_sum",
@@ -68,10 +90,9 @@ def test_ratio(data):
             kaons=["hPt_K^{+}_", "hPt_K^{-}_"],
             pions=["hPt_#pi^{+}_", "hPt_#pi^{-}_"]
         ),
-        fitfunc=fitfunc
+        reduce_func=reduce_func
     )
 
     estimator = KaonToPionDoubleRatio(options, plot=True)
-    with open_loggs("pion to kaon") as loggs:
-        double_ratio = estimator.transform(data, loggs)
-        Comparator().compare(double_ratio)
+    with open_loggs() as loggs:
+        estimator.transform(data, loggs)
--- neutral-meson-spectra/spectrum/tools/kaon2pion.py
+++ neutral-meson-spectra/spectrum/tools/kaon2pion.py
@@ -4,8 +4,6 @@ from spectrum.input import SingleHistInput
 from spectrum.pipeline import TransformerBase
 from spectrum.pipeline import ReducePipeline, ParallelPipeline
 from spectrum.pipeline import ComparePipeline
-from spectrum.pipeline import Pipeline
-from spectrum.comparator import Comparator
 
 
 class HistSum(TransformerBase):
@@ -20,7 +18,8 @@ class HistSum(TransformerBase):
 
 
 K2POptions = namedtuple("K2POptions", ["pions", "kaons"])
-DoubleK2POptions = namedtuple("DoubleK2POptions", ["data", "mc", "fitfunc"])
+DoubleK2POptions = namedtuple("DoubleK2POptions", ["data", "mc",
+                                                   "reduce_func"])
 
 
 class KaonToPionRatioMC(TransformerBase):
@@ -43,52 +42,13 @@ class KaonToPionRatioData(TransformerBase):
         ], plot)
 
 
-def rebin_compare(inputs, loggs):
-    data, mc = inputs
-    title = """
-    Charged kaon to charged pion yields ratio; p_{T} (GeV/#it{c})
-    ; #frac{K^{+} + K^{-}}{#pi^{+} + #pi^{-}}"
-    """
-    mc.SetTitle(title)
-    mc.label = "PYTHIA8, #sqrt{s} = 13 TeV"
-    data.SetTitle(title)
-    data.label = "ALCIE, #sqrt{s} = 13 TeV, preliminary"
-    mc.SetAxisRange(0.2, 20, "X")
-    mc, data = br.rebin_as(mc, data)
-    data.SetAxisRange(0.2, 20, "X")
-    return Comparator().compare(data, mc, loggs=loggs)
-
-
-class DoubleRatioFitter(TransformerBase):
-    def __init__(self, fitfunc, plot=False):
-        super(DoubleRatioFitter, self).__init__(plot)
-        self.fitfunc = fitfunc
-
-    def transform(self, double_ratio, loggs):
-        double_ratio.SetMarkerSize(0)
-        double_ratio.Fit(self.fitfunc, "RQ")
-        self.fitfunc.SetRange(0, 20)
-        title = "Chi2/ndf = " + \
-            str(self.fitfunc.GetChisquare() / self.fitfunc.GetNDF())
-        double_ratio.SetTitle(title)
-        double_ratio.label = "pp at #sqrt{s} = 13 TeV"
-        double_ratio.SetName("kaon2pion")
-        print(br.pars(self.fitfunc))
-        return double_ratio
-
-
 class KaonToPionDoubleRatio(TransformerBase):
     def __init__(self, options, plot=False):
         super(KaonToPionDoubleRatio, self).__init__()
-        double_ratio = ReducePipeline(
+        self.pipeline = ReducePipeline(
             ParallelPipeline([
                 ("data", KaonToPionRatioData(options.data)),
                 ("mc", KaonToPionRatioMC(options.mc)),
             ]),
-            rebin_compare
+            options.reduce_func
         )
-
-        self.pipeline = Pipeline([
-            ("data_mc_ratio", double_ratio),
-            ("fit", DoubleRatioFitter(options.fitfunc, plot)),
-        ])
