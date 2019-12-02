4dabfea52a29fba67d229e6cffd4f99c7859dc24
Sasha <q.dev.null@gmail.com>
2019-12-02 09:22:28 +0100

    Add levels and fix the aspect ration of eta/pion energy dependence

--- neutral-meson-spectra/analysis/final/eta_pion_energies.py
+++ neutral-meson-spectra/analysis/final/eta_pion_energies.py
@@ -1,3 +1,4 @@
+import ROOT
 import pytest
 import pandas as pd
 import spectrum.broot as br
@@ -8,17 +9,32 @@ from spectrum.plotter import plot
 def data():
     raw = pd.read_json("config/physics/eta_pion_energies.json")
     return br.graph(
-        "; p_{T} (GeV/#it{c}); #eta/#pi^{0}",
+        "; #sqrt{s} (GeV); #eta/#pi^{0}",
         raw["energy"],
         raw["eta_pion"],
         dy=raw["eta_pion_err"])
 
 
-def test_reports_energies(data):
+@pytest.fixture
+def delimeter(level=0.5):
+    func = ROOT.TF1("const", "[0]", 13.8, 13000)
+    func.SetParameter(0, level)
+    func.SetLineColor(ROOT.kBlack)
+    func.SetLineStyle(9)
+    return func
+
+
+@pytest.fixture
+def oname():
+    return "results/eta_pion_energies.pdf"
+
+
+def test_reports_energies(data, delimeter, oname):
     plot(
-        [data],
+        [data, delimeter],
         logy=False,
         legend_pos=None,
         ylimits=(0, 1),
-        csize=(126, 126),
+        csize=(126, 96),
+        oname=oname,
     )
