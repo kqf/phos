c91d4251d8d9484ba755dcd8de0c95ed338aa708
Sasha <q.dev.null@gmail.com>
2019-12-05 11:05:13 +0100

    Use separate fixtures for functinons in plotter tests

--- neutral-meson-spectra/tests/software/compare/test_plotter.py
+++ neutral-meson-spectra/tests/software/compare/test_plotter.py
@@ -1,17 +1,26 @@
+import pytest
+
 from spectrum.plotter import plot, hplot
 from vault.formulas import FVault
+
+
+@pytest.fixture
+def functions(data):
+    funcs = []
+    for d in data:
+        func = FVault().tf1("tsallis")
+        d.Fit(func)
+        funcs.append(func)
+    return funcs
 
 
 def test_plots_histograms(data, stop):
     plot(data, "p_{T}", "y", logy=True, logx=True, stop=stop)
 
 
-def test_plots_functions(data, stop):
-    fitf = FVault().tf1("tsallis")
-    fitf.SetTitle("approximation")
-    data.append(fitf)
-    plot(data, "p_{T}", "y", logy=True, logx=True, stop=stop)
+def test_plots_functions(data, functions, stop):
+    plot(data + functions, "p_{T}", "y", logy=True, logx=True, stop=stop)
 
 
-def test_hplots_histograms(data, stop=True):
+def test_hplots_histograms(data, stop):
     hplot(data, "p_{T}", "y", logy=True, logx=True, stop=stop)
