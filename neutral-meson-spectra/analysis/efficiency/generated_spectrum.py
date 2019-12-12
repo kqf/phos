885c55ff434f7353d92476dde3a86e840c62a3d2
Sasha <q.dev.null@gmail.com>
2019-12-12 08:04:35 +0100

    Switch to fixtures in spmc generated spectrum tests

--- neutral-meson-spectra/analysis/efficiency/generated_spectrum.py
+++ neutral-meson-spectra/analysis/efficiency/generated_spectrum.py
@@ -4,10 +4,10 @@ import ROOT
 
 from spectrum.comparator import Comparator
 from spectrum.input import SingleHistInput
-from vault.datavault import DataVault
 
 
-def fit_function(particle):
+@pytest.fixture
+def fitf(particle):
     tsallis = ROOT.TF1(
         "f", "x[0] * (x[0])*[0]/2./3.1415*([2]-1.)*([2]-2.)/([2]*[1]*([2]*[1]+[4]*([2]-2.))) * (1.+(sqrt((x[0])*(x[0])+[3]*[3])-[4])/([2]*[1])) ** (-[2])", 0, 20)  # noqa
     tsallis.SetParameters(21.339890553914014,
@@ -19,21 +19,15 @@ def fit_function(particle):
 
 
 @pytest.fixture
-def data():
-    return (
-        DataVault().input("single #pi^{0}", "low", "PhysEff"),
-        DataVault().input("single #pi^{0}", "high", "PhysEff")
-    )
-
-
-@pytest.fixture
 def target_hist():
     return "hPt_#pi^{0}_primary_standard"
 
 
-def test_generated_distribution(target_hist, data):
-    for prod in data:
+@pytest.mark.parametrize("particle", [
+    "#pi^{0}",
+])
+def test_generated_distribution(particle, target_hist, fitf, spmc):
+    for prod in spmc:
         generated = SingleHistInput(target_hist).transform(prod)
         generated.Scale(1. / generated.Integral())
-        function = fit_function("#pi^{0}")
-        Comparator().compare(generated, function.GetHistogram())
+        Comparator().compare(generated, fitf.GetHistogram())
