587bee70bc96a262328552cabc3f1f4302595613
Sasha <q.dev.null@gmail.com>
2019-12-14 20:08:15 +0100

    Use kwargs to pass extra paramters in DataVault

--- neutral-meson-spectra/analysis/data/conftest.py
+++ neutral-meson-spectra/analysis/data/conftest.py
@@ -15,20 +15,33 @@ def data():
 @pytest.fixture
 def tof_data():
     return (
-        DataVault().input("data", "staging tof", listname="TagAndProbleTOF",
-                          histname="MassEnergyTOF_SM0"),
-        DataVault().input("data", "staging tof", listname="TagAndProbleTOF",
-                          histname="MassEnergyAll_SM0"),
+        DataVault().input(
+            "data",
+            "staging tof",
+            listname="TagAndProbleTOF",
+            histname="MassEnergyTOF_SM0"
+        ),
+        DataVault().input(
+            "data",
+            "staging tof",
+            listname="TagAndProbleTOF",
+            histname="MassEnergyAll_SM0"
+        ),
     )
 
 
 @pytest.fixture
 def tof_data_old():
     return (
-        DataVault().input("data", "uncorrected",
-                          "TagAndProbleTOFOnlyTender",
-                          histname="MassEnergyTOF_SM0"),
-        DataVault().input("data", "uncorrected",
-                          "TagAndProbleTOFOnlyTender",
-                          histname="MassEnergyAll_SM0"),
+        DataVault().input(
+            "data",
+            "uncorrected",
+            listname="TagAndProbleTOFOnlyTender",
+            histname="MassEnergyTOF_SM0"),
+        DataVault().input(
+            "data",
+            "uncorrected",
+            listname="TagAndProbleTOFOnlyTender",
+            histname="MassEnergyAll_SM0"
+        ),
     )
