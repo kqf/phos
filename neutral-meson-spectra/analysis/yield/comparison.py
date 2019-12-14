51bcfd87e3918c6ba56c28b8f7a60a35ae607a1a
Sasha <q.dev.null@gmail.com>
2019-12-14 07:16:59 +0100

    Add conftest to the yield scripts

--- neutral-meson-spectra/analysis/yield/comparison.py
+++ neutral-meson-spectra/analysis/yield/comparison.py
@@ -2,21 +2,13 @@ import pytest
 
 from spectrum.output import open_loggs
 from spectrum.analysis import Analysis
-from vault.datavault import DataVault
 from spectrum.tools.feeddown import data_feeddown
 from spectrum.options import Options
 from spectrum.corrected_yield import CorrectedYield
 from spectrum.options import CompositeCorrectedYieldOptions
 from spectrum.efficiency import Efficiency
 from spectrum.options import CompositeEfficiencyOptions
-
-
 from spectrum.comparator import Comparator
-
-
-@pytest.fixture
-def data():
-    return DataVault().input("data", histname="MassPtSM0")
 
 
 @pytest.fixture
@@ -25,45 +17,23 @@ def yield_data(data, particle):
     return data, data_feeddown(dummy=use_dummy)
 
 
-def pion_data():
-    return (
-        DataVault().input("single #pi^{0}", "low", "PhysEff"),
-        DataVault().input("single #pi^{0}", "high", "PhysEff"),
-    )
-
-
-def eta_data():
-    return (
-        DataVault().input("single #eta", "low", "PhysEff"),
-        DataVault().input("single #eta", "high", "PhysEff"),
-    )
-
-
-@pytest.fixture
-def mc(particle):
-    return {
-        "#pi^{0}": pion_data(),
-        "#eta": eta_data(),
-    }.get(particle)
-
-
 @pytest.mark.onlylocal
 @pytest.mark.interactive
 @pytest.mark.parametrize("particle", [
     "#pi^{0}",
     "#eta",
 ])
-def test_simple(particle, mc, data, yield_data):
+def test_simple(particle, spmc, data, yield_data):
     with open_loggs() as loggs:
         estimator = Analysis(Options(particle=particle))
         raw_yield = estimator.transform(data, loggs).spectrum
 
         estimator = Efficiency(CompositeEfficiencyOptions(particle=particle))
-        efficiency = estimator.transform(mc, loggs)
+        efficiency = estimator.transform(spmc, loggs)
 
         estimator = CorrectedYield(
             CompositeCorrectedYieldOptions(particle=particle)
         )
-        corr_yield = estimator.transform((yield_data, mc), loggs)
+        corr_yield = estimator.transform((yield_data, spmc), loggs)
         print([raw_yield, efficiency, corr_yield])
         Comparator().compare([raw_yield, efficiency, corr_yield])
---
+++ neutral-meson-spectra/analysis/yield/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from vault.datavault import DataVault
+
+
+@pytest.fixture
+def ltitle(particle):
+    return "{} #rightarrow #gamma #gamma".format(particle)
+
+
+@pytest.fixture
+def data():
+    return DataVault().input("data", histname="MassPtSM0")
+
+
+@pytest.fixture
+def spmc(particle):
+    production = "single {}".format(particle)
+    return (
+        DataVault().input(production, "low", "PhysEff"),
+        DataVault().input(production, "high", "PhysEff"),
+    )
