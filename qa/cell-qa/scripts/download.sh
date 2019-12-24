64ddee3ca9eba4a9bd8d6494698995643b949a19
Sasha <q.dev.null@gmail.com>
2019-12-24 09:25:24 +0100

    Fix the hardcoded paths to the datasets

--- qa/cell-qa/scripts/download.sh
+++ qa/cell-qa/scripts/download.sh
@@ -1,14 +1,14 @@
 function download_from_grid()
 {
     counter=1
-    MHOME=/alice/cern.ch/user/o/okovalen
+    MHOME=/alice/cern.ch/user/o/${USER}
 
     # for run in $$(alien_ls /$(MPATH) | grep 000);do alien_cp alien://$(MPATH)/$$run/AnalysisResults.root $(PERIOD)/$$run.root; done
     for run in $(alien_ls $MHOME/$1/output | grep -o "^[0-9]*") 
     do 
-        echo $run
+        echo ${run}
         # until alien_cp alien://$MHOME/$1/output/$run/$2 $3/$((counter++)).$run.root; do echo 'Trying again in 10 sec.'; sleep 10; done
-        alien_cp alien://$MHOME/$1/output/$run/$2 $3/$((counter++)).$run.root
+        alien_cp alien://$MHOME/$1/output/$run/$2 $3/$((counter++)).${run}.root
     done
 }
 
