9f0516c1905a70941ac71848cab9a9a23ea0d872
Sasha <q.dev.null@gmail.com>
2019-12-30 13:50:28 +0100

    Update the spmc qa plots for the custom inputs

--- neutral-meson-spectra/scripts/efficiency/qa.py
+++ neutral-meson-spectra/scripts/efficiency/qa.py
@@ -1,13 +1,24 @@
 import pytest
 
 import ROOT
-from spectrum.input import SingleHistInput
+from spectrum.pipeline import SingleHistReader
 from spectrum.output import open_loggs
 import spectrum.broot as br  # noqa
 from spectrum.comparator import Comparator  # noqa
 
 import spectrum.plotter as plt
 from spectrum.tools.validate import validate  # noqa
+from vault.datavault import DataVault
+
+
+@pytest.fixture
+def spmc(particle, selection, histname):
+    production = "single {}".format(particle)
+    histname = histname.format(particle)
+    return (
+        DataVault().input(production, "low", selection, histname=histname),
+        DataVault().input(production, "high", selection, histname=histname),
+    )
 
 
 @pytest.fixture
@@ -21,13 +32,15 @@ def oname(particle):
     "#pi^{0}",
     "#eta",
 ])
+@pytest.mark.parametrize("selection", ["PhysEff"])
+@pytest.mark.parametrize("histname", ["hEtaPhi_#gamma"])
 def test_eta_phi(particle, spmc, oname):
     with plt.style(), plt.pcanvas(oname=oname), open_loggs() as loggs:
         ROOT.gStyle.SetPalette(ROOT.kLightTemperature)
         ROOT.gPad.SetLogz(True)
         ROOT.gPad.SetRightMargin(0.12)
         summed = br.hsum([
-            SingleHistInput("hEtaPhi_#gamma").transform(d, loggs)
+            SingleHistReader().transform(d, loggs)
             for d in spmc
         ])
         summed.GetYaxis().SetTitleOffset(1.0)
@@ -52,12 +65,12 @@ def wname(particle, selection):
     "NoWeights",
     "PhysEff",
 ])
+@pytest.mark.parametrize("histname", ["hPtLong_{}"])
 def test_spectral_shape(particle, spmc, ltitle, selection, wname):
     with open_loggs() as loggs:
         hists = []
         for name, d in zip(["low p_{T}", "high p_{T}"], spmc):
-            hist = SingleHistInput(
-                "hPtLong_{}".format(particle), selection).transform(d, loggs)
+            hist = SingleHistReader().transform(d, loggs)
             hist.SetTitle(name)
             hists.append(br.hist2graph(hist, "positive"))
 
