95e4fcd43f1c51cec3ee69ced9c0b3e445b43607
Sasha <q.dev.null@gmail.com>
2020-01-01 10:15:28 +0100

    Simplify the mc eta/pion ratio

--- neutral-meson-spectra/tests/mc/test_eta_pion_ratio.py
+++ neutral-meson-spectra/tests/mc/test_eta_pion_ratio.py
@@ -1,8 +1,7 @@
 import pytest
 from spectrum.comparator import Comparator
-from spectrum.input import SingleHistInput
+from spectrum.pipeline import SingleHistReader
 from spectrum.pipeline import ComparePipeline
-from spectrum.pipeline import TransformerBase
 from spectrum.output import open_loggs
 from vault.datavault import DataVault
 
@@ -10,23 +9,18 @@ from vault.datavault import DataVault
 @pytest.fixture
 def data():
     return (
-        DataVault().input("pythia8"),
-        DataVault().input("pythia8"),
+        DataVault().input("pythia8", histname="hPtLong_#eta"),
+        DataVault().input("pythia8", histname="hPtLong_#pi^{0}"),
     )
-
-
-class EtaPionRatio(TransformerBase):
-    def __init__(self, options, plot=False):
-        super(EtaPionRatio, self).__init__()
-        self.pipeline = ComparePipeline([
-            ("#eta", SingleHistInput("hPtLong_#eta")),
-            ("#pi^{0}", SingleHistInput("hPtLong_#pi^{0}")),
-        ], plot)
 
 
 @pytest.mark.onlylocal
 @pytest.mark.interactive
 def test_calculate_eta_pion_ratio(data):
+    estimator = ComparePipeline([
+        ("#eta", SingleHistReader()),
+        ("#pi^{0}", SingleHistReader()),
+    ], plot=False)
     with open_loggs("eta pion ratio generated") as loggs:
-        ratio = EtaPionRatio(None).transform(data, loggs)
+        ratio = estimator.transform(data, loggs)
         Comparator().compare(ratio)
