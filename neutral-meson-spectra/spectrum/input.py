94a4c195c6c3c888125ec240a64cf405135c074f
Sasha <q.dev.null@gmail.com>
2020-01-06 05:30:15 +0100

    Remove the outdated input module

--- neutral-meson-spectra/spectrum/input.py
+++
@@ -1,9 +0,0 @@
-import spectrum.broot as br
-
-
-class AnalysisReaderBase(object):
-    def _events(self, filename, listname):
-        return br.io.read(
-            filename,
-            listname,
-            "EventCounter").GetBinContent(2)
