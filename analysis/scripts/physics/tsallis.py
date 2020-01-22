1c5f5f45fcd129509f3ac1e5f6152642486c2de2
Sasha <q.dev.null@gmail.com>
2020-01-22 16:11:16 +0100

    Add the tsallis parameters energy dependence

--- analysis/scripts/physics/tsallis.py
+++ analysis/scripts/physics/tsallis.py
@@ -1,4 +1,7 @@
+import json
 import pytest  # noqa
+import numpy as np
+import pandas as pd
 
 import spectrum.broot as br
 from spectrum.spectra import spectrum
@@ -44,5 +47,51 @@ def test_tsallis_tcm_fit(particle, ranges, tsallis, ltitle, stop, oname):
         legend_pos=(0.65, 0.7, 0.8, 0.88),
         yoffset=1.4,
         more_logs=False,
-        oname=oname.format("phenomenology/fits_"),
+        oname=oname.format("phenomenology/tsallis_"),
+    )
+
+
+@pytest.fixture
+def pars():
+    with open("config/predictions/tsallis-pion.json") as f:
+        data = json.load(f)
+    return pd.DataFrame(data.values())
+
+
+@pytest.mark.skip
+@pytest.mark.thesis
+@pytest.mark.onlylocal
+@pytest.mark.interactive
+@pytest.mark.parametrize("particle", [
+    "#pi^{0}",
+    # "#eta",
+])
+@pytest.mark.parametrize("parameter", [
+    "n",
+    "C"
+])
+def test_tsallis_parameter_dependence(pars, parameter, stop, ltitle, oname):
+    print(pars)
+    graph = br.graph(
+        "test",
+        pars["energy"],
+        pars[parameter],
+        np.zeros_like(pars["energy"]),
+        pars["d{}".format(parameter)])
+    graph.SetMarkerColor(4)
+    graph.SetMarkerStyle(21)
+
+    plot(
+        [graph],
+        stop=stop,
+        logy=False,
+        ytitle=invariant_cross_section_code(),
+        xtitle="p_{T} (GeV/#it{c})",
+        xlimits=(0.8, 14),
+        csize=(96, 128),
+        ltitle=ltitle,
+        legend_pos=(0.65, 0.7, 0.8, 0.88),
+        yoffset=1.4,
+        more_logs=False,
+        oname=oname.format("phenomenology/tsallis_"),
     )
