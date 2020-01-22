4c7c654af9201d5dfd83046b03740959379f07d7
Sasha <q.dev.null@gmail.com>
2020-01-22 10:15:45 +0100

    Add the data / fit ratio for tsallis analysis

--- analysis/scripts/physics/tsallis.py
+++ analysis/scripts/physics/tsallis.py
@@ -13,19 +13,34 @@ from spectrum.constants import invariant_cross_section_code
     "#pi^{0}",
     "#eta",
 ])
-def test_tsallis_tcm_fit(particle, tcm, tsallis, oname):
+def test_tsallis_tcm_fit(particle, tsallis, ltitle, stop, oname):
     cs = spectrum(particle)
     tsallis.SetRange(0.8, 20)
     cs.Fit(tsallis, "Q")
     br.report(tsallis, particle)
-    cs.Divide(tsallis)
     plot(
         [cs],
         ytitle=invariant_cross_section_code(),
         xtitle="p_{T} (GeV/#it{c})",
         # xlimits=(0.7, 22),
         csize=(96, 128),
-        ltitle="{} #rightarrow #gamma#gamma".format(particle),
+        ltitle=ltitle,
+        legend_pos=(0.65, 0.7, 0.8, 0.88),
+        yoffset=1.4,
+        more_logs=False,
+        oname=oname.format("phenomenology/fits_"),
+    )
+    ratio = cs.Clone()
+    ratio.Divide(tsallis)
+    plot(
+        [ratio],
+        stop=stop,
+        logy=False,
+        ytitle=invariant_cross_section_code(),
+        xtitle="p_{T} (GeV/#it{c})",
+        # xlimits=(0.7, 22),
+        csize=(96, 128),
+        ltitle=ltitle,
         legend_pos=(0.65, 0.7, 0.8, 0.88),
         yoffset=1.4,
         more_logs=False,
