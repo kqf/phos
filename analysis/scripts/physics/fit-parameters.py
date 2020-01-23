77e6a5dec5660d4fd175fd01b2f7bf23eff45e5d
Sasha <q.dev.null@gmail.com>
2020-01-23 22:17:45 +0100

    Add a separate script to study the tsallis/tcm parameters

---
+++ analysis/scripts/physics/fit-parameters.py
@@ -0,0 +1,71 @@
+import pytest
+import json
+import six
+
+import spectrum.broot as br
+from spectrum.pipeline import TransformerBase
+from spectrum.pipeline import ParallelPipeline
+from spectrum.output import open_loggs
+from spectrum.spectra import spectrum
+from spectrum.plotter import plot
+from spectrum.constants import invariant_cross_section_code
+
+
+class HepdataInput(TransformerBase):
+    def __init__(self, table_name="Table 1",
+                 histname="Graph1D_y1", plot=False):
+        super(HepdataInput, self).__init__(plot)
+        self.histname = histname
+
+    def transform(self, item, loggs):
+        filename = ".hepdata-cachedir/{}".format(item["file"])
+        br.io.hepdata(item["hepdata"], filename, item["table"])
+        graph = br.io.read(filename, item["table"], self.histname)
+        hist = br.graph2hist(graph)
+        hist.GetXaxis().SetTitle("p_{T} (GeV/#it{c})")
+        hist.SetTitle(item["title"])
+        hist.Scale(item["scale"])
+        hist.energy = item["energy"]
+        return hist
+
+
+@pytest.fixture
+def data(particle, tsallis):
+    with open("config/predictions/hepdata.json") as f:
+        data = json.load(f)[particle]
+    labels, links = zip(*six.iteritems(data))
+    steps = [(l, HepdataInput()) for l in labels]
+    with open_loggs() as loggs:
+        histograms = ParallelPipeline(steps).transform(links, loggs)
+    spectra = sorted(histograms, key=lambda x: x.energy)
+    measured = spectrum(particle)
+    measured.SetTitle("pp, #sqrt{s} = 13 TeV")
+    spectra.append(measured)
+    for data in spectra:
+        tsallis.SetRange(0, 40)
+        data.Fit(tsallis, "QR", "", 0, 40)
+        br.report(tsallis)
+        plot([data, tsallis])
+    return spectra
+
+
+@pytest.mark.onlylocal
+@pytest.mark.interactive
+@pytest.mark.parametrize("particle", [
+    "#pi^{0}",
+    "#eta",
+])
+def test_downloads_from_hepdata(particle, data):
+    plot(
+        data,
+        ytitle=invariant_cross_section_code(),
+        xtitle="p_{T} (GeV/#it{c})",
+        # xlimits=(0.7, 22),
+        csize=(96, 128),
+        ltitle="{} #rightarrow #gamma#gamma".format(particle),
+        # legend_pos=(0.65, 0.7, 0.8, 0.88),
+        legend_pos=(0.52, 0.72, 0.78, 0.88),
+        yoffset=1.4,
+        more_logs=False,
+        oname="results/discussion/energies/{}.pdf".format(br.spell(particle))
+    )
