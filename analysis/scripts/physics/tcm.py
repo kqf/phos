b8fa87232c2f0aa07a685aeae0b77e6f52e6014c
Sasha <q.dev.null@gmail.com>
2020-03-01 19:10:42 +0100

    Remove fit-parameters in favour of inplace fixtures in TCM script

--- analysis/scripts/physics/tcm.py
+++ analysis/scripts/physics/tcm.py
@@ -7,6 +7,46 @@ import uncertainties.unumpy as unp
 import spectrum.broot as br
 from spectrum.plotter import plot
 
+from spectrum.spectra import energies, DataExtractor
+from spectrum.pipeline import FunctionTransformer
+from spectrum.pipeline import DataFitter, Pipeline
+
+
+def fitted(fitf):
+    def p(x, loggs):
+        return x
+
+    def fit_results(x, loggs):
+        res = br.fit_results(x.fitf)
+        res["energy"] = x.energy
+        return res
+
+    return Pipeline([
+        ("cyield", DataExtractor()),
+        ("fit", DataFitter(fitf)),
+        ("show", FunctionTransformer(p)),
+        ("res", FunctionTransformer(fit_results)),
+    ])
+
+
+@pytest.fixture
+def rawdata(particle, tcm):
+    return energies(particle, fitted(tcm))
+
+
+@pytest.mark.onlylocal
+@pytest.mark.interactive
+@pytest.mark.parametrize("particle", [
+    "#pi^{0}",
+    "#eta",
+])
+def test_updates_rawdata(particle, rawdata, ltitle):
+    with open("config/predictions/tcm.json") as f:
+        final = json.load(f)
+    final[particle] = rawdata
+    with open("config/predictions/tcm.json", "w") as f:
+        json.dump(final, f, indent=4)
+
 
 @pytest.fixture
 def data():
