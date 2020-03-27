0336b6cf1a225671687f8d8f848ecfe9fd2e8e9d
Sasha <q.dev.null@gmail.com>
2020-03-27 15:35:51 +0100

    Fix the debug pT plot

--- analysis/tests/debug/test_pt_dependence.py
+++ analysis/tests/debug/test_pt_dependence.py
@@ -1,30 +1,26 @@
 from __future__ import print_function
 import pytest
 
+import spectrum.broot as br
 from spectrum.output import open_loggs
 from spectrum.options import CompositeEfficiencyOptions
 from spectrum.efficiency import PeakPositionWidthEstimator
+from spectrum.efficiency import efficiency_data
 from spectrum.comparator import Comparator
-import spectrum.broot as br
-from spectrum.vault import DataVault
 
 
 @pytest.fixture
 def data():
-    return (
-        DataVault().input("single #pi^{0}", "low", "PhysEff"),
-        DataVault().input("single #pi^{0}", "high", "PhysEff"),
-    )
+    return efficiency_data()
 
 
 @pytest.mark.onlylocal
 @pytest.mark.interactive
-def test_pt_ranges(data):
+def test_pt_ranges(data, stop=False):
     estimator = PeakPositionWidthEstimator(
         CompositeEfficiencyOptions("#pi^{0}")
     )
-    with open_loggs("test ranges") as loggs:
+    with open_loggs() as loggs:
         mass, massf, width, widthf = estimator._estimate(data, loggs)
-        Comparator().compare(mass)
-        # Comparator().compare(width)
+        Comparator(stop=stop).compare(mass)
         print(br.pars(massf))
