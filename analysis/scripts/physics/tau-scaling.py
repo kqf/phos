682f73268a8749404ab121e1b85c645d276308c3
Sasha <q.dev.null@gmail.com>
2020-10-16 10:44:35 +0200

    Fix the Tau distribution calculation

--- analysis/scripts/physics/tau-scaling.py
+++ analysis/scripts/physics/tau-scaling.py
@@ -8,15 +8,20 @@ from spectrum.spectra import energies, DataEnergiesExtractor
 from spectrum.constants import invariant_cross_section_code
 
 
+def tau(pt, energy, lambda_=0.2, Q0=1, W0=1e-3):
+    W = W0 * energy
+    return (pt ** 2) / Q0 * (pt / W) ** lambda_
+
+
 class TauTransformer(TransformerBase):
     def transform(self, x, loggs):
         edges = br.edges(x.tot)
-        xtedges = 2 * edges / x.energy
+        tau_edges = tau(edges, x.energy)
 
         xt = br.PhysicsHistogram(
-            self._transform(x.tot, xtedges),
-            self._transform(x.stat, xtedges),
-            self._transform(x.syst, xtedges),
+            self._transform(x.tot, tau_edges),
+            self._transform(x.stat, tau_edges),
+            self._transform(x.syst, tau_edges),
             energy=x.energy,
         )
         xt.fitf = x.fitf
@@ -50,26 +55,6 @@ def tau_data(particle, tcm):
     return spectra
 
 
-@pytest.fixture
-def combined_n(xtrange):
-    return 5.099, 0.009
-
-
-@pytest.fixture
-def xtrange():
-    return 2.9e-03, 1.05e-02
-
-
-@pytest.fixture
-def tau_sdata(tau_data, combined_n):
-    scaled = []
-    for h in tau_data:
-        s = h.Clone("{}_scaled".format(h.GetName()))
-        s.Scale(h.energy ** combined_n[0])
-        scaled.append(s)
-    return scaled
-
-
 @pytest.mark.thesis
 @pytest.mark.onlylocal
 @pytest.mark.interactive
@@ -77,12 +62,11 @@ def tau_sdata(tau_data, combined_n):
     "#pi^{0}",
     "#eta",
 ], scope="module")
-def test_scaled_spectra(tau_sdata, combined_n, ltitle, oname):
-    title = "(#sqrt{{#it{{s}}}})^{{{n:.3f}}} (GeV)^{{{n:.3g}}} #times {t}"
+def test_scaled_spectra(tau_data, ltitle, oname):
     plt.plot(
-        tau_sdata,
-        ytitle=title.format(n=combined_n[0], t=invariant_cross_section_code()),
-        xtitle="#it{x}_{T}",
+        tau_data,
+        ytitle=invariant_cross_section_code(),
+        xtitle="#tau",
         ltitle=ltitle,
         more_logs=False,
         legend_pos=(0.24, 0.15, 0.5, 0.35),
