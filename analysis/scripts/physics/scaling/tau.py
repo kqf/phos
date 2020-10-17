9b29c76d5f4e8a41999f5c22158101d844ec0f18
Sasha <q.dev.null@gmail.com>
2020-10-17 12:48:38 +0200

    Move all scaling scripts to the same module

---
+++ analysis/scripts/physics/scaling/tau.py
@@ -0,0 +1,83 @@
+import pytest
+
+import spectrum.broot as br
+import spectrum.plotter as plt
+
+from spectrum.pipeline import TransformerBase, Pipeline, DataFitter
+from spectrum.spectra import energies, DataEnergiesExtractor
+from spectrum.constants import invariant_cross_section_code
+
+
+def tau(pt, energy, lambda_=0.27, Q0=1, W0=1e-3):
+    W = W0 * energy
+    return (pt ** 2) / Q0 * (pt / W) ** lambda_
+
+
+class TauTransformer(TransformerBase):
+    def __init__(self, lambda_, plot=False):
+        super().__init__(plot)
+        self.lambda_ = lambda_
+
+    def transform(self, x, loggs):
+        edges = br.edges(x.tot)
+        tau_edges = tau(edges, x.energy, lambda_=self.lambda_)
+
+        htau = br.PhysicsHistogram(
+            self._transform(x.tot, tau_edges),
+            self._transform(x.stat, tau_edges),
+            self._transform(x.syst, tau_edges),
+            energy=x.energy,
+        )
+        htau.fitf = x.fitf
+        return htau
+
+    @staticmethod
+    def _transform(hist, edges):
+        return br.table2hist(
+            "{}_tau".format(hist.GetName()),
+            hist.GetTitle(),
+            br.bins(hist).contents,
+            br.bins(hist).errors,
+            edges
+        )
+
+
+def tau_spectra(fitf, lambda_):
+    return Pipeline([
+        ("cyield", DataEnergiesExtractor()),
+        ("fit", DataFitter(fitf)),
+        ("tau", TauTransformer(lambda_=lambda_)),
+    ])
+
+
+@pytest.fixture
+def tau_data(particle, lambda_, tcm):
+    spectra = sorted(
+        energies(particle, tau_spectra(tcm, lambda_)),
+        key=lambda x: -x.energy
+    )
+    return spectra
+
+
+@pytest.fixture
+def lambda_():
+    return 0.27
+
+
+@pytest.mark.thesis
+@pytest.mark.onlylocal
+@pytest.mark.interactive
+@pytest.mark.parametrize("particle", [
+    "#pi^{0}",
+    "#eta",
+], scope="module")
+def test_scaled_spectra(tau_data, lambda_, ltitle, oname):
+    plt.plot(
+        tau_data,
+        ytitle=invariant_cross_section_code(),
+        xtitle="#tau (#lambda = {})".format(lambda_),
+        ltitle=ltitle,
+        more_logs=False,
+        legend_pos=(0.24, 0.15, 0.5, 0.35),
+        oname=oname.format("tau_scaling/tau_normalized_cross_section_"),
+    )
---
+++ analysis/scripts/physics/scaling/u.py
@@ -0,0 +1,82 @@
+import pytest
+
+import spectrum.broot as br
+import spectrum.plotter as plt
+
+from spectrum.pipeline import TransformerBase, Pipeline, DataFitter
+from spectrum.spectra import energies, DataEnergiesExtractor
+from spectrum.constants import invariant_cross_section_code
+
+
+# Empirical approximation of <pT(s)>
+def average_pt(energy, a=0.235, n=0.096):
+    return a * energy ** n
+
+
+def b_(energy, a=-0.0397, b=0.08, n=-0.075):
+    return a + b * energy ** n
+
+
+def u_(pt, energy, mean_pt):
+    return pt / (mean_pt - b_(energy) * pt)
+
+
+class UTransformer(TransformerBase):
+    def transform(self, x, loggs):
+        edges = br.edges(x.tot)
+        u_edges = u_(edges, x.energy, average_pt(x.energy))
+
+        u = br.PhysicsHistogram(
+            self._transform(x.tot, u_edges),
+            self._transform(x.stat, u_edges),
+            self._transform(x.syst, u_edges),
+            energy=x.energy,
+        )
+        u.fitf = x.fitf
+        return u
+
+    @staticmethod
+    def _transform(hist, edges):
+        return br.table2hist(
+            "{}_u".format(hist.GetName()),
+            hist.GetTitle(),
+            br.bins(hist).contents,
+            br.bins(hist).errors,
+            edges
+        )
+
+
+def u_spectra(fitf):
+    return Pipeline([
+        ("cyield", DataEnergiesExtractor()),
+        ("fit", DataFitter(fitf)),
+        ("u", UTransformer()),
+    ])
+
+
+@pytest.fixture
+def u_data(particle, tcm):
+    spectra = sorted(
+        energies(particle, u_spectra(tcm)),
+        key=lambda x: -x.energy
+    )
+    return spectra
+
+
+@pytest.mark.thesis
+@pytest.mark.onlylocal
+@pytest.mark.interactive
+@pytest.mark.parametrize("particle", [
+    "#pi^{0}",
+    "#eta",
+], scope="module")
+def test_scaled_spectra(u_data, ltitle, oname):
+    plt.plot(
+        u_data,
+        ytitle=invariant_cross_section_code(),
+        xtitle="#it{u}",
+        ltitle=ltitle,
+        more_logs=False,
+        legend_pos=(0.24, 0.15, 0.5, 0.35),
+        oname=oname.format("u_scaling/u_normalized_cross_section_"),
+    )
