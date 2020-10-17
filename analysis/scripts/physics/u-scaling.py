07f9783fcfaa0a1d216da3da45cba42cf10c1b68
Sasha <q.dev.null@gmail.com>
2020-10-17 10:47:39 +0200

    Clean the names and labels for u-scaling

--- analysis/scripts/physics/u-scaling.py
+++ analysis/scripts/physics/u-scaling.py
@@ -8,7 +8,7 @@ from spectrum.spectra import energies, DataEnergiesExtractor
 from spectrum.constants import invariant_cross_section_code
 
 
-def tau(pt, energy, lambda_=0.27, Q0=1, W0=1e-3):
+def u_(pt, energy, lambda_=0.27, Q0=1, W0=1e-3):
     W = W0 * energy
     return (pt ** 2) / Q0 * (pt / W) ** lambda_
 
@@ -19,27 +19,23 @@ def average_pt(energy, a=0.235, n=0.096):
 
 
 class UTransformer(TransformerBase):
-    def __init__(self, lambda_, plot=False):
-        super().__init__(plot)
-        self.lambda_ = lambda_
-
     def transform(self, x, loggs):
         edges = br.edges(x.tot)
-        tau_edges = tau(edges, x.energy, lambda_=self.lambda_)
+        u_edges = u_(edges, x.energy)
 
-        htau = br.PhysicsHistogram(
-            self._transform(x.tot, tau_edges),
-            self._transform(x.stat, tau_edges),
-            self._transform(x.syst, tau_edges),
+        u = br.PhysicsHistogram(
+            self._transform(x.tot, u_edges),
+            self._transform(x.stat, u_edges),
+            self._transform(x.syst, u_edges),
             energy=x.energy,
         )
-        htau.fitf = x.fitf
-        return htau
+        u.fitf = x.fitf
+        return u
 
     @staticmethod
     def _transform(hist, edges):
         return br.table2hist(
-            "{}_tau".format(hist.GetName()),
+            "{}_u".format(hist.GetName()),
             hist.GetTitle(),
             br.bins(hist).contents,
             br.bins(hist).errors,
@@ -47,18 +43,18 @@ class UTransformer(TransformerBase):
         )
 
 
-def tau_spectra(fitf, lambda_):
+def u_spectra(fitf):
     return Pipeline([
         ("cyield", DataEnergiesExtractor()),
         ("fit", DataFitter(fitf)),
-        ("tau", UTransformer(lambda_=lambda_)),
+        ("u", UTransformer()),
     ])
 
 
 @pytest.fixture
-def tau_data(particle, lambda_, tcm):
+def u_data(particle, tcm):
     spectra = sorted(
-        energies(particle, tau_spectra(tcm, lambda_)),
+        energies(particle, u_spectra(tcm)),
         key=lambda x: -x.energy
     )
     return spectra
@@ -71,13 +67,13 @@ def tau_data(particle, lambda_, tcm):
     "#pi^{0}",
     "#eta",
 ], scope="module")
-def test_scaled_spectra(tau_data, lambda_, ltitle, oname):
+def test_scaled_spectra(u_data, ltitle, oname):
     plt.plot(
-        tau_data,
+        u_data,
         ytitle=invariant_cross_section_code(),
-        xtitle="#tau (#lambda = {})".format(lambda_),
+        xtitle="#it{u/u_{0}}",
         ltitle=ltitle,
         more_logs=False,
         legend_pos=(0.24, 0.15, 0.5, 0.35),
-        oname=oname.format("tau_scaling/tau_normalized_cross_section_"),
+        oname=oname.format("u_scaling/u_normalized_cross_section_"),
     )
